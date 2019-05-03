05171545c2468cec9a8071efc22355604402b9ec
Kelley <kelleyr@duvalschools.org>
2019-05-03 09:06:05 -0400

    Multiplying and Dividing Fractions
    Use this code template to write a program that can multiply or divide proper and improper fractions.  You do not need to convert mixed numbers or reduce the final answer.

---
+++ multiplying_dividing_fractions/multiply_divide_fractions.py
@@ -0,0 +1,28 @@
+# This program will multiply and divide proper and improper fractions. 
+
+print("Hello, Welcome to FractionBot 5000.\n") # Change this code to say something different.
+print("I am fully capable of multiplying and dividing proper and improper fractions.\n")
+
+numerator0 = int(input("What is the first numerator? [Type a number and press enter.] ")) # Make sure it's an integer!
+denominator0 = int(input("What is the first denominator? [Type a number and press enter.] " ))
+
+print("Your first fraction is",numerator0,"/",denominator0,".")
+
+# Run the above code and see how it works! 
+
+print("What is the second fraction?")
+
+# Add code here to input the numerator and denominator for the second fraction.  Print the second fraction on the screen. 
+
+multiply_divide = int(input(print("Type 1 and press enter to multiply the fractions.  Type 2 and press enter to divide the fractions.\n")
+
+# This creates two variables for the NEW fraction.  Both variables are set equal to zero to start. 
+new_numerator = 0
+new_denominator = 0 
+
+if multiply_divide == : # What number for this variable means to multiply? 
+    # Write the code to multiply the two fractions here. 
+else: 
+    # Write the code to divide the two fractions here.  
+
+# Print the new fraction back onto the screen. 
