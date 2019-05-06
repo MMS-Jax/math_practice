67bc34a2529db18ee7c93aa22cb3588bb95f7d03
MoltenEngine <49403073+MoltenEngine@users.noreply.github.com>
2019-05-06 10:16:43 -0400

    Update eden_b_multiplying_and_dividing.py

--- multiplying_dividing_fractions/eden_b_multiplying_and_dividing.py
+++ multiplying_dividing_fractions/eden_b_multiplying_and_dividing.py
@@ -1,1 +1,10 @@
 # Multiplying and Dividing Eden B 5/06/2019
+
+print("Welcome to fraction bot 9001! I can multiply and divide fractions for you!\n")
+print("In order to multiply and divide fractions i need to know the [....] and [....]\n")
+
+numerator0=0
+numerator1=0
+denominator0=0
+denominator1=0
+
