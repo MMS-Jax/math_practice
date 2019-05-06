eea30687b299cce617e2bfe761eaa73e90fc14c1
Jonathan Meister <49206594+JonathanHatesMathForeverUntillDeath@users.noreply.github.com>
2019-05-06 11:46:38 -0400

    Update meister_j_multiplying_divide_fractions.py
    i added the print commands

--- multiplying_dividing_fractions/meister_j_multiplying_divide_fractions.py
+++ multiplying_dividing_fractions/meister_j_multiplying_divide_fractions.py
@@ -1,1 +1,3 @@
 # Jonathan M. , Multiplying and dividing fractions, 05/06/2019
+print("Hello! My name is Satan. I can multiply and divide fractions")
+print("To multiply and divide fractions I need to know the numerator and the denominator")
