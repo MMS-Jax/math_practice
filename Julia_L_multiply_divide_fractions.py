32d4219843234129464463230a9f1541c626ef2d
Julia.L <s11042561@students.duvalschools.org>
2019-05-06 11:06:10 -0400

    Julia_L
    leaned how to multiply and divide fractions

--- Julia_L_multiply_divide_fractions.py
+++ Julia_L_multiply_divide_fractions.py
@@ -1,3 +1,7 @@
 #Julia L ., Multiply and Dividing Fractions
 
 print('Hello my name is fractionbot 900 I multiply and divide frcations.\n')
+print('To multiply and divide fractions I need to know the [Blank] and [Blank] for each fraction.\n')
+
+numerator0 = 0
+numerator1 = 0
