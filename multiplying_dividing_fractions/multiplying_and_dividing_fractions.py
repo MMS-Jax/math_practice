360f8572859a16392ab82166eb81b106ebad8147
s11177524 <49206539+s11177524@users.noreply.github.com>
2019-05-06 11:57:49 -0400

    Update multiplying_and_dividing_fractions.py

--- multiplying_dividing_fractions/multiplying_and_dividing_fractions.py
+++ multiplying_dividing_fractions/multiplying_and_dividing_fractions.py
@@ -1,3 +1,4 @@
 #s11177524, multiplying and dividing fractions, 5/06/19
 
-print("Hello, I'm Dean. I will help you multiply and divide fractions.")
+print("Hello, I'm Dean. I will help you multiply and divide fractions.\n")
+print("In order to multiply or divide fractions, you need to know the numerator and the denominator of each fraction before you begin.")
