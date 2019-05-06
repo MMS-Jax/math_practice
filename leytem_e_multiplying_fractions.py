1945f9bbe16b7f8c358774598ce31aaface22a14
CheeseWife <49404967+CheeseWife@users.noreply.github.com>
2019-05-06 11:05:43 -0400

    new code

--- leytem_e_multiplying_fractions.py
+++ leytem_e_multiplying_fractions.py
@@ -1,3 +1,7 @@
 #Evan L. fraction robot 5/6/19
 
 print("Give me fractions to multiply, if you dare.\n")
+print("if you are up to the challenge you must give me the numerator and denominator for each desired fraction.")
+
+numerator0 = 0
+numerator1 = 0
