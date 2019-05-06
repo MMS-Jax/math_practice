598b30bc4899eb5b342b4ae0ea3580d1ce2be3d6
Parker <s11562949@students.duvalschools.org>
2019-05-06 11:06:00 -0400

    Update kane_p_multiplying_dividing_fractions.py

--- kane_p_multiplying_dividing_fractions.py
+++ kane_p_multiplying_dividing_fractions.py
@@ -1,3 +1,10 @@
 # Parker K., Multiplying And Dividing Fractions, 05/06/19
 
 print("Hello my name is fractiony boy. I can multiply and divide fractions for you.\n")
+print("To multiply and divide fractions I need you to tell me what the numerator and denominator of the fraction is.\n")
+
+numerator0 = 0
+numerator1 = 0
+
+denominator0 = 0
+denominator1 = 0
