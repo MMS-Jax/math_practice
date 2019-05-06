b7a24bfcd717dffa01888b566affd8e7e7266824
Ryan Kelley <kelleyr@duvalschools.org>
2019-05-06 11:17:52 -0400

    delete

--- multiplying_dividing_fractions/kelley_r_multiply_divide_fractions.py
+++ multiplying_dividing_fractions/kelley_r_multiply_divide_fractions.py
@@ -1,3 +1,11 @@
 # Ryan K., Multiply and Divide Fractions, 05/06/2019 
 
 print("Hello, my name is FractionBot 9000.  I can multiply and divide fractions for you.\n")
+print("To multiply and divide fractions I need to know the numerator and denominator for each fraction.\n")
+
+# Create variables for both numerators and initialize them to 0. 
+numerator0 = 0
+numerator1 = 0 
+
+# What do you call the variables for the denominator? 
+
