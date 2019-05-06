5e842ef90775851bd297113f360262a1e5c085a8
s11649159 <49206466+s11649159@users.noreply.github.com>
2019-05-06 11:58:44 -0400

    Update ben_h_multiply_divide_fractions.py
    fixed things

--- multiplying_dividing_fractions/ben_h_multiply_divide_fractions.py
+++ multiplying_dividing_fractions/ben_h_multiply_divide_fractions.py
@@ -1,3 +1,4 @@
 # Ben H., Multiplying and Divivding Fractions, 05/06/2019
 
-print("Hello!  My name is Fraction Master- Over 9000. I will ultiply and divide fractions for you. ")
+print("Hello! My name is Fraction Master- Over 9000. I will ultiply and divide fractions for you. \n ")
+print ("In order to Multiply or Divivde fractions, I need to know the numerator and denomenator for both fractions.")
