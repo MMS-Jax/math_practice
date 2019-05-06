cda847c7ceafa890dd554203ae58464fb748e4df
EpIc gAmEr <49242497+s11590452@users.noreply.github.com>
2019-05-06 11:05:40 -0400

    SDFFSDF
    SDFASDFFSFSA

--- brown_c_multiplying_fractions.py
+++ brown_c_multiplying_fractions.py
@@ -1,3 +1,10 @@
 # Chase b. fraction math 5/6/19
 
-print("My name is BigNerd 72. I will multiply your fractions.\n")
+print("My name is BigNerd 72. I will multiply and divide your fractions.\n")
+print("In order to be your nerd i need to know the denominator and the numerator of the fractions you wish to multiply or divide\n")
+
+numer_0 = 0
+numer_1 = 0
+
+denom_0 = 0
+denom_1 = 0
