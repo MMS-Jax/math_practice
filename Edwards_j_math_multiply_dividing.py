2f7a78309a381cf5fd355e3c2a6276b6290bae73
joshua <s11688777@students.duvalschools.org>
2019-05-06 11:06:50 -0400

    Update Edwards_j_math_multiply_dividing.py
    need numarator

--- Edwards_j_math_multiply_dividing.py
+++ Edwards_j_math_multiply_dividing.py
@@ -1,3 +1,6 @@
 # joshua_edwards,multiply and divide fractions 05/06/2019
 
 print("hello,my name is fraction bot i can do any fraction math problem for you i am the smarted bot in the world!!!!!!!!!!!!\n")
+print("to multiply and divide frations i need to know the [blank] and [blank]" )
+
+numerator0=0
