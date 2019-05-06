d1a79be7359f584af4f2a8a31054fcac49eff673
Potato5789 <49240664+Potato5789@users.noreply.github.com>
2019-05-06 10:16:25 -0400

    Updated_math_code

--- multiplying_dividing_fractions/Math_Code.py
+++ multiplying_dividing_fractions/Math_Code.py
@@ -1,1 +1,10 @@
 # Multiplying and Dividing Fractions by Skylar C. 5/06/19
+
+print("Welcome to the Fraction Calculator! I can multiply and divide fractions for you.\n" )
+print("If you wish to multiply and divide fractions we need to know the numerator and denominator.\n")
+
+numerator0 = 0
+numerator1 = 0
+denominator0 = 0
+denominator1 = 0
+print("The first numerator is: 0, \n")
