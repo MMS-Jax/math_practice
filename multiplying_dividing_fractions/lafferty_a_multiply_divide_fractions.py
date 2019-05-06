9f81d4ca20abb76d844738592eba92fb345185fa
Ashley L <s11544541@student.duvalschools.org>
2019-05-06 11:57:45 -0400

    Update lafferty_a_multiply_divide_fractions.py
    code for multiplying and dividing fractions.

--- multiplying_dividing_fractions/lafferty_a_multiply_divide_fractions.py
+++ multiplying_dividing_fractions/lafferty_a_multiply_divide_fractions.py
@@ -1,4 +1,4 @@
 #Ashley L., Multiplying and Dividing Fractions, 05/06/2019
 
-print("Hello, My name is Mathemater and i will multiply and divide fractions.")
-print("In order to multipy I need to know the numerator an the denominator of the fractions.")
+print("Hello, My name is Mathemater and i will multiply and divide fractions.\n")
+print("In order to multipy and divide fractions I need to know the numerator an the denominator of both fractions.")
