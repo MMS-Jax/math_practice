fa392b6bef368f435443624976994c44349c2fa7
SheepE5185 <49403223+SheepE5185@users.noreply.github.com>
2019-05-06 10:12:27 -0400

    Created multiplying and dividing fractions version 1.0

---
+++ multiplying_dividing_fractions/eslinger_logan-multiplydividefractions.py
@@ -0,0 +1,7 @@
+#Multiplying and dividing fractions by Logan E. Version 1.0
+
+print("Hello, welcome to fraction calculator 1234. I will multiply and divide fractios for you.\n")
+print("For me to do this, I need to know the numerator and the denominator.\n")
+
+numerator0 = 0
+numerator1 = 0
