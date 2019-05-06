b6d2bc0d9445a4b34910aa3c7f1d3c4f18c423d7
xxxTENSION-CORD <s11928454@students.duvalschools.org>
2019-05-06 11:05:35 -0400

    Create AARON_H.py

---
+++ AARON_H.py
@@ -0,0 +1,4 @@
+#AARON_HUNGERMAN, MULTIPLY AND DIVIDE FRACTIONS, 5-6-19\
+
+print("YO! WUT IZZZzzz UP. IMA GONNA DO UR MATH CUZZZzzz ima DR. Savage FractionZZZzzz!!!!!!!!!!!!!!!!!!!!!!!!!!!\n")
+print("to do the multipying and dividing of fractions savgely we must know blank and blank about them")
