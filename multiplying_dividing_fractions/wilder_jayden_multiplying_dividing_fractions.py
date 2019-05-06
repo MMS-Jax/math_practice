0afd83c5238c51ac9f2f490225c2da1f3bf3cce0
DGK-Jayden <49407930+DGK-Jayden@users.noreply.github.com>
2019-05-06 11:59:18 -0400

    dividing and multiplying
    we did some coding for multiplication and division

---
+++ multiplying_dividing_fractions/wilder_jayden_multiplying_dividing_fractions.py
@@ -0,0 +1,4 @@
+# Jayden W, Multiplying and Dividing Fractions, 05/06/2019
+
+print("Mr. Thick is going to destroy the earth, we need to use math to help bepis protect the people of shell city\n")
+print("In order to save shell city, bepis will need to know the numerator and denominator for the fractions\n")
