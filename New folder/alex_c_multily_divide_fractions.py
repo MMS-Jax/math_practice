aaaa51b536c0595c95b6722ff61a9b344ba95153
alexalexrc <49314337+alexalexrc@users.noreply.github.com>
2019-05-06 11:07:18 -0400

    alexalexrc
    multiply and divide fractions.

--- New folder/alex_c_multily_divide_fractions.py
+++ New folder/alex_c_multily_divide_fractions.py
@@ -1,3 +1,7 @@
 # Alex C., Multiply and Divide Fractions, 05/06/2019
 
 print("Hello, my name is fraction bot 9000. I can multiply and divide fractions for you.\n")
+print("To multiply and divide fractions I need to know the numerator and denominator for each fraction./n")
+
+numerator0=0
+numerator1=0
