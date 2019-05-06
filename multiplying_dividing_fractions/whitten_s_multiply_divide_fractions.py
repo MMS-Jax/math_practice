42026f03d8c2ec5ed5d0b878b31e94de043f87a6
Shay <49175183+this-mailbox-is-mine@users.noreply.github.com>
2019-05-06 11:58:41 -0400

    Updated whitten_s_multiply_divide_fractions
    Added print lines to code.

--- multiplying_dividing_fractions/whitten_s_multiply_divide_fractions.py
+++ multiplying_dividing_fractions/whitten_s_multiply_divide_fractions.py
@@ -1,1 +1,4 @@
 # Sheyna W., Multiplying and Dividing Fractions, 05/06/19
+
+print("Hello, my name is Dr. Phil the Fraction God, and I was made to consult you on multiplying and dividing fractions. Nice to meet you.\n ")
+print("In order to multiply and divide fractions for you, sir or madam, I need to know both the numerator and denominator of your fractions.")
