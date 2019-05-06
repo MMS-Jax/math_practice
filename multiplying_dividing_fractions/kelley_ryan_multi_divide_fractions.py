d28482d0087eaff55ae4dbcc839905c449131c93
Ryan Kelley <kelleyr@duvalschools.org>
2019-05-06 10:23:25 -0400

    Test

--- multiplying_dividing_fractions/kelley_ryan_multi_divide_fractions.py
+++ multiplying_dividing_fractions/kelley_ryan_multi_divide_fractions.py
@@ -1,1 +1,9 @@
 # Multiplying and Dividing Fractions by Ryan K.  05/06/2019 version 1.0 
+
+print("Welcome to Fraction-Bot 9000!  I can multiply and divide fractions for you.\n")
+print("In order to multiply or divide fractions we need to know the [....] and [....].\n")
+
+numerator0 = 0
+numerator1 = 0
+
+print("The first numerator is:", numerator0,".")
