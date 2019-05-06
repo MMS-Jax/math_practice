269834918907688f7d40de28f2f7f73dff635bb1
Cavespider386 <49206765+Cavespider386@users.noreply.github.com>
2019-05-06 11:47:18 -0400

    Added print command for user
    Introduced user to math homework bot... talked about fractiomanians.

--- multiplying_dividing_fractions/carson_s_mtply_dvde_fractions.py
+++ multiplying_dividing_fractions/carson_s_mtply_dvde_fractions.py
@@ -1,1 +1,3 @@
 # Carson S., Multiply and Divide Fractions, 05/06/2019 
+
+print("Hello, my name is MathHomeworkBotwork. My Master sent me to help you with your math.  I can MULTIPLY and DIVIDE FRACTIOMANIANS for you, or on your planet, they call them fractions.")
