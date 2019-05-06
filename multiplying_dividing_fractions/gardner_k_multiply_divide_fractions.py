b550744330ad853ddc5b292105681dc1cc9d6d7b
Khalil06 <49448376+FireAndIce904@users.noreply.github.com>
2019-05-06 11:59:09 -0400

    added speech
    added speech

--- multiplying_dividing_fractions/gardner_k_multiply_divide_fractions.py
+++ multiplying_dividing_fractions/gardner_k_multiply_divide_fractions.py
@@ -1,1 +1,4 @@
 # Gardner K., Multiplying and Dividing Fractions ,05/06/2019
+
+print("Hello, My name is Mathmatical radical calculator. I will help you calculate fractions.\n")
+print("In order to multiply and divide your fractions I need the numerator and demoninator")
