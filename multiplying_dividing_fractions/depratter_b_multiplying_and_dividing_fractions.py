8f0f8eefb02d480ea3ccd80f0f5eec52d39d4cdf
Zephyrhills <49448677+Zephyrhills@users.noreply.github.com>
2019-05-06 11:58:52 -0400

    Update depratter_b_multiplying_and_dividing_fractions.py

--- multiplying_dividing_fractions/depratter_b_multiplying_and_dividing_fractions.py
+++ multiplying_dividing_fractions/depratter_b_multiplying_and_dividing_fractions.py
@@ -1,1 +1,4 @@
-# Brendan D. , Multiplying and Dividing Fractions, 05/06/2019tion
+# Brendan D. , Multiplying and Dividing Fractions, 05/06/2019
+
+print("Hello! My name is Engamespoiler-9000, here to help you multiply and divide fractions.\n")
+print("In order to help youmultiply and divide fractions I need to know the numerator and denomerator for each fraction. ")
