5f00e144877d20ca2182dd044a0bdf599dd5086d
isabella1231 <49206750+isabella1231@users.noreply.github.com>
2019-05-06 11:57:22 -0400

    added print with instructions

--- isabella_S._multiplying_and_dividing_fractions.py
+++ isabella_S._multiplying_and_dividing_fractions.py
@@ -1,3 +1,4 @@
 #isabella S. , multiplying and dividing fractions 05/06/2019
 
-print("hello! my name is super math bot i will multiply and divide fractions for you!")
+print("hello! my name is super math bot i will multiply and divide fractions for you!\n")
+print("to multiply and divide fractions i need to know the numeraator and denomenator for each fraction i am given.\n")
