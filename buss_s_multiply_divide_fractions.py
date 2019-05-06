b09dc7c2797f2bd963fb86332071dfc05ebf78a6
LilFoxy1983 <49206772+LilFoxy1983@users.noreply.github.com>
2019-05-06 11:56:23 -0400

    Update buss_s_multiply_divide_fractions.py
    Multiplying and dividing fractions

--- buss_s_multiply_divide_fractions.py
+++ buss_s_multiply_divide_fractions.py
@@ -1,3 +1,4 @@
 # Serinity B., Multiplying and divinding Fractions, 05/06/2019
 
 print("hello! My Name Is MathHelper and I will multiply and divide fractions.")
+Print("In order to multiply fractions, you need to know the numerator and the denominator of the fractions.")
