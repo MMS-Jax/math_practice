ebf4dc699ef5abb92cc9d7004e90e40af77b24fd
Rainbow SteveGamingYT <49448622+RainbowSteveGamingYT@users.noreply.github.com>
2019-05-06 11:58:16 -0400

    Added second print code.
    Added second print code in the VSC.

--- multiplying_dividing_fractions/Washington_j_multiply_divide_fractions.py
+++ multiplying_dividing_fractions/Washington_j_multiply_divide_fractions.py
@@ -1,3 +1,4 @@
 # Joseph W., Multiplying and Dividing Fractions, 05/06/2019
 
-print ("Hello! My name is PCBrine. I will multiply and divide fractions for you.")
+print ("Hello! My name is PCBrine. I will multiply and divide fractions for you.\n")
+print ("In order for me to know how to multiply and divide fractions, I need to know numerator and denominator.")
