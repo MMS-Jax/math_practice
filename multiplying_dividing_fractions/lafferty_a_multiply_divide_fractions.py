96775ac60a9dabfaf7d46880248bbe43e653b621
Ashley L <s11544541@student.duvalschools.org>
2019-05-06 11:54:26 -0400

    Update lafferty_a_multiply_divide_fractions.py
    multiplying and dividing fractons code.

--- multiplying_dividing_fractions/lafferty_a_multiply_divide_fractions.py
+++ multiplying_dividing_fractions/lafferty_a_multiply_divide_fractions.py
@@ -1,3 +1,4 @@
 #Ashley L., Multiplying and Dividing Fractions, 05/06/2019
 
 print("Hello, My name is Mathemater and i will multiply and divide fractions.")
+print("In order to multipy I need to know the numerator an the denominator of the fractions.")
