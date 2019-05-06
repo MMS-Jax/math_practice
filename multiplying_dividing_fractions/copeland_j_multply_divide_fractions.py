5199aab9370a60df2cba51c9207e606ccba31293
JacksonXD <s11558988@students.duvalschools.org>
2019-05-06 11:58:12 -0400

    Update copeland_j_multply_divide_fractions.py
    He needs to know the denominator and numerator

--- multiplying_dividing_fractions/copeland_j_multply_divide_fractions.py
+++ multiplying_dividing_fractions/copeland_j_multply_divide_fractions.py
@@ -1,3 +1,4 @@
 #Jackson C., Multpily and dividing fractions, 05/06/19
 
-print("hello my name is Big Billy Chilly Boi and I multiply and divide fractions")
+print("hello my name is Big Billy Chilly Boi and I multiply and divide fractions"\n)
+print("To multiply and divide fractions for you master,I need to know the numerator and denominator."\n)
