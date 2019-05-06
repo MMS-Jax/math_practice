1c2ba7872853702542196b823159663f572053f5
s11649159 <49206466+s11649159@users.noreply.github.com>
2019-05-06 11:48:48 -0400

    Updated ben_h_multiply_divide_fractions
    Added my first python coding, hello world!

--- multiplying_dividing_fractions/ben_h_multiply_divide_fractions.py
+++ multiplying_dividing_fractions/ben_h_multiply_divide_fractions.py
@@ -1,1 +1,3 @@
-# Ben H., Multiplyeing and Divivding Fractions, 05/06/2019
+# Ben H., Multiplying and Divivding Fractions, 05/06/2019
+
+print("Hello!  My name is Fraction Master- Over 9000. I will ultiply and divide fractions for you. ")
