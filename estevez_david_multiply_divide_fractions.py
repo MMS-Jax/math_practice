3de02a9ca3736e87a2abecedd1ecacb50438af76
DoontBullyMe <49206746+DoontBullyMe@users.noreply.github.com>
2019-05-06 11:57:46 -0400

    Update estevez_david_multiply_divide_fractions.py
    Update

--- estevez_david_multiply_divide_fractions.py
+++ estevez_david_multiply_divide_fractions.py
@@ -1,3 +1,4 @@
 # David E., Multiplying and Dividing Fractions, 05/06/2019 
 
-print("Hello My name is MethSolver 2398. I will solve any math problem for you.")
+print("Hello My name is MethSolver 2398. I will multiply and divide fractions for you.\n")
+print("In oerder to save Mathmoland. I need to know the numerator and denominator for both fractions")
