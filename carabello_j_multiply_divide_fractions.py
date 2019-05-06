a07326ee1cb963bafd801e237de6475b7a561796
JayBello33 <49405355+JayBello33@users.noreply.github.com>
2019-05-06 11:07:34 -0400

    Multiplying and dividing fractions
    I coded today :)

---
+++ carabello_j_multiply_divide_fractions.py
@@ -0,0 +1,10 @@
+# Carabello J., Multiply and divide fractions 05/06/2019
+
+print("Hello, my name is LilRobot 3000 and I multiply and divide fractions for you.")
+("To multiply and divide fractions i needto kmow the numerator and denominator of the fraction.")
+
+numerator0 = 0
+numerator0 = 1
+
+
+
