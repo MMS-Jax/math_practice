ee1e8a0f61d7e26ebdc0cc13a36746c61bc5deee
Zani <s11673969@students.duvalschools.org>
2019-05-07 12:37:58 -0400

    Update andi_zani_multiplying_dividing_fractions.py

--- andi_zani_multiplying_dividing_fractions.py
+++ andi_zani_multiplying_dividing_fractions.py
@@ -1,4 +1,5 @@
 # Andi Z., Mutiplying and Dividing 05/06/19 
 
-print('Hello! My Name is Soviet Sausage I am here to help you multiply and divide fraction')
-print("In order to multiply or divde fractions,you must give me the [...] and [...] of the fractions.|")
+print('Hello! My Name is Soviet Sausage   I can multiply and divide fraction for you!\n')
+user_name = input ("What is your name? [Type your name and press ENTER.]\n")
+print("Hello,", user_name" how are you today?") 
