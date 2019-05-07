29355c311036bbce55fa2c421fcd9a715b28786e
DGK-Loo <s11370137@students.duvalschools.org>
2019-05-07 12:39:29 -0400

    added first print() string.
    printed some basi intructions for the user.

---
+++ multiplying_dividing_fractions/joseph_loovensky_multiplying_dividing_fractions_0.1.py
@@ -0,0 +1,4 @@
+# Joseph., Multiplying and Dividing Fraction, 05/06/19, version 0.1
+
+print("Hi, my fish is drowning please help me solve a math question to revive him. \n")
+print("In order to revive my dying fish we must find out the numerator and dinomerator. \n")
