cb43da52e101de26ea426b60208917d108036241
Ashley L <s11544541@student.duvalschools.org>
2019-05-07 12:34:53 -0400

    Update lafferty_a_multiply_divide_fractions.py
    Coding for username in multiplying and dividing fractions.

--- multiplying_dividing_fractions/lafferty_a_multiply_divide_fractions.py
+++ multiplying_dividing_fractions/lafferty_a_multiply_divide_fractions.py
@@ -2,3 +2,5 @@
 
 print("Hello, My name is Mathemater and i will multiply and divide fractions.\n")
 print("In order to multipy and divide fractions I need to know the numerator an the denominator of both fractions.")
+user_name = input("What is your name? [Type your name and press ENTER.]\n")
+print("Hello, ", user_name," how are you today?")
