d6d4744f6eab4d2e84b73b1c3aaa5ef92bed1011
Zani <s11673969@students.duvalschools.org>
2019-05-07 12:15:00 -0400

    Update andi_zani_multiplying_dividing_fractions.py

--- andi_zani_multiplying_dividing_fractions.py
+++ andi_zani_multiplying_dividing_fractions.py
@@ -1,1 +1,4 @@
-# Andi Z., Mutiplying and Dividing 05/06/19
+# Andi Z., Mutiplying and Dividing 05/06/19 
+
+print('Hello! My Name is Soviet Sausage I am here to help you multiply and divide fraction')
+print("In order to multiply or divde fractions,you must give me the [...] and [...] of the fractions.|")
