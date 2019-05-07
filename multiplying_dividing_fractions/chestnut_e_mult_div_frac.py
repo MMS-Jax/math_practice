543b68a504cec9f7eb53355f900ea3115ffb24b0
realistik702 <s12441838@students.duvalschools.org>
2019-05-07 12:25:25 -0400

    Update chestnut_e_mult_div_frac.py

--- multiplying_dividing_fractions/chestnut_e_mult_div_frac.py
+++ multiplying_dividing_fractions/chestnut_e_mult_div_frac.py
@@ -1,1 +1,4 @@
 # Chestnut E., Multiplying and Dividing Fractions 05/06/2019
+
+print("Hello! My name is Mathulator 5000 and I will help you with your math!\n")
+print("In order to multiply and divide you fractions you need to know the numerator and the denominator.")
