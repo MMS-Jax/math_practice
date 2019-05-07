68d9c77b1910f1e2fd8a56ea546ad172d6be21ff
s11649159 <49206466+s11649159@users.noreply.github.com>
2019-05-07 12:37:00 -0400

    Added user_name part of code
    added "what is your user_name" to line 4 of coding, and i am curently iignoring line 7

--- multiplying_dividing_fractions/ben_h_multiply_divide_fractions.py
+++ multiplying_dividing_fractions/ben_h_multiply_divide_fractions.py
@@ -1,4 +1,7 @@
 # Ben H., Multiplying and Divivding Fractions, 05/06/2019
 
-print("Hello! My name is Fraction Master- Over 9000. I will ultiply and divide fractions for you. \n ")
+print("Hello! My name is Fraction Master- Over 9000. I will ultiply and divide fractions for you!\n ")
+user_name = input("What is your name is your name? Ben Hipp \n")
+print("Hello, ",  user_name," how are you today?\n")
+
 print ("In order to Multiply or Divivde fractions, I need to know the numerator and denomenator for both fractions.")
