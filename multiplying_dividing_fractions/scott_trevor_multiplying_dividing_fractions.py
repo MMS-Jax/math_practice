51de7d823cc9d25fd1dd91f837f7ffddee3aa29c
LemonHead9906 <49240006+LemonHead9906@users.noreply.github.com>
2019-05-07 15:45:07 -0400

    made username and to type your name
    ask what is your name and put type your name

--- multiplying_dividing_fractions/scott_trevor_multiplying_dividing_fractions.py
+++ multiplying_dividing_fractions/scott_trevor_multiplying_dividing_fractions.py
@@ -1,9 +1,4 @@
 # Multiplying & Dividing Trevor.S 5/06/2019
 
 print("welcome to fraction bot 9000! I can multiply and divide fractions. \n")
-print("In order to multiply or divide fractions we need to know the [....] and [....].\n")
-
-numerator0 = 0
-numerator1 = 0
-denominator0 = 1
-denominator1 = 2
+user_name = input("what is your name? [Type your name and press ENTER.\n")
