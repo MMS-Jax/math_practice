0b4f20ed76285ab2f0c9c25a8e27d15174e995b2
Shay <49175183+this-mailbox-is-mine@users.noreply.github.com>
2019-05-07 12:35:44 -0400

    Added user_name variable and input() line.
    Added two lines of code to ask for the user's name, saved it to the variable usng input() and then printed the name back on the screen.

--- multiplying_dividing_fractions/whitten_s_multiply_divide_fractions_0.2.py
+++ multiplying_dividing_fractions/whitten_s_multiply_divide_fractions_0.2.py
@@ -2,6 +2,6 @@
 
 print("Hello, my name is Dr. Phil the Fraction God, and I was made to consult you on multiplying and dividing fractions. Nice to meet you.\n ")
 print("In order to multiply and divide fractions for you, sir or madam, I need to know both the numerator and denominator of your fractions.\n")
-user_name = input("First, I need to know what your name is. Please type it into the space provided. [Type your username and press ENTER.] ")
+user_name = input("First, I need to know what your name is. Please type it into the space provided. [Type your username and press ENTER.] \n")
 print("Welcome to the Ranch,", user_name,)
 
