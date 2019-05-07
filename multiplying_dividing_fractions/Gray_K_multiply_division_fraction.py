d833f91ecf9f655690853c377bf0ea7481818a58
Kennedy122 <49565896+Kennedy122@users.noreply.github.com>
2019-05-07 12:12:42 -0400

    Update Gray_K_multiply_division_fraction.py
    Multiplying and dividing fractions

--- multiplying_dividing_fractions/Gray_K_multiply_division_fraction.py
+++ multiplying_dividing_fractions/Gray_K_multiply_division_fraction.py
@@ -1,3 +1,4 @@
 # Ryan K., Multiplying and Dividing Fractions, 05/05/2019
 
-print("hello! My Name Is MathBotand I Will multiply and divide fractions.")
+print("hello! My Name Is MathBotand I Will multiply and divide fractions.\n")
+Print("In Order to multiply fractions, you need too  knwthe numerator and denomerator of the fractions.")
