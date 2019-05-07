05f809991c361d75b2cad77c8723e208d4eaa258
LemonHead9906 <49240006+LemonHead9906@users.noreply.github.com>
2019-05-07 15:56:19 -0400

    entered username and how are yuo today
    entered usename trevor and it worked perfectly

--- multiplying_dividing_fractions/scott_trevor_multiplying_dividing_fractions.py
+++ multiplying_dividing_fractions/scott_trevor_multiplying_dividing_fractions.py
@@ -1,4 +1,5 @@
 # Multiplying & Dividing Trevor.S 5/06/2019
 
 print("welcome to fraction bot 9000! I can multiply and divide fractions. \n")
-user_name = input("what is your name? [Type your name and press ENTER.\n")
+user_name = input("what is your name? [Type your name and press ENTER.]\n")
+print("Hello, ", user_name," how are you today?")
