5115dc2ebd4c6aaa2147c9f94e927332e3127818
DGK-Jayden <49407930+DGK-Jayden@users.noreply.github.com>
2019-05-07 12:46:47 -0400

    added user_nsmr input variable
    now when you type your name and press enter, a text pops upp and asks you how your day was.

--- multiplying_dividing_fractions/wilder_jayden_multiplying_dividing_fractions_0.2.py
+++ multiplying_dividing_fractions/wilder_jayden_multiplying_dividing_fractions_0.2.py
@@ -3,4 +3,4 @@
 print("Mr. Thick is going to destroy the earth, we need to use math to help bepis protect the people of shell city!\n")
 print("In order to save shell city, bepis will need to know the numerator and denominator for the fractions.\n")
 user_name = input("What is your name? [Type your name amd press ENTER.]\n")
-print("Hello ", user_name, " How are you today?")
+print("Hello ", user_name," How are you today?\n")
