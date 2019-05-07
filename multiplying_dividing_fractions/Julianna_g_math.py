babb6da404edcda58a1c648819ad9a527420bca8
Jewel2143 <s11586674@student.duvalschools.org>
2019-05-07 12:14:02 -0400

    ignore this
    jusy ignore

--- multiplying_dividing_fractions/Julianna_g_math.py
+++ multiplying_dividing_fractions/Julianna_g_math.py
@@ -1,3 +1,4 @@
 # Julianna G., Muliplying and Dividing fractions, 05/06/2019
 
-print("hi, this is for multiplying and dividing fractions")
+print("hi, this is for multiplying and dividing fractions\n")
+print("in order for me to be able to multiply and dividing fractions i need to know the numerator and denoiminatorfor both\n")
