c47ff03aa37866382c16295c13204ba1818108e3
avrielle <s11650678@students.duvalschools.org>
2019-05-07 16:07:05 -0400

    added variables
    added variables for adding and subtracting

---
+++ multiplying_dividing_fractions/adding_and_subtracting_numbers 0.3.py
@@ -0,0 +1,15 @@
+# Adding And Subtracting Whole Numbers 5/06/19 Avrielle G.
+
+print("welcome to Math-Ro-Bot I can Add and Subtract Whole Numbers for you!\n")
+user_name = input("What's your name? [Type your name and press ENTER.]\n")
+print("Hello", user_name, "How is your day going?\n")
+
+print("For this area, I need to know the numerator and the denomerator so i can add or subtract them.\n")
+
+# variables for adding and subtracting 0
+numerator = 0
+denomerator = 0
+
+# variables for adding and subtracting 1
+numerator1 = 0
+denomerator1 = 0
