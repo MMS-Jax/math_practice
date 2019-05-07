de9e370d5773d5cefd164e0d28a27dc73e4dd032
CheeseWife <49404967+CheeseWife@users.noreply.github.com>
2019-05-07 10:15:47 -0400

    new and improved fraction code
    still cant do mathbut the robot will know ask and print your fractions

--- leytem_e_multiplying_fractions.py
+++ leytem_e_multiplying_fractions.py
@@ -3,5 +3,11 @@
 print("Give me fractions to multiply, if you dare.\n")
 print("if you are up to the challenge you must give me the numerator and denominator for each desired fraction.")
 
-numerator0 = 0
-numerator1 = 0
+numerator0 = int(input("What is the first numerator? [Type number and press enter.]"))
+denominator0 = int(input("What is the first denominator? [Type number and press enter.]"))
+print("Then your first fraction must be",numerator0,"/",denominator0,".")
+
+numerator1 = int(input("What is the second numerator? [Type number and press enter.]"))
+denominator1 = int(input("What is the second denominator?[Type number and press enter.]"))
+print("Then your second fraction must be",numerator1,"/",denominator1,".")
+#where mutiplying/dividing happens
