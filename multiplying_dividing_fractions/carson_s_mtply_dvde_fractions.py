4fc463fa2e5206522b1baa573704149a33216500
Cavespider386 <49206765+Cavespider386@users.noreply.github.com>
2019-05-07 12:12:19 -0400

    Update carson_s_mtply_dvde_fractions.py

--- multiplying_dividing_fractions/carson_s_mtply_dvde_fractions.py
+++ multiplying_dividing_fractions/carson_s_mtply_dvde_fractions.py
@@ -1,3 +1,14 @@
 # Carson S., Multiply and Divide Fractions, 05/06/2019 
 
-print("Hello, my name is MathHomeworkBotwork. My Master sent me to help you with your math.  I can MULTIPLY and DIVIDE FRACTIOMANIANS for you, or on your planet, they call them fractions.")
+print("Hello, my name is MathHomeworkBotwork. My Master sent me to help you with your math.  I can MULTIPLY and DIVIDE FRACTIOMANIANS for you, or on your planet, they call them fractions. \n")
+print("First I will need some information. \n")
+print("Input the first numerator!")
+numerator1 = input()
+print("And now the second numerator please.")
+numerator2 = input()
+print("Now the denominator for the first numerator.")
+denominator1 = input()
+print("And the denominator for the second.")
+denominator2 = input()
+playerAnswer = numerator1/denominator1 * numerator2/denominator2
+print("The Answer is!")
