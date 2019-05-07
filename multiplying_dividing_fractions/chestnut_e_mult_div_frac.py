cf64af36df4da0fe4260ac81edd4649f57f33e0b
realistik702 <s12441838@students.duvalschools.org>
2019-05-07 12:44:48 -0400

    Added user_name variable and input() line.
    Added 2 lines of code to ask for the user's name, save it to a variable using input() and then printing the name back on the screen.

--- multiplying_dividing_fractions/chestnut_e_mult_div_frac.py
+++ multiplying_dividing_fractions/chestnut_e_mult_div_frac.py
@@ -2,3 +2,5 @@
 
 print("Hello! My name is Mathulator 5000 and I will help you with your math!\n")
 print("In order to multiply and divide you fractions you need to know the numerator and the denominator.")
+user_name = input("What is your name? [Type your name and press ENTER.]\n")
+print("Hello,",user_name,"how are you today?\n")
