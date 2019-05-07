d688960c53f18c9bb0c733eb0326c16c03e5a2a0
Cavespider386 <49206765+Cavespider386@users.noreply.github.com>
2019-05-07 12:37:59 -0400

    Create carson_s_mtply_dvde_fractions0.3.py

---
+++ multiplying_dividing_fractions/carson_s_mtply_dvde_fractions0.3.py
@@ -0,0 +1,16 @@
+# Carson S., Multiply and Divide Fractions, 05/06/2019 Ver 0.3
+
+print("Hello, my name is MathHomeworkBotwork. My Master sent me to help you with your math.  I can MULTIPLY and DIVIDE FRACTIOMANIANS for you, or on your planet, they call them fractions. \n")
+user_name = input("What is your name? Type your name and press enter to continue. \n")
+print("Nice to meet you,", user_name , "\n")
+
+print("MathHomeworkBotwork OVERLOAD: REBOOT \n")
+print("Okay, I am better now. \n")
+
+# Variables for fraction 0.
+numerator0 = 0
+denominator0 = 0
+
+# Variables for fraction 1.
+numerator1 = 0
+denominator1 = 0
