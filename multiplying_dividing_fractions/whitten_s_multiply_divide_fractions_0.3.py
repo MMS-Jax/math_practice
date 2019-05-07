e7ff7df97ef6f555d96c11a86ad72435228724df
Shay <49175183+this-mailbox-is-mine@users.noreply.github.com>
2019-05-07 12:44:14 -0400

    Declared variables for fractions.
    Added the numerator and denominator values for the two fractions.

---
+++ multiplying_dividing_fractions/whitten_s_multiply_divide_fractions_0.3.py
@@ -0,0 +1,17 @@
+# Sheyna W., Multiplying and Dividing Fractions, 05/06/19, Version 0.3
+
+print("Hello, my name is Dr. Phil the Fraction God, and I was made to consult you on multiplying and dividing fractions. Nice to meet you.\n ")
+user_name = input("First, I need to know what your name is. Please type it into the space provided. [Type your username and press ENTER.] \n")
+print("Welcome to the Ranch,", user_name,)
+
+print("In order to multiply and divide fractions for you, sir or madam, I need to know both the numerator and denominator of your fractions.\n")
+
+# Variables for Fraction 0.
+numerator0 = 0
+denominator0 = 0
+# Variables for Fraction 1.
+numerator1 = 0
+denominator1 = 0
+
+
+
