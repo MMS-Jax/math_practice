9d05cb8b7de165be9e3c88b758e3004f6c5d588e
imaginewagons19 <49206729+imaginewagons19@users.noreply.github.com>
2019-05-07 12:33:59 -0400

    Update josephine_y_muliplication_division_fractions.py
    added variables for faction 1 and 0

--- multiplying_dividing_fractions/josephine_y_muliplication_division_fractions.py
+++ multiplying_dividing_fractions/josephine_y_muliplication_division_fractions.py
@@ -2,3 +2,15 @@
 
 print ("Hello! My name is Barry B. Benson and i will help you muliply and divide fractions.\n")
 print('In order to muliply or divide fractions you will need to input your numerator [...] and denominator [...]')
+
+user_name =('What is your name? [type in your name and press ENTER] \n')
+print("Hello", user_name, "How are you today?") 
+
+print("For this program, I need to know the numerator and denominator for both fractions.\n")
+
+# Variables for fraction 0
+numerator0=0
+denominator0=0
+# Variables for fraction 1
+numerator1=0
+denominator1=0
