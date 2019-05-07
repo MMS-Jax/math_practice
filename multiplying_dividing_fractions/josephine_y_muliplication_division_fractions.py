8cebfc0b814a72b32f46f97a3e4d55e00bf300b9
imaginewagons19 <49206729+imaginewagons19@users.noreply.github.com>
2019-05-07 12:10:24 -0400

    Update josephine_y_muliplication_division_fractions.py
    added some instructions

--- multiplying_dividing_fractions/josephine_y_muliplication_division_fractions.py
+++ multiplying_dividing_fractions/josephine_y_muliplication_division_fractions.py
@@ -1,1 +1,4 @@
 # Josephine Y. multiplying and dividing fractions, 05/06/19
+
+print ("Hello! My name is Barry B. Benson and i will help you muliply and divide fractions.\n")
+print('In order to muliply or divide fractions you will need to input your numerator [...] and denominator [...]')
