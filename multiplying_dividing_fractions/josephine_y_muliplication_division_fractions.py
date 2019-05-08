a4f80be12a5b660a72673fc9c858994054a92874
imaginewagons19 <49206729+imaginewagons19@users.noreply.github.com>
2019-05-08 12:29:25 -0400

    fixed some bugs
    noticed it wasn't running right so i fixed the bugs and mispells

--- multiplying_dividing_fractions/josephine_y_muliplication_division_fractions.py
+++ multiplying_dividing_fractions/josephine_y_muliplication_division_fractions.py
@@ -1,19 +1,27 @@
 # Josephine Y. multiplying and dividing fractions, 05/06/19
 
 print ("Hello! My name is Barry B. Benson and i will help you muliply and divide fractions.\n")
-print('In order to muliply or divide fractions you will need to input your numerator [...] and denominator [...]')
+print('In order to muliply or divide fractions you will need to input your numerator and denominator')
 
-user_name =('What is your name? [type in your name and press ENTER] \n')
+user_name = input ('What is your name? [type in your name and press ENTER] \n')
 print("Hello", user_name, "How are you today?") 
 
 print("For this program, I need to know the numerator and denominator for both fractions.\n")
 
 # Variables for fraction 0
-numerator0=0
-denominator0=0
+numerator0=1
+denominator0=2
 print("The first fraction is", numerator0, "/", denominator0, ".\n")
 
 # Variables for fraction 1
-numerator1=0
-denominator1=0
+numerator1=1
+denominator1=3
 print("The second fraction is", numerator1, "/", denominator1, ".\n")
+
+print("When mulitplying fractions you muliply the two numerators together. \n")
+print("Then you will multiply the two denominators together. \n")
+
+new_numerator = numerator0 * numerator1
+new_denominator = denominator0 * denominator1
+
+print("The new fraction is", new_numerator, "/", new_denominator, ".\n")
