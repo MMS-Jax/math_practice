260f32036907680f3514f0265d41806b0d760a84
Botolino <s12701736@students.duvalschools.org>
2019-05-08 12:43:56 -0400

    did the thing for version 4
    added the print yes

--- multiplying_dividing_fractions/tyler_botolino_multiplying_and_dividing_fractions_0.4.py
+++ multiplying_dividing_fractions/tyler_botolino_multiplying_and_dividing_fractions_0.4.py
@@ -1,4 +1,4 @@
- # Tyler B., Multiplying and Dividing Fractions, 05/06/2019, version 0.3
+ # Tyler B., Multiplying and Dividing Fractions, 05/06/2019, version 0.4
 
 print("Hello! My name is FractionBot-9000 here to help you multiply and divide fractions.\n")
 user_name = input("What is your name? [Type your name and press ENTER.]\n")
