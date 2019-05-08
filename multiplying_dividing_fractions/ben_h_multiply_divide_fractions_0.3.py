2ea0555d26aa1fb08b9d9f49a8171851d532bfd7
s11649159 <49206466+s11649159@users.noreply.github.com>
2019-05-08 12:41:42 -0400

    added to variables for the fractions.
    put to fractions, fraction 0 and fraction 1.

---
+++ multiplying_dividing_fractions/ben_h_multiply_divide_fractions_0.3.py
@@ -0,0 +1,14 @@
+# Ben H., Multiplying and Divivding Fractions, 05/06/2019, version 0.2
+
+print("Hello! My name is Fraction Master- Over 9000. I will ultiply and divide fractions for you!\n ")
+user_name = input("What is your name is your name? Ben Hipp \n")
+print("Hello, ",  user_name,"how are you today?\n")
+
+print("For this program, I need to know the numerator and the denominator for both fractions. \n")
+ 
+# Variables for fraction 0.
+numerator0 = 0
+denominator0 = 0
+# Varables for fraction 1.
+numerator1 = 0
+denominator1 = 0
