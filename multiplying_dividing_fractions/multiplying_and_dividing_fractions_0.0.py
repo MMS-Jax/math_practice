2ef162a09f2c7f4bc0a5067a9f16d572ce553e07
Elon Musk <49207612+youngdashboi@users.noreply.github.com>
2019-05-08 14:02:24 -0400

    First version of program
    Created the first code for the fraction programs.

---
+++ multiplying_dividing_fractions/multiplying_and_dividing_fractions_0.0.py
@@ -0,0 +1,5 @@
+# Powell C., Multiplying and Divide Fractions, 05/06/2019
+
+print("Hello, my name is Youngmathboi. I will multiply and divide fractions for you.\n")
+user_name = input("What is your name? [Type your name and press ENTER].\n")
+print("In order for me to do that i need to know the numerator and the denominator of the equation.\n")
