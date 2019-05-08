b28963718dea9b616f87f8647fbcff8b1b376fbf
Jayla00 <49207622+Jayla00@users.noreply.github.com>
2019-05-08 14:20:31 -0400

    Update Multiplying and Dividing factions
    Commited to github.

--- multiplying_dividing_fractions/multiplying_dividing_fractions_0.2.py
+++ multiplying_dividing_fractions/multiplying_dividing_fractions_0.2.py
@@ -1,5 +1,5 @@
-# Ryan K., Multiplying and Dividing Fractions, 05/06/2019, version 0.2
+#Jayla L., Mulitpying and Dividing Fractions, 05/07/19,   Version 0.2  
 
-print("Hello, welcome to Fraction-Bot 9000.   I can multiply and divide fractions for you!\n")
-user_name = input("What is your name? [Type your name and press ENTER.]\n")
-print("Hello,", user_name,"how are you today?\n")
+print("Hello, welcome to Fraction- Bot 9000.   I can multiply and divide factions for you!\n")
+user_name = input ("What is your name? [Type your name here and press ENTER.]\n")
+print("Hello",user_name,"How are you today?")
