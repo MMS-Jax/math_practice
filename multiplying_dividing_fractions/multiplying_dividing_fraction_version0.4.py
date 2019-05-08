6fe2f714de567d5bf6d56b88120126986dc75f22
NicD101 <49321370+NicD101@users.noreply.github.com>
2019-05-08 11:05:24 -0400

    Create multiplying_dividing_fraction_version0.4.py
    Prints the first two fraction Version 4

---
+++ multiplying_dividing_fractions/multiplying_dividing_fraction_version0.4.py
@@ -0,0 +1,17 @@
+# Nicolas D., Multiplying and Dividing Fraction, version 0.4
+
+print("This program is used to multiply and divide fractions.\n")
+
+user_name = input("What is your name? [Type your name and press ENTER.]\n")
+print("Hello",user_name,"how are you today")
+
+print("For this program, I need the numerator and the denominator of both fractions.\n")
+# Variables for fraction 0
+numerator0 = 0
+denominator0 = 0
+print("The first fraction is", numerator0,"/",denominator0,".\n")
+
+# Variables for fraction 1
+numerator1 = 1
+denominator1 = 1
+print("The second fraction is", numerator1,"/", denominator1,".\n")
