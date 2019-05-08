c23e355257afed93a2140540aa7b2ac183e827a1
s11177524 <49206539+s11177524@users.noreply.github.com>
2019-05-08 12:47:16 -0400

    i added more code

--- multiplying_dividing_fractions/multiplying_and_dividing_fractions.py
+++ multiplying_dividing_fractions/multiplying_and_dividing_fractions.py
@@ -1,4 +1,17 @@
 #s11177524, multiplying and dividing fractions, 5/06/19
 
 print("Hello, I'm Dean. I will help you multiply and divide fractions.\n")
+user_name = input("What is your name? [type your name and press ENTER]")
+print("Hello,", user_name,"how are you today?\n")
+
 print("In order to multiply or divide fractions, you need to know the numerator and the denominator of each fraction before you begin.")
+
+# Variables for fraction 0
+numerator0 = 0
+denominator0 = 0
+print("The first fraction is", numerator0,"/", denominator0,".\n")
+
+# Variables for fraction 1
+numerator1 = 0
+denominator1 = 0
+print("The first fraction is", numerator0,"/", denominator0,".\n")
