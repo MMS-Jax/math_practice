d9cc290ec3ebb0ac59e020ded9a882fd62cad1f8
Jewel2143 <s11586674@student.duvalschools.org>
2019-05-08 12:20:33 -0400

    Julianna_g_math
    denomirator/numerator

--- multiplying_dividing_fractions/Julianna_g_math.py
+++ multiplying_dividing_fractions/Julianna_g_math.py
@@ -1,4 +1,15 @@
 # Julianna G., Muliplying and Dividing fractions, 05/06/2019
 
-print("hi, this is for multiplying and dividing fractions\n")
-print("in order for me to be able to multiply and dividing fractions i need to know the numerator and denoiminatorfor both\n")
+print("Hi, this is for multiplying and dividing fractions\n")
+user_name = input("What is your name? [Type you name and press enter.\n ")
+print("Hello", user_name,"how are you today")
+
+print("For this program, I need to know the numertor and the demominator for both fractions.\n")
+
+# Variables for fraction 0.
+numertor0 = 0
+denomnator0 = 0
+# Variables for fraction 1.
+numerator1 = 0
+denominator1 = 0
+
