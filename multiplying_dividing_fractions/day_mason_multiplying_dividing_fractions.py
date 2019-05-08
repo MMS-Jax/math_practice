dccd2be875bc8171dc727c85d05b0b77a6d71dfd
HeWasNumber1 <49246121+HeWasNumber1@users.noreply.github.com>
2019-05-08 12:27:35 -0400

    added first print string
    printed some basic instructions for the user

--- multiplying_dividing_fractions/day_mason_multiplying_dividing_fractions.py
+++ multiplying_dividing_fractions/day_mason_multiplying_dividing_fractions.py
@@ -1,4 +1,4 @@
-# Mason D., Multiplying and Dividing Fractions, 5/6/2019
+# Mason D., Multiplying and Dividing Fractions, 5/6/2019, version 0.1
 
 print("Hello! My name is lil Fraction- Over 9000. I Will Multiply and Divide your Fractions for You \n")
 print("In order for us to save exponent land- You need to tell me the numerator and denomenator for both fractions \n")
