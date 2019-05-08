bf664ff775f17d92e5b4542fee89c5d3734f5a90
EthanDaSavage <s11279171@student.duvalschools.org>
2019-05-08 10:36:24 -0400

    Added first print() string
    Printed a basic string of instructions for the user.

---
+++ multiplying_dividing_fractions/Multiplying_dividing_factions_0.1.py
@@ -0,0 +1,3 @@
+# Ethan B., Multiplying and Dividing Fractions, 05/06/2019, version 0.1
+
+print("Hello, welcome to the Frction-Bomber 3000.   I will multiply and divide those hard fractions for you!\n")
