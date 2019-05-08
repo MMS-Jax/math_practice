66f5e96fa18d3ee49d19f1768e6484f62a832cdd
LilFoxy1983 <49206772+LilFoxy1983@users.noreply.github.com>
2019-05-08 12:32:48 -0400

    Declared variables for the fractions
    Added the numberator and denominator variables for the two fractions

---
+++ buss_s_multiply_divide_fractions_0.3.py
@@ -0,0 +1,14 @@
+# Serinity B., Multiplying and divinding Fractions, 05/06/2019 version 0.3
+
+print("Hello, welcome to MathHelper. I can multiply and divide fractions for you!\n")
+user_name = input("What is your name? [Type your name and press ENTER]\n")
+print("Hello", user_name,"how are you today?\n")
+
+print("For this program, i need to know the numerator and the denominator for both fractions.\n")
+
+3 Variables for fraction 0. 
+numberatorA = 0
+denominatorB = 0
+# Variables for fraction 1.
+numberator1 = 0
+denominator1 = 0
