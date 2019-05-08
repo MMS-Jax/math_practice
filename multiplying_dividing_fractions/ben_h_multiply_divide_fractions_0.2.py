a2c08859c25c1fb376e0637eb30e51c6b3fef2ef
s11649159 <49206466+s11649159@users.noreply.github.com>
2019-05-08 12:33:53 -0400

    added user_name version 0.1 and 0.2
    Added line 5, user name commitment and saved as version 0.2

--- multiplying_dividing_fractions/ben_h_multiply_divide_fractions_0.2.py
+++ multiplying_dividing_fractions/ben_h_multiply_divide_fractions_0.2.py
@@ -1,4 +1,4 @@
-# Ben H., Multiplying and Divivding Fractions, 05/06/2019, version 0.1
+# Ben H., Multiplying and Divivding Fractions, 05/06/2019, version 0.2
 
 print("Hello! My name is Fraction Master- Over 9000. I will ultiply and divide fractions for you!\n ")
 user_name = input("What is your name is your name? Ben Hipp \n")
