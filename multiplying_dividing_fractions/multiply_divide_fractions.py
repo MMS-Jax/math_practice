2a2926035609d7ed40b04a9b56ecee632a03374f
Baretus <s11651403@students.duvalschools.org>
2019-05-08 13:54:59 -0400

    first print
    printed basic instructions for the user.

---
+++ multiplying_dividing_fractions/multiply_divide_fractions.py
@@ -0,0 +1,1 @@
+# Haris H., Multiplying and Dividing Fractions, 05/07/19, version 0.0
---
+++ multiplying_dividing_fractions/multiply_divide_fractions0.1.py
@@ -0,0 +1,3 @@
+# Haris H., Multiplying and Dividing Fractions, 05/07/19, version 0.0
+
+print("Hello, welcome to Beatit Bot 9000. I can multiply and divide fractions for you!\n")
