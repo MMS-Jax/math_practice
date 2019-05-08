d0849fdac33576bf199320eb970acd76b9441d1b
Ashley L <s11544541@student.duvalschools.org>
2019-05-08 12:35:27 -0400

    Added user_name variable and input0 line.
    Added two lines of code to ask for the user's name, save it to a variable using input0 and then oprinting the name back onto the screen.

--- multiplying_dividing_fractions/lafferty_a_multiply_divide_fractions_0.2.py
+++ multiplying_dividing_fractions/lafferty_a_multiply_divide_fractions_0.2.py
@@ -1,4 +1,4 @@
-#Ashley L., Multiplying and Dividing Fractions, 05/06/2019
+#Ashley L., Multiplying and Dividing Fractions, 05/06/2019, version 0.2
 
 print("Hello, My name is Mathemater and i will multiply and divide fractions.\n")
 print("In order to multipy and divide fractions I need to know the numerator an the denominator of both fractions.")
