ecaad57346d7a305fe2f42a5b7a9c7e010d4e3c2
isabella1231 <49206750+isabella1231@users.noreply.github.com>
2019-05-08 12:29:17 -0400

    added user_name variable and input() line.
    added two lines of code to ask for the user's name saving it to a variable using input() and then printing the name back on the screen

--- isabella_S._multiplying_and_dividing_fractions_version_0.2.py
+++ isabella_S._multiplying_and_dividing_fractions_version_0.2.py
@@ -1,5 +1,5 @@
-#isabella S. , multiplying and dividing fractions 05/06/2019 version 0.1
+#isabella S. , multiplying and dividing fractions 05/06/2019 version 0.2
 
 print("hello! my name is super math bot i will multiply and divide fractions for you!\n")
 user_name = input("what is your name? [Type your name and press ENTER.]/n")
-print("Hello,",user_name,"how are you today?" )
+print("Hello,",user_name,"how are you today?")
