f8fa9c91f7846e2e5b5036b7e60de04417a26ecc
deangelaxo <49207879+deangelaxo@users.noreply.github.com>
2019-05-08 14:21:48 -0400

    started program
    did print commands and saved to github.

--- multiplying_dividing_fractions/multiplying_dividing_fractions_0.1.py
+++ multiplying_dividing_fractions/multiplying_dividing_fractions_0.1.py
@@ -1,3 +1,5 @@
 # Deangela C., Multiplyig and Dividing Fractions, 5/07/19, version 0.1
 
-print("Hi, I am working on multiplying and dividing fractions./n ")
+print("Hi, I am working on multiplying and dividing fractions.\n ")
+user_name = input("What is your name? [Type your name and press ENTER.]\n")
+print("Hello, My name is Deangela", user_name,"how was your day?")
--- multiplying_dividing_fractions/multiplying_dividing_fractions_0.2.py
+++ multiplying_dividing_fractions/multiplying_dividing_fractions_0.2.py
@@ -1,5 +1,5 @@
-# Ryan K., Multiplying and Dividing Fractions, 05/06/2019, version 0.2
+# Deangela C., Multiplyig and Dividing Fractions, 5/07/19, version 0.2
 
-print("Hello, welcome to Fraction-Bot 9000.   I can multiply and divide fractions for you!\n")
+print("Hi, I am working on multiplying and dividing fractions.\n ")
 user_name = input("What is your name? [Type your name and press ENTER.]\n")
-print("Hello,", user_name,"how are you today?\n")
+print("Hello, My name is Deangela", user_name,"how was your day?")
