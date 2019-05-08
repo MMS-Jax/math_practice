4959ff3973baa8449e380c6d7ee79c172aa39338
fattocattos <49206622+fattocattos@users.noreply.github.com>
2019-05-08 12:32:31 -0400

    Declared Variables for the fractions
    I added the Numerator and the Denominator for the two fractions.

---
+++ multiplying_dividing_fractions/davis_k_multiplying_dividing_fractions_0.3.py
@@ -0,0 +1,14 @@
+# Kailey D., Multiplying and Dividing Fractions, 05/06/2019, version 0.3
+
+print("Hello! My name is FattoCattoPugFraction and I will multiply and Divide Fractions for you.\n")
+user_name = input("What is your name? [ Type your name and press ENTER.]\n")
+print("Hello, ", user_name," how are you today?\n")
+
+print("In order for me to find the answer, I need you to tell me the numerator and the denominator for both fraction.\n")
+
+# Variables for fraction 0.
+numerator0 = 0
+denominator0 = 0
+# Variables for fraction 1.
+numerator1 = 0
+denominator1 = 0
