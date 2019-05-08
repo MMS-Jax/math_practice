9008d9e416f8311557187234e5796d3a91439119
NicD101 <49321370+NicD101@users.noreply.github.com>
2019-05-08 10:44:52 -0400

    Create multiplying_dividing_fraction_version0.2.py
    Added user_name variable

---
+++ multiplying_dividing_fractions/multiplying_dividing_fraction_version0.2.py
@@ -0,0 +1,6 @@
+# Nicolas D., Multiplying and Dividing Fraction, version 0.2
+
+print("This program is used to multiply and divide fractions.\n")
+
+user_name = input("What is your name? [Type your name and press ENTER.]\n")
+print("Hello",user_name,"how are you today")
