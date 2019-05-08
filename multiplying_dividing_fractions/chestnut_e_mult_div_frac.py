de95e58ecae9e1c7db19b22af1c73c9f1e40da3b
realistik702 <s12441838@students.duvalschools.org>
2019-05-08 12:20:26 -0400

    Declared variables for the fractions.
    Added the numerator and denominator values for the two fractions.

--- multiplying_dividing_fractions/chestnut_e_mult_div_frac.py
+++ multiplying_dividing_fractions/chestnut_e_mult_div_frac.py
@@ -1,6 +1,14 @@
 # Chestnut E., Multiplying and Dividing Fractions 05/06/2019
 
 print("Hello! My name is Mathulator 5000 and I will help you with your math!\n")
-print("In order to multiply and divide you fractions you need to know the numerator and the denominator.")
 user_name = input("What is your name? [Type your name and press ENTER.]\n")
 print("Hello,",user_name,"how are you today?\n")
+
+print("For this program, I need to know the numerrator and denominator for both fractions.\n")
+
+# Variables for fraction 0
+numerator0 = 0
+denominator0 = 0
+# Variables for fraction 1
+numerator1 = 0
+denominator1 = 0
