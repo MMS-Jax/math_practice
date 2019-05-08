f3e48ec6936ff8d669020a83a639393adcc27439
Hobensack <s10676963@students.duvalschools.org>
2019-05-08 10:47:13 -0400

    Added user_name and input line
    Added twop lines of code to ask the user's name ,save it to a variable using input and then printing the name back on the screen.

--- multiplying_dividing_fractions/Multiply_Dividing_Fractions_0.2.py
+++ multiplying_dividing_fractions/Multiply_Dividing_Fractions_0.2.py
@@ -1,4 +1,4 @@
-#Luke H.,Multiplying and Dividing Fractions,5/7/19,version 0.1
+#Luke H.,Multiplying and Dividing Fractions,5/7/19,version 0.2
 
 print("Hello! I am Mr.Fraction. I can multiply and divide fractions for you!\n")
 user_name = input("What is your name?[Typle your name and press ENTER.]\n")
