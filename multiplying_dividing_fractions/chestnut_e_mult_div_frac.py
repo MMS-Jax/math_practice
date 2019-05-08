1f3876ebb963be4fb75cac82be7b6ff7e11c68a0
realistik702 <s12441838@students.duvalschools.org>
2019-05-08 12:45:26 -0400

    Added variables for the new fraction.
    Declared new_numerator and new_denominator, also wrote the code to multiply the two fractions together.

--- multiplying_dividing_fractions/chestnut_e_mult_div_frac.py
+++ multiplying_dividing_fractions/chestnut_e_mult_div_frac.py
@@ -4,7 +4,7 @@ print("Hello! My name is Mathulator 5000 and I will help you with your math!\n")
 user_name = input("What is your name? [Type your name and press ENTER.]\n")
 print("Hello,",user_name,"how are you today?\n")
 
-print("For this program, I need to know the numerrator and denominator for both fractions.\n")
+print("For this program, I need to know the numerator and denominator for both fractions.\n")
 
 # Variables for fraction 0
 numerator0 = 0
@@ -15,3 +15,11 @@ print("The first fraction is", numerator0,"/", denominator0,".\n")
 numerator1 = 0
 denominator1 = 0
 print("The second fraction is", numerator1,"/", denominator1,".\n")
+
+print("When multiplying fractions you multiply the two numerators together.\n")
+print("Then you will multiply the two denominators together.\n")
+
+new_numerator = numerator0 * numerator1
+new_denominator = denominator0 * denominator1
+
+print("The new fraction is", new_numerator,"/", new_denominator,".\n")
