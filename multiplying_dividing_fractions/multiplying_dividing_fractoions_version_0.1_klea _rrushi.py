be705f5b52b33491ba197cdc81cb598d769c8a31
klea <s12366969@students.duvalschools.org>
2019-05-08 11:07:16 -0400

    added user_name variable and input0line

--- multiplying_dividing_fractions/multiplying_dividing_fractoions_version_0.1_klea _rrushi.py
+++ multiplying_dividing_fractions/multiplying_dividing_fractoions_version_0.1_klea _rrushi.py
@@ -1,3 +1,7 @@
 # Klea R., Multiplying and Dividing Fractions, 05/06/19,version 0.1
 
-print("hello welcome to fraction-bot 9000.  I will multiply and divide fractions for you !\n")
+print("hello, welcome to fraction-bot 9000.  I can  multiply and divide fractions for you !\n")
+user_name=input("what is your name?[type yuor name and press ENTER.\n )
+print("hello,",user_name ," how are you today?")
+
+
