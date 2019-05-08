3f6db67005d0d28fce78dd0439c476b42f7b385a
Dustin <s11314051@students.duvalschools.org>
2019-05-08 15:49:12 -0400

    added numerator and denominator
    added numerator and denominator

--- multiplying_dividing_fractions/Lima_dustin_multidivide.py
+++ multiplying_dividing_fractions/Lima_dustin_multidivide.py
@@ -1,1 +1,11 @@
 #Multiply the horse by Dustin.L 5/06/19
+
+print("Hello there i am Math-Bro 2.0! im here to teach you how to multiply and divide fractions.\n")
+print("in order to multiply and divide fractions we must need to find the numerator and the denominator.\n")
+
+numerator0 = 0
+numerator1 = 0
+
+denominator0 = 0
+denominator1 = 0
+print("The first numerator is":numerator0".")
