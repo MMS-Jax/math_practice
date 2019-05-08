db04b9b2e96897fd3d8d75fa1a60e08d20768276
Zephyrhills <49448677+Zephyrhills@users.noreply.github.com>
2019-05-08 12:37:55 -0400

    declared variablesfor the fractions.
    Added the numerator and denominator values.

--- multiplying_dividing_fractions/depratter_b_multiplying_and_dividing_fractions0.3.py
+++ multiplying_dividing_fractions/depratter_b_multiplying_and_dividing_fractions0.3.py
@@ -1,4 +1,4 @@
-# Brendan D. , Multiplying and Dividing Fractions, 05/06/2019, version 0.2
+# Brendan D. , Multiplying and Dividing Fractions, 05/06/2019, version 0.3
 
 print("Hello! My name is Engamespoiler-9000, here to help you multiply and divide fractions.\n")
 user_name = input("What is your name? [Type your name and press ENTER.]\n")
@@ -6,8 +6,9 @@ print("Hello, ", user_name," how are you today?\n")
 
 print("For this program, I need to know the numerator and the denominator for both fractions.\n")
 
+# Variables for fraction 0.
 numerator0 = 0
 denominator0 = 0
-
+# Variables for fraction 1. 
 numerator1 = 0
 denominator1 = 0
