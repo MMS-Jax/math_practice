95b138f1b5176cb1f342ffad848c63d4a63f7685
Isaac Jacobs <s11723988@students.duvalschools.org>
2019-05-08 16:03:40 -0400

    Added fisrt print() string
    Printed some basic instructions for the user.

--- multiplying_dividing_fractions/Isaac_Jacobs_Multi_Divide_Fractions.py
+++ multiplying_dividing_fractions/Isaac_Jacobs_Multi_Divide_Fractions.py
@@ -1,1 +1,7 @@
-# Multiplying and Dividing Fractions by Isaac J.
+#Multiplying and Dividing Fractions by Isaac J.
+
+print("Welcome to the auto fraction solver 5000 I can multipley and divide fractions for you. \n")
+print("In order to multiply and divide fractions we need to know the [Numerator] and [Denominator]. \n"
+
+numerator0 = 0
+numerator1 = 0
