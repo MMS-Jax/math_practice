c4bfb807483b9bec4516931e72b37643ec6db86f
Potato5789 <49240664+Potato5789@users.noreply.github.com>
2019-05-08 15:44:46 -0400

    Declared variables for the fraction.
    Set numerator and denomenator for both fractions at 0 as a starting point.

---
+++ multiplying_dividing_fractions/multiply_divide_fractions 0.3.py
@@ -0,0 +1,14 @@
+# Multiplying and Dividing Fractions by Skylar C. 5/06/19 version 0.3
+
+print("Welcome to the Fraction Calculator! I can multiply and divide fractions for you.\n" )
+user_name = input("What is your name? [Type your name and press ENTER.] \n")
+print("Welcome,", user_name,"How has your day been? ")
+
+print("In order to run the program, I need to know the numerator and denomenator for both fractions. \n" )
+
+# Variables for fraction 0.
+numerator0 = 0
+numerator1 = 0
+# Variables for fraction 1.
+denomenator0 = 0
+denomenator1 = 0
