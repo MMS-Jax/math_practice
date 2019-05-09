e9b50211b2d7084dabac3547177dbc35bf3c08d0
Hobensack <s10676963@students.duvalschools.org>
2019-05-09 10:01:56 -0400

    Added Variables for the new fraction.
    Declared new_numerator and new_denominator, also wrote the code to multiply the two fractions together.

--- multiplying_dividing_fractions/Multiply_Dividing_Fractions_0.5.py
+++ multiplying_dividing_fractions/Multiply_Dividing_Fractions_0.5.py
@@ -1,4 +1,4 @@
-#Luke H.,Multiplying and Dividing Fractions,5/7/19,version 0.4
+#Luke H.,Multiplying and Dividing Fractions,5/7/19,version 0.5
 
 print("Hello! I am Mr.Fraction. I can multiply and divide fractions for you!\n")
 user_name = input("What is your name?[Typle your name and press ENTER.]\n")
