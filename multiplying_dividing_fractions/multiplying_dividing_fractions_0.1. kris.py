d027cb6f1851b7e8e5800c397b4a5c395b06c8f8
imnotcool1 <49570771+imnotcool1@users.noreply.github.com>
2019-05-09 10:51:34 -0400

    added to commet
    we added what it is about

---
+++ multiplying_dividing_fractions/multiplying_dividing_fractions_0.1. kris.py
@@ -0,0 +1,3 @@
+# kris m, nulitiplying and dividing fractions 05/09/2019 virsion 0.1
+
+print("hello lets do some dividing   i can help you divid and mulitiply fractions so you can get good grades in math class!\n")
