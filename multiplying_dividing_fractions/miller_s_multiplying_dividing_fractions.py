d50870713235f72f4ccc678478c24b9dc4a57033
mintgreen18 <49650916+mintgreen18@users.noreply.github.com>
2019-05-09 12:35:20 -0400

    Declared Variables for the fractions.
    Added the numerator and denominator values for the two fractions.

--- multiplying_dividing_fractions/miller_s_multiplying_dividing_fractions.py
+++ multiplying_dividing_fractions/miller_s_multiplying_dividing_fractions.py
@@ -3,4 +3,12 @@
 print("my name is duck")
 user_name = input("what is your name? Sophie.\n")
 print("Hello",",mintgreen18,how are you today?\n")
+
 print("in order to multiply or divide fractions i need to know the numerator and denominator for both fractions.\n")
+
+# Variables for fraction 0.
+numerator0 = 0
+denominator0 = 0
+# Variables for fraction 1.
+numerator1 = 0
+denominator1 = 0
