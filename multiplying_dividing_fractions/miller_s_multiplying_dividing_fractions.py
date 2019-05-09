15e9a4de9fa418fb7ce3434fe51f48bddfa7eae2
mintgreen18 <49650916+mintgreen18@users.noreply.github.com>
2019-05-09 12:16:28 -0400

    added user_name variable and input line.
    added two lines of code to ask for the user's name save it to a variable using input and then printing the name back on the screen.

--- multiplying_dividing_fractions/miller_s_multiplying_dividing_fractions.py
+++ multiplying_dividing_fractions/miller_s_multiplying_dividing_fractions.py
@@ -1,1 +1,6 @@
 #Sophie M., Multiplying and dividing fractions, 05/06/2019
+
+print("my name is duck")
+user_name = input("what is your name? Sophie.\n")
+print("Hello",",mintgreen18,how are you today?\n")
+print("in order to multiply or divide fractions i need to know the numerator and denominator for both fractions.\n")
