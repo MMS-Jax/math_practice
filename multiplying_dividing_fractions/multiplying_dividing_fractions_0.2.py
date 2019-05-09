540557b0c50e8c2f1b371da79875d838cc4082ef
BestFortnitePlayer <49210308+BestFortnitePlayer@users.noreply.github.com>
2019-05-09 10:35:36 -0400

    Declared Variables for the fractions.
    Added the numerator and denominator for the two fractions.

--- multiplying_dividing_fractions/multiplying_dividing_fractions_0.2.py
+++ multiplying_dividing_fractions/multiplying_dividing_fractions_0.2.py
@@ -1,5 +1,7 @@
-# Jonathan W., Multiplying and Dividing Fractions, 05/08/2019, version 0.2
+# Jonathan W., Multiplying and Dividing Fractions, 05/08/2019, version 0.1
 
 print("Hello, welcome to Math-Bot 8000.   I can multiply and divide fractions for you!\n")
 user_name = input("What is your name?  [Type your name and press ENTER]\n")
-print("Hello,", user_name, "how are you today?\n")
+print("Hello,", user_name, "how are you today?")
+
+print("For this program, I need to know the numerator and the denominator for both fractions.\n")
