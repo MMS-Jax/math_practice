4876aeb4f00d5673daf98d62413bc743a95ff015
s11312444 <49413999+s11312444@users.noreply.github.com>
2019-05-09 09:39:14 -0400

    added first string
    printed basic user instructions.

--- multiplying_dividing_fractions/Gray_j_multiplying_dividing_fractions.py
+++ multiplying_dividing_fractions/Gray_j_multiplying_dividing_fractions.py
@@ -1,3 +1,3 @@
-# Jeremy G., Multiplying and Divide Fractions, 05/6/19
+# Jeremy G., Multiplying and Divide Fractions, 05/6/19 version 0.0
 
-
+print("Hello, welcome to Fraction Bot 9000.  I can multiply and divide fractions for you\n")
