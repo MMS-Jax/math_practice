44c36cb2ed85fd4c68227eb6374a8cbdf83909a2
EpicGamesx <49415014+EpicGamesx@users.noreply.github.com>
2019-05-09 10:41:30 -0400

    version 0.4
    Added to print to lines to print the two fractions on the screen

--- multiplying_dividing_fractions/multiplying_dividing_fractions_0.3.py
+++ multiplying_dividing_fractions/multiplying_dividing_fractions_0.3.py
@@ -6,9 +6,11 @@ print("Hello,", user_name,"how are you today?")
 
 print("For this program, I need to know the numerator and the denominator for both fractions.\n")
 
-# Varaibles fro fraction 0.
-numerator0 =0
-denominator0 =0
-# Varaibles fro fraction 1.
-numerator1 =0
-denominator1 =0
+# Varaibles for fraction 0.
+numerator0 = 0
+denominator0 =  0
+print("The first fraction is",numerator0,"/",denominator0,".\n")
+
+# Varaibles for fraction 1.
+numerator1 = 0
+denominator1 = 0
