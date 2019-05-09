e22d96d317889328121bd0414fc8a3599305faa5
Hobensack <s10676963@students.duvalschools.org>
2019-05-09 10:13:08 -0400

    Starting to divide the fractions
    Started the code to divide fractions entered, explained how to divide using the reciprocal.

---
+++ multiplying_dividing_fractions/Multiply_Dividing_Fractions_0.7.py
@@ -0,0 +1,36 @@
+#Luke H.,Multiplying and Dividing Fractions,5/7/19,version 0.7
+
+print("Hello! I am Mr.Fraction. I can multiply and divide fractions for you!\n")
+user_name = input("What is your name?[Typle your name and press ENTER.]\n")
+print("Hello",user_name,"how are you today?")
+
+print("For this progam, I need to know the numerator and the denominator for both fractions.\n")
+# variables for fraction 0.
+numerator0=0
+denominator0=0
+
+print("The first fractions is",numerator0,"/",denominator0,".\n")
+# Variables for the fraction 1.
+numerator1=0
+denominator1=0
+
+print("The second fractions is",numerator1,"/",denominator1,".\n")
+
+print("When mulitplying fractions you multiply the two numerators together.\n")
+print("Then you multiply the two denominators together.\n")
+
+
+new_numerator= numerator0 * numerator1
+new_denominator= denominator0 * denominator1
+
+print("The new fractions is",new_numerator,"/",new_denominator,".\n")
+
+
+#This is where the division of the fractions will start. 
+
+pie= 22/7
+print(pie)
+print("To divide a fraction, you will multiply using the reciprocal or inverse of the second fraction fraction.\n")
+
+
+
