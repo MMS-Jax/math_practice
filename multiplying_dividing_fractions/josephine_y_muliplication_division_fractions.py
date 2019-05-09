982e6438fb73cd56e01cfe18a800ff4b49c71b3c
imaginewagons19 <49206729+imaginewagons19@users.noreply.github.com>
2019-05-09 12:48:21 -0400

    added input
    input for fractions was added

--- multiplying_dividing_fractions/josephine_y_muliplication_division_fractions.py
+++ multiplying_dividing_fractions/josephine_y_muliplication_division_fractions.py
@@ -9,36 +9,36 @@ print("Hello", user_name, "How are you today?")
 print("For this program, I need to know the numerator and denominator for both fractions.\n")
 
 # Variables for fraction 0
-numerator0=0
-denominator0=0
-print("The first fraction is", numerator0, "/", denominator0, ".\n")
+numerator_0 = input ("What is the numerator for the first fraction? \n")
+denominator_0 = input ("What is the numerator for the first fraction? \n")
+print("The first fraction is", numerator_0 , "/", denominator_0 , ".\n")
 
 # Variables for fraction 1
-numerator1=0
-denominator1=0
-print("The second fraction is", numerator1, "/", denominator1, ".\n")
+numerator_1 = input ("What is the numerator for the second fraction? \n")
+denominator_1 = input ("What is your denominator for the second fraction? \n")
+print("The second fraction is", numerator_1 , "/", denominator_1 , ".\n")
 
 print("When mulitplying fractions you muliply the two numerators together. \n")
 print("Then you will multiply the two denominators together. \n")
 
-new_numerator = numerator0 * numerator1
-new_denominator = denominator0 * denominator1
+new_numerator = numerator_0 * numerator_1
+new_denominator = denominator_0 * denominator_1
 
 #This is where the divisions of fractions will start.
 
 print("The new fraction is", new_numerator, "/", new_denominator, ".\n")
 # Variables for fraction 0
-numerator0=0
-denominator0=0
+numerator_2 = input ("What is the numerator for the first fraction? \n")
+denominator_2 = input ("What is denominator for the first fraction? \n")
 
-print("The first fraction is", numerator0, "/", denominator0, ".\n")
+print("The first fraction is", numerator_2, "/", denominator_2, ".\n")
 
 # Variables for fraction 1
-numerator1=0
-denominator1=0
-print("The second fraction is", numerator1, "/", denominator1, ".\n")
+numerator_3 = input ("What is the numerator for the first fraction? \n")
+denominator_3 = input ("What is the denominator for the second fraction? \n")
+print("The second fraction is", numerator_3, "/", denominator_3, ".\n")
 
 print('To divide a fraction, you will multiply using the reciprical or inverse of the second fraction. \n')
 
-new_numerator = numerator0 * denominator1
-new_denominator = denominator0 *  numerator1
+new_numerator = numerator_2 * denominator_3
+new_denominator = denominator_2 *  numerator_3
