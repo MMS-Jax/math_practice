3210dc6030faa5478dfff23cdc6765c44490fd99
Derpy Puffer fish <49402886+Preston893@users.noreply.github.com>
2019-05-09 11:38:03 -0400

    added user_name variable( )
    created username variable and input line.

--- multiplying_dividing_fractions/multiplying_dividing_fractions_0.1.py
+++ multiplying_dividing_fractions/multiplying_dividing_fractions_0.1.py
@@ -1,3 +1,5 @@
 # Kyle P. , Multiplying and Dividing Fractions, 05/09/2019, version 0.1
 
-print("Hello, welcome to fraction-bot 9000, I can multiply and divide fractions for you!/n"
+print("Hello, welcome to fraction-bot 9000,       I can multiply and divide fractions for you!/n"
+user_name = input("What is your name? [Type your name and press ENTER.]/n")
+print("Hello", user_name,"How are you today?")
