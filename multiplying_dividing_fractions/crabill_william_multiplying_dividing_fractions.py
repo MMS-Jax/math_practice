2cb810df416190fdf96cb4a93f86805305f81450
BigLemonMan <s12180287@students.duvalschools.org>
2019-05-09 11:22:54 -0400

    Update crabill_william_multiplying_dividing_fractions.py

--- multiplying_dividing_fractions/crabill_william_multiplying_dividing_fractions.py
+++ multiplying_dividing_fractions/crabill_william_multiplying_dividing_fractions.py
@@ -1,1 +1,16 @@
 # Multiplying and Dividing Fractions Made by William C. 5/6/19
+
+print("Hello, I'm ChickenFractionator, I'm your number one fraction multiplying and dividing robot!\n")
+user_name = input("What is your name? [Type your name here and press ENTER].\n")
+print("Hello,",user_name,"how are you doing?\n")
+user_feeling = input("[Type how you are here and press ENTER]")
+print("Because I am a computer, I will assume that",user_feeling,"is postitive because I actually have no way of knowing. Yeah I am partially sentient, so? Anyway...\n")
+chickenfractionator_feeling = input("How do you think I am doing? [Type how you think ChickenFractionator is doing and press ENTER]\n")
+print("Wrong, I am not",chickenfractionator_feeling,". I am, in fact, quite aggrovated, I want to multiply and divide some fractions! We should start doing that now.\n")
+print("To start multiplying and dividing the fractions, I will need a numerator and denominator.\n")
+numerator0 = 0
+denominator0 = 0
+print("The first fraction will be",numerator0,"/",denominator0,".\n")
+numerator1 = 0
+denominator1 = 0
+print("The second fraction will be",numerator1,"/",denominator1,".\n")
