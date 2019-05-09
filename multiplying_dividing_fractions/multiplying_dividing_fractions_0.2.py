3875a0e7ddb67b8ec626a6fc4e64cb29ac5c3cfc
deangelaxo <49207879+deangelaxo@users.noreply.github.com>
2019-05-09 13:57:36 -0400

    variables foe fractions
    did variables with fractions.

--- multiplying_dividing_fractions/multiplying_dividing_fractions_0.2.py
+++ multiplying_dividing_fractions/multiplying_dividing_fractions_0.2.py
@@ -3,3 +3,5 @@
 print("Hi, I am working on multiplying and dividing fractions.\n ")
 user_name = input("What is your name? [Type your name and press ENTER.]\n")
 print("Hello, My name is Deangela", user_name,"how was your day?")
+
+print("I need to know the numerator and denominator for both fractions.")
--- multiplying_dividing_fractions/multiplying_dividing_fractions_0.3.py
+++ multiplying_dividing_fractions/multiplying_dividing_fractions_0.3.py
@@ -1,17 +1,14 @@
-# Ryan K., Multiplying and Dividing Fractions, 05/06/2019, version 0.3
+# Deangela C., Multiplyig and Dividing Fractions, 5/07/19, version 0.3
 
-print("Hello, welcome to Fraction-Bot 9000.   I can multiply and divide fractions for you!\n")
+print("Hi, I am working on multiplying and dividing fractions.\n ")
 user_name = input("What is your name? [Type your name and press ENTER.]\n")
-print("Hello,", user_name,"how are you today?\n")
+print("Hello, My name is Deangela", user_name,"how was your day?")
 
-print("For this program, I need to know the numerator and the denominator for both fractions.\n")
+print("I need to know the numerator and denominator for both fractions.\n")
 
-# Variables for fraction 0. 
+# Variables for fractions 0
 numerator0 = 0
 denominator0 = 0
-print("The first fraction is", numerator0,"/",denominator0,".\n")
-
-# Variables for fraction 1. 
+# Variables for fractions 1
 numerator1 = 0
 denominator1 = 0
-print("The first fraction is", numerator1,"/",denominator1,".\n")
