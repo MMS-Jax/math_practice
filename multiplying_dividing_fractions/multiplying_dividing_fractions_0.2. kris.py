5d70a89148baf17b64c4e87f5f0961dedc95f6d9
imnotcool1 <49570771+imnotcool1@users.noreply.github.com>
2019-05-09 11:07:31 -0400

    added new commet
    made it say my name

--- multiplying_dividing_fractions/multiplying_dividing_fractions_0.2. kris.py
+++ multiplying_dividing_fractions/multiplying_dividing_fractions_0.2. kris.py
@@ -1,7 +1,8 @@
-# kris m, mulitiplying and dividing fractions 05/09/2019 virsion 0.1
+# kris m, mulitiplying and dividing fractions 05/09/2019 virsion 0.2
 
 print("hello lets do some dividing   i can help you divid and mulitiply fractions so you can get good grades in math class!\n")
 user_name = input("what is your name going to be? [type what your name is then press ENTER.]\n")
-print("hello,", user_name,"how is your day?")
+print("hello,", user_name,"how is your day?\n")
 
 
+kris
