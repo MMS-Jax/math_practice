359b0031970bf40a01a7085cd3b92083357344c9
LemonHead9906 <49240006+LemonHead9906@users.noreply.github.com>
2019-05-09 11:29:20 -0400

    Version 0.4
    Added to print0 lines to print the two fractions on the screen to varify they are correct.

--- multiplying_dividing_fractions/scott_trevor_multiplying_dividing_fractions_0.4.py
+++ multiplying_dividing_fractions/scott_trevor_multiplying_dividing_fractions_0.4.py
@@ -1,4 +1,4 @@
-# Multiplying & Dividing Trevor.S 5/06/2019 VERSION O.3
+# Multiplying & Dividing Trevor.S 5/06/2019 VERSION O.4
 
 print("welcome to fraction bot 9000! I can multiply and divide fractions. \n")
 user_name = input("what is your name? [Type your name and press ENTER.]\n")
