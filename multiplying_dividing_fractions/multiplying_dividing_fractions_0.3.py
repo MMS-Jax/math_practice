cba34096ab11c0abb23ca56caeb05a60614a8cbe
deangelaxo <49207879+deangelaxo@users.noreply.github.com>
2019-05-09 14:17:25 -0400

    Version 0.4
    Added to print() lines to print the two fractions 0 and 1.

--- multiplying_dividing_fractions/multiplying_dividing_fractions_0.3.py
+++ multiplying_dividing_fractions/multiplying_dividing_fractions_0.3.py
@@ -9,6 +9,9 @@ print("I need to know the numerator and denominator for both fractions.\n")
 # Variables for fractions 0
 numerator0 = 0
 denominator0 = 0
+print("The first fraction is", numerator0,"/", denominator0,".\n" )
+
 # Variables for fractions 1
 numerator1 = 0
 denominator1 = 0
+print("The first fraction is", numerator1,"/", denominator1,".\n" )
