ef3a073ba0318a61d937b07c35cd9be4ee7f89a1
NicD101 <49321370+NicD101@users.noreply.github.com>
2019-05-09 09:40:19 -0400

    Create multiplying_dividing_fraction_version0.5.py
    Added the multiply function and added the variables for the multiplied fraction

---
+++ multiplying_dividing_fractions/multiplying_dividing_fraction_version0.5.py
@@ -0,0 +1,26 @@
+# Nicolas D., Multiplying and Dividing Fraction, version 0.5
+
+print("This program is used to multiply and divide fractions.\n")
+
+user_name = input("What is your name? [Type your name and press ENTER.]\n")
+print("Hello",user_name,"how are you today")
+
+print("For this program, I need the numerator and the denominator of both fractions.\n")
+# Variables for fraction 0
+numerator0 = 0
+denominator0 = 0
+print("The first fraction is", numerator0,"/",denominator0,".\n")
+
+# Variables for fraction 1
+numerator1 = 1
+denominator1 = 1
+print("The second fraction is", numerator1,"/", denominator1,".\n")
+
+print("When multiplying fractions you multiply the two numerators together.\n")
+print("Then you multiply the two denominators together.\n")
+
+new_numerator   = numerator0 * numerator1
+new_denominator = denominator0 * denominator1
+
+print("The second fraction is", new_numerator,"/", new_denominator 
+,".\n")
