e0898df291a9ce2b1798c53e0b2219d500dcc3e4
Mibsii <49242388+Shibazzz@users.noreply.github.com>
2019-05-10 10:49:41 -0400

    Declared Variables for the Fraction
    Added the numerator and denominator values for the two fractions

---
+++ mlg_multiply_divide_fractions0.2.py
@@ -0,0 +1,11 @@
+# Mya G., Multiply & Divide Fractions, 05/06/19
+
+print('Hello, I am Ducky Momo! I can mulitply and divide fractions for you.\n')
+print('To multiply and divide fractions, I need to know the numerator and denominator for each fraction.\n')
+
+numerator0 = 0
+numerator1 = 0
+denominator0 = 0
+denominator1 = 0
+user_name = input('What is your name? [Type your name and press ENTER.]\n')
+print('Hello', user_name, 'How are you today?')
