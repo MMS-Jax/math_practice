20bb2448f4b1b3b99bb729b465c8d9b8c35a805f
OSOK6ix <49444784+OSOK6ix@users.noreply.github.com>
2019-05-10 10:12:56 -0400

    Added new fraction
    added new_denominator new_numerator also wrote the code to multiply the fractions together

---
+++ multiplying_dividing_fractions/DA_mult_divid_fractions_0.5.py
@@ -0,0 +1,25 @@
+# Multiplying and Dividing Fractions By Daniel A. 05/06/19 Version 0.5
+
+print("Welcome to the Math bot 6IX9INE I can MULTIPLY AND DIVIDE FRACTIONS!\n ")
+user_name = input("What is your name? [type your name press ENTER.]\n ")
+print("Hello," ,user_name,"How are you today?")
+
+print("for this program,i need to know numerator and the denominator for both fractions.\n")
+
+# variables for fraction 0
+numerator0 = 0
+denominator0 = 0
+print("the fist fraction is", numerator0,"/", denominator0,".\n")
+
+# Variable for fraction 1
+numerator1 = 0
+denominator1 = 0
+print("the second fraction is", numerator1,"/", denominator1,".\n")
+
+print("When multiplying fractions you multiply the two numerators together.\n")
+print("Then you will multiply the two denominaators together.\n")
+
+new_numerator = numerator0 * numerator1
+new_denominator = denominator0 * denominator1
+
+print("the new fraction is",new_numerator,"/", new_denominator ,".\n")
