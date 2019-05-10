0f75ddba49d30f0a79e3def4e7e85efa3f0580f1
Zani <s11673969@students.duvalschools.org>
2019-05-10 11:39:52 -0400

    Create az_multiplying_dividing_fractions_0.3.py

---
+++ az_multiplying_dividing_fractions_0.3.py
@@ -0,0 +1,14 @@
+# Andi Z., Mutiplying and Dividing 05/06/19, version 0.3
+
+print("Hello, welcome to Soviet Sausage.   I can multiply and divide fraction for you!\n")
+user_name = input("What is your name? [Type your name and press ENTER.]\n")
+print("Hello, ", user_name," how are you today?") 
+
+print("For this program, I need to know the numerator and denominator for both fractions.\n")
+
+# Variables for fraction 0. 
+numerator0 = 0 
+denominator0 = 0
+# Variables for fraction 1.
+numerator1 = 0
+denominator1 = 0
