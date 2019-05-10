49a0fdaf5de07c78ad5ab133157f89248e782511
Ashley L <s11544541@student.duvalschools.org>
2019-05-10 11:23:25 -0400

    Declared variables for the fractions.
    Added the numerator and denominator values for the two fractions.

---
+++ multiplying_dividing_fractions/aml_multiply_divide_fractions_0.3.py
@@ -0,0 +1,14 @@
+#Ashley L., Multiplying and Dividing Fractions, 05/06/2019, version 0.3
+
+print("Hello, My name is Mathemater and i will multiply and divide fractions.\n")
+user_name = input("What is your name? [Type your name and press ENTER.]\n")
+print("Hello,", user_name,"how are you today?")
+
+print("In order to multipy and divide fractions I need to know the numerator an the denominator of both fractions.")
+
+# Variables for fraction 0.
+numerator0 = 0
+denominator0 = 0
+# Variables for fraction 1.
+numerator1 = 0
+denominator1 = 0
