bd51042355b68f15cb3915894b861d244d8c9298
Jewel2143 <s11586674@student.duvalschools.org>
2019-05-10 11:58:08 -0400

    Jvg_math_04
    dividing and multiplying fractions

---
+++ multiplying_dividing_fractions/jvg_Dividing_multiplying_fractions_04.py
@@ -0,0 +1,33 @@
+# Julianna G., Muliplying and Dividing fractions, 05/06/2019
+
+print("Hi, this is for multiplying and dividing fractions\n")
+user_name = input("What is your name? [Type you name and press enter.\n ")
+print("Hello", user_name,"how are you today")
+
+print("For this program, I need to know the numerator and the denominator for both fractions.\n")
+
+# Variables for fraction 0.
+numertor0 = 0
+denomnator0 = 0
+print("The frist fraction is", numerator0,"/",denominator0,".\n")
+
+# Variables for fraction 1.
+numerator1 = 0
+denominator1 = 0
+print("The second fraction is", numerator1,"/",denominator1,".\n")
+
+print("What multiplying fractions you multiply the two numerators together.\n")
+print("Then you will multiply the two denominators together. \n")
+
+new_numerator = numerator0 * numerator1
+new_denominator = denominator0 * denominator1
+
+print("The new fraction is", new_numerator,"/",new_denominator,".\n")
+
+# this is where the division of the fractions will start
+pie = 22 / 7
+print(pie)
+print("to divide a fraction, you will multiply using the reciprocal or inverse of the second fraction.\n")
+
+
+
