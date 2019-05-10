47b1c8e7621e202790fe929fe36bdc284f0b5580
Sergey <s11575552@students.duvalschools.org>
2019-05-10 16:05:02 -0400

    Added user name variable
    Added two lines of code to ask for username and than present username

--- multiplying_dividing_fractions/SVR_multiplying_dividing_Fractions_0.2.py
+++ multiplying_dividing_fractions/SVR_multiplying_dividing_Fractions_0.2.py
@@ -1,5 +1,5 @@
-# Sergey R., Multiplying and Dividing Fractions, 05/10/2019 version 0.1
+# Sergey R., Multiplying and Dividing Fractions, 05/10/2019 version 0.2
 
 print("Hello, welcome to 2 way calculator. I can multiply and divide fractions for you!\n")
 user_name = input("What is your name? [Type your name and press ENTER.]\n")
-print("Hello,", user_name,"how are you today?")
+print("Hello,", user_name,"how are you today?\n")
