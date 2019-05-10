09053b1c64936dfebcd21c89c7c78726bc419c57
MoltenEngine <49403073+MoltenEngine@users.noreply.github.com>
2019-05-10 09:44:42 -0400

    Added user_name variable and input() line.
    Added two lines of code to ask for the user's name, save it to a variable using input() and then printing the name back on the screen.

--- multiplying_dividing_fractions/eden_b_multiplying_and_dividing_0.2.py
+++ multiplying_dividing_fractions/eden_b_multiplying_and_dividing_0.2.py
@@ -1,7 +1,8 @@
 # Multiplying and Dividing Eden B 5/06/2019
 
 print("Welcome to fraction bot 9001! I can multiply and divide fractions for you!\n")
-user_name = input("What is your name? [Type your name and press ENTER.\n")
+user_name = input("What is your name? [Type your name and press ENTER.]\n")
+print("Hello, ", user_name," how are you today?\n")
 print("In order to multiply and divide fractions i need to know the [....] and [....]\n")
 
 
