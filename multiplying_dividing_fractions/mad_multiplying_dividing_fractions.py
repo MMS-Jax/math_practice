b8fa8f6dbe8a818cbc35849dabfb7cbbed963057
HeWasNumber1 <49246121+HeWasNumber1@users.noreply.github.com>
2019-05-10 11:43:35 -0400

    asked how are you today
    Asked how are you today

---
+++ multiplying_dividing_fractions/mad_multiplying_dividing_fractions.py
@@ -0,0 +1,5 @@
+# Mason D., Multiplying and Dividing Fractions, 5/6/2019, version 0.1
+
+print("Hello! Welcome to lil Fraction- Over 9000. I Will Multiply and Divide your Fractions for You \n")
+user_name = input ("what is your name? [Type your name and press ENTER.]\n")
+print("Hello, ",user_name," how are you today?" )
