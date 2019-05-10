873677f9b1fda5494cbcf02ebd4889c9ac3f9ac5
klea <s12366969@students.duvalschools.org>
2019-05-10 15:20:05 -0400

    Added variables for the new fraction.
    Declared new_numerator and new_denominator, also wrote the code to multiply the two fractions together.

--- multiplying_dividing_fractions/kr_multiplying_dividing_fractoions_version_0.5_klea _rrushi.py
+++ multiplying_dividing_fractions/kr_multiplying_dividing_fractoions_version_0.5_klea _rrushi.py
@@ -1,4 +1,4 @@
-# Klea R., Multiplying and Dividing Fractions, 05/06/19,version 0.4
+# Klea R., Multiplying and Dividing Fractions, 05/06/19,version 0.5
 
 print("Hello, welcome to fraction-bot 9000.  I can  multiply and divide fractions for you !\n")
 user_name=input("What is your name?[Yype yuor name and press ENTER.]\n")
