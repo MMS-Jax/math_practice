857e75aac062aa32428377b21f0254eea2f04060
Jadin Rose <s20051643@students.duvalschools.org>
2019-05-10 13:25:02 -0400

    Create jcr_multiplying_dividing_fractions_0.2a.py

---
+++ multiplying_dividing_fractions/jcr_multiplying_dividing_fractions_0.2a.py
@@ -0,0 +1,5 @@
+# Jadin R., Multiplying and Dividing Fractions, 05/09/2019, version 0.1
+
+print ("Hello, welcome to fraction cowboy 69.   I can multiply and divide fractions for you!\n")
+user_name = input ("what is your name? [Type your name and press ENTER.]\n")
+print ("Hello! ,", user_name ,"How are you today?")
