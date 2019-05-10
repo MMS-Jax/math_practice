3e44d482c3dc5751fbf2b2674b37cd7fea7dc96c
s11492378 <49407919+s11492378@users.noreply.github.com>
2019-05-10 11:28:21 -0400

    added first print() string.
    printed basic instructions for the user.

---
+++ NO_multiply_divide_fractions_0.1.py
@@ -0,0 +1,3 @@
+# Nolan.O, Multiplying and dividing Fractions, 05/06/2019, version 0.1
+print("Hello! My name is yung fraction gawd. I can multiply and devide fractions for you!\n")
+print("I will need to know the numerator and the denomenator.")
