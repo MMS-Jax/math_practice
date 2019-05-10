88e1862ee8d5d6401d1b613e0d87f4951649057b
Zephyrhills <49448677+Zephyrhills@users.noreply.github.com>
2019-05-10 11:30:24 -0400

    Added variable for ther new fraction
    Declared new_numerator and new_denominator

--- multiplying_dividing_fractions/bjd_multiplying_dividing_fractions0.5.py
+++ multiplying_dividing_fractions/bjd_multiplying_dividing_fractions0.5.py
@@ -1,4 +1,4 @@
-# Brendan D. , Multiplying and Dividing Fractions, 05/06/2019, version 0.4
+# Brendan D. , Multiplying and Dividing Fractions, 05/06/2019, version 0.5
 
 print("Hello! My name is Engamespoiler-9000, here to help you multiply and divide fractions.\n")
 user_name = input("What is your name? [Type your name and press ENTER.]\n")
