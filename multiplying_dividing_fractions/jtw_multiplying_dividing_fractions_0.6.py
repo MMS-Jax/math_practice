636e6396110189e7672f379e040c025f9a94119a
DGK-Jayden <49407930+DGK-Jayden@users.noreply.github.com>
2019-05-10 11:41:58 -0400

    starting to divide the fractions
    starting to code the fractions entering the code to divide the fractions.

---
+++ multiplying_dividing_fractions/jtw_multiplying_dividing_fractions_0.6.py
@@ -0,0 +1,39 @@
+# Jayden W, Multiplying and Dividing Fractions, 05/06/2019, VERSION 0.6
+
+print("Mr. Thick is going to destroy the earth, we need to use math to help bepis protect the people of shell city!\n")
+user_name = input("What is your name? [Type your name amd press ENTER.]\n")
+print("Hello ", user_name," How are you today?\n")
+
+print("For this important mission i need the numerator and denominator for both fractions.\n")
+
+# Variables for fraction 0.
+numerator0 = 0
+denominator0 = 0
+print("The first fraction is", numerator0, "/", denominator0,".\n")
+
+# Variables for fraction 1.
+numerator1 = 0
+denominator1 = 0
+print("The second fraction is", numerator1, "/", denominator1,".\n")
+
+print("When multiplying fractions, you multiply the two numerators togeather.\n")
+print("Then you will multiply the two denominators togeather.\n")
+
+numerator0 = int(input("Type the first numerator and press enter.\n"))
+numerator1 = int(input("Type the second numerator and press enter.\n"))
+
+denominator0 = int(input("Type the first denominator and press enter.\n"))
+denominator1 = int(input("Type the second denominator and press enter.\n"))
+
+print("The first fraction is", numerator0,"/",denominator0,".\n")
+print("The second fraction is", numerator1,"/",denominator1,".\n")
+
+new_numerator = numerator0 * numerator1
+new_denominator = denominator0 * denominator1
+
+print("The new fraction is", new_numerator, "/", new_denominator,".\n")
+
+# This is where the division of the freactions will start.
+pie = 22 / 7
+print(pie)
+print("To divide a fraction, you will muultiply using the reciprocal or inverse of the second fraction.\n")
