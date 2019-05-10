a102cbcc1cf1d4a4e9a33247e59a187f4206186b
s20014067 <49455500+s20014067@users.noreply.github.com>
2019-05-10 15:08:56 -0400

    test

---
+++ multiplying_dividing_fractions_0.0/NXSmultiplying_and_dividing fractions_0.3py.py
@@ -0,0 +1,18 @@
+# Nehemiah S., Multiplying and Dividing Fractions, 05/08/2019 version 0.3
+
+print("Hello, my name is FractionBot 9000.  I can multiply and divide fractions for you!\n") 
+user_name = input("what is your name? [type your name and press ENTER.]\n")
+print("Hello,", user_name",how are you today?")
+
+print("For this program, I need to know the numerator and the denominator for both fractions.\n")
+
+#Variables for fraction 0.
+numerator0=0
+denominator0=0
+print("The first fraction is", numerator0,"/",denominator0,".\n")
+
+#Variables for fraction 1.
+numerator1=0
+denominator1=0
+print("The first fraction is" numerator1,"/",denominator1,".\n")
+
