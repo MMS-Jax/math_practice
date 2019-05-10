44d6874a4dfe5138dc5e7e3eba974e93fb0acc7e
Jevon <s10494276@students.duvalschools.org>
2019-05-10 15:01:05 -0400

    declare variable for the program
    added numerator and denominator

--- multiplying_dividing_fractions/jr_multiplying_dividing_fractions_0.2.py
+++ multiplying_dividing_fractions/jr_multiplying_dividing_fractions_0.2.py
@@ -1,9 +1,13 @@
 # Jevon R., Multlying and divide fraction, 05/08/2019, version 0.2
 
 print("Hello, Welcome to Fraction-Bot 9000.  I can multiply and divide fraction for you!\n")
-user_name = input("What is your name? [Type ypur name and press ENTER.]\n")
+user_name = input("What is your name? [Jevon.]\n")
 print ("Hello,", user_name,"how are you today?\n")
 
 print("For this program, I need to know the numerator and the denominator for both fractions.\n")
 
-numerator
+numerator0 = 
+denominator0 =
+
+numerator1
+denominator1 
--- multiplying_dividing_fractions/jr_multiplying_dividing_fractions_0.3.py
+++ multiplying_dividing_fractions/jr_multiplying_dividing_fractions_0.3.py
@@ -1,9 +1,14 @@
-# Jevon R., Multlying and divide fraction, 05/08/2019, version 0.2
+# Jevon R., Multlying and divide fraction, 05/08/2019, version 0.3
 
 print("Hello, Welcome to Fraction-Bot 9000.  I can multiply and divide fraction for you!\n")
-user_name = input("What is your name? [Type ypur name and press ENTER.]\n")
+user_name = input("What is your name? [Type your name and press ENTER.]\n")
 print ("Hello,", user_name,"how are you today?\n")
 
 print("For this program, I need to know the numerator and the denominator for both fractions.\n")
 
-numerator
+# Variable for fraction 0
+numerator0 = 0
+denominator0 = 0
+
+numerator1 = 0
+denominator1 = 0 
