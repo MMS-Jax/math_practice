cfcfbd1bac3d0e7aeb8c33bd2971e7fc525eb8be
Jonathan Meister <49206594+JonathanHatesMathForeverUntillDeath@users.noreply.github.com>
2019-05-10 11:17:57 -0400

    Create jmm_multiplying_divide_fractions.py
    changed nmae

---
+++ multiplying_dividing_fractions/jmm_multiplying_divide_fractions.py
@@ -0,0 +1,5 @@
+# Jonathan M. , Multiplying and dividing fractions, 05/06/2019, version
+print("Hello! My name is Satan. I can multiply and divide fractions\n")
+print("To multiply and divide fractions I need to know the numerator and the denominator")
+user_name = input("What is you name? [Type your name here and press enter.]\n")
+print("Hello, ", user_name, "how are you today?")
