14f3cdd47d7f4527b43d9a1c72a30237f19e4091
s11649159 <49206466+s11649159@users.noreply.github.com>
2019-05-10 11:50:46 -0400

    Made 0.8 version file
    Added new numerator and new denominator and multiplied by the reciporical of new dnominator and the new numeratoes.

---
+++ multiplying_dividing_fractions/bhh_multiply_divide_fractions_0.8.py
@@ -0,0 +1,33 @@
+# Ben H., Multiplying and Divivding Fractions, 05/06/2019, version 0.7
+
+print("Hello! My name is Fraction Master- Over 9000. I will ultiply and divide fractions for you!\n ")
+user_name = input("What is your name is your name? Ben Hipp \n")
+print("Hello, ",  user_name,"how are you today?\n")
+
+print("For this program, I need to know the numerator and the denominator for both fractions. \n")
+
+# Variables for fraction 0.
+numerator0 = 0
+denominator0 = 0
+print("The first fraction is", numerator0, "/",denominator0,". \n,")
+
+# Variables for fraction 1.
+numerator1 = 0
+denominator1 = 0
+print("The first fraction is", numerator1,"/", denominator1,".\n")-
+
+print("When multiplying fractions you multiply the two numerators together. \n")
+print("Then you will multiply the two demoninators together. \n")
+
+new_numerator = numerator0 * numerator1
+new_denominator = denominator0 * denominator1
+
+print("The first fraction is", new_numerator,"/", new_denominator,".\n")
+
+# This is where the division of the fractions will start. 
+
+print("To divide a fraction, you will multiply using the reciporical or inverse of the second fraction. \n")
+
+new_numerator = numerator0 * denominator1
+new_denominator = denominator0 * numerator1
+
