fb9fd2d1c8a34a40da84df90b17be89d3a2f63e2
Mljs1158 <49206695+Mljs1158@users.noreply.github.com>
2019-05-10 11:20:02 -0400

    added more to prgram for fractions
    added more codes to the program

---
+++ multiplying_dividing_fractions/jackson_m_multiply_divide_fractions_0.2.py
@@ -0,0 +1,5 @@
+# Marcellus J., Multiplying and Dividing Fractions, 05/06/2019, version 0.2
+
+print("Hello, My name is QuickmathsBot. I can Multiply and Divide fractions fro you!\n")
+user_name = input("What's your name? [Type your name and press ENTER.\n")
+print("Hello, ", user_name,"how are you today?")
---
+++ multiplying_dividing_fractions/mlj_multiply_divide_fractions_0.3.py
@@ -0,0 +1,17 @@
+# Marcellus J., Multiplying and Dividing Fractions, 05/06/2019, version 0.3
+
+print("Hello, My name is QuickmathsBot. I can Multiply and Divide fractions fro you!\n")
+user_name = input("What's your name? [Type your name and press ENTER.\n")
+print("Hello,", user_name,"how are you today?\n")
+
+print("For this program, I need to know the numerator and the denominator for both fractions.\n")
+
+# Variables for fraction 0. 
+numerator0 = 0
+denominator0 = 0
+print("The first fraction is", numerator0,"/",denominator0,".\n")
+
+# Variables for fraction 1. 
+numerator1 = 0
+denominator1 = 0
+print("The first fraction is", numerator1,"/",denominator1,".\n")
