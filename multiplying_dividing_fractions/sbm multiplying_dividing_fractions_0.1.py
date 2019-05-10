85dc741326cc4c59ea4611c807edcc378b6dcd69
Overpowered112 <s11524089@students.duvalschools.org>
2019-05-10 16:10:29 -0400

    helped user with information
    added common information

--- multiplying_dividing_fractions/sbm multiplying_dividing_fractions_0.1.py
+++ multiplying_dividing_fractions/sbm multiplying_dividing_fractions_0.1.py
@@ -1,3 +1,3 @@
-# Sasha M., Multiplying and Dividing Fractions, 05/10/2019, version 0.0
+# Sasha M., Multiplying and Dividing Fractions, 05/10/2019, version 0.1
 
 print("Hello, welcome to bobster   I can multiply and divide fractions for you!\n")
