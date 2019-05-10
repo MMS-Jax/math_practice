44ed44b384f00c2ed21df74b1a17e3cb2fb97629
Parker <s11562949@students.duvalschools.org>
2019-05-10 10:58:09 -0400

    Declared variables for fractions.
    Added the numerator and denominator values for the two fractions

---
+++ pck_multiplying_dividing_fractions_0.3.py
@@ -0,0 +1,14 @@
+# Parker K., Multiplying And Dividing Fractions, 05/06/19
+
+print("Hello my name is fractiony boy. I can multiply and divide fractions for you.\n")
+user_name = input("What is your name? [Type your name and press ENTER.]\n")
+print("Hello,", user_name, "how are you today?\n")
+
+print("For this program, I need to know the numerator and denominator for both the fractions.\n")
+
+# Variables for fraction 0.
+numerator0 = 0
+numerator1 = 0
+# Variables for fraction 1.
+denominator0 = 0
+denominator1 = 0
