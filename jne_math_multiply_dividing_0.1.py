ee004a86b01799478ce3741d8a570196e856094f
joshua <s11688777@students.duvalschools.org>
2019-05-10 10:59:01 -0400

    added a user_name variable and input line
    added a user name cariable and input so it can read your name and say hello

---
+++ jne_math_multiply_dividing_0.1.py
@@ -0,0 +1,5 @@
+# joshua_edwards,multiply and divide fractions 05/06/2019 0.1
+
+print("hello,my name is fraction bot i can do any fraction math problem for you i am the smarted bot in the world!!!!!!!!!!!!\n")
+user_name = input("what is your  [type your name and press ENTER.]\n")
+print ("hello ," ,user_name, "how are you today?")
