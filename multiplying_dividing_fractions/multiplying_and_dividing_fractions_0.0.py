61b12f8e3ec37ce7d76a8b8a2fd79a7dd51c0ce4
Elon Musk <49207612+youngdashboi@users.noreply.github.com>
2019-05-10 12:54:26 -0400

    Update multiplying_and_dividing_fractions_0.0.py
    Ver 0.1

--- multiplying_dividing_fractions/multiplying_and_dividing_fractions_0.0.py
+++ multiplying_dividing_fractions/multiplying_and_dividing_fractions_0.0.py
@@ -2,4 +2,5 @@
 
 print("Hello, my name is Youngmathboi. I will multiply and divide fractions for you.\n")
 user_name = input("What is your name? [Type your name and press ENTER].\n")
+print("Hello,", user_name,"how are you today")
 print("In order for me to do that i need to know the numerator and the denominator of the equation.\n")
