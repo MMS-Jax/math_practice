7afad57e47bae0794804ddc94d9b94e793501be3
DGK-Loo <s11370137@students.duvalschools.org>
2019-05-10 11:54:04 -0400

    Declared variables for the fractions.
    Added thenumerator and denominator values for the two fractions.

---
+++ multiplying_dividing_fractions/laj_multiplying_dividing_fractions_0.3.py
@@ -0,0 +1,14 @@
+# Joseph., Multiplying and Dividing Fraction, 05/06/19, version 0.3
+
+print("Hi, my fish is drowning please help me solve a math question to revive him. \n")
+user_name = input("Whats your name? [Type your name and press ENTER.]\n") 
+print("Hello, ", user_name,"how are you today?")
+
+print("For this program, i need to know the numerator and denominator for both fractions.\n")
+
+# Variable for fraction 0.
+numerator0 = 0
+denominator0 = 0
+# Variables for fraction 1.
+numerator1 = 0
+denominator1 = 0
