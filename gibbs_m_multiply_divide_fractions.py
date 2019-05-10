5e98c44695c31e5f61c9bba55cd09eed97637866
Mibsii <49242388+Shibazzz@users.noreply.github.com>
2019-05-10 10:33:20 -0400

    Update
    Added new file name

--- gibbs_m_multiply_divide_fractions.py
+++
@@ -1,3 +0,0 @@
-# Mya G., Multiply & Divide Fractions, 05/06/19
-
-print('Hello, I am Ducky Momo! I can mulitply and divide fractions for you.\n')
---
+++ mlg_multiply_divide_fractions.py
@@ -0,0 +1,9 @@
+# Mya G., Multiply & Divide Fractions, 05/06/19
+
+print('Hello, I am Ducky Momo! I can mulitply and divide fractions for you.\n')
+print('To multiply and divide fractions, I need to know the numerator and denominator for each fraction.\n')
+
+numerator0 = 0
+numerator1 = 0
+denominator0 = 0
+denominator1 = 0
