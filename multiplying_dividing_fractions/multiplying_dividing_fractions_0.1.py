454ddbcf0da11e781cda9a39dbd3f3d3789cc3f6
notsafe777 <49414115+notsafe777@users.noreply.github.com>
2019-05-10 15:11:24 -0400

    Created first version of program
    Added my first command to start the program

--- multiplying_dividing_fractions/multiplying_dividing_fractions_0.1.py
+++ multiplying_dividing_fractions/multiplying_dividing_fractions_0.1.py
@@ -1,5 +1,3 @@
-# Ryan K., Multiplying and Dividing Fractions, 05/06/2019, version 0.1
+# Brayan S., Multiplying and Dividing Fractions, 05/08/2019, version 0.1
 
-print("Hello, welcome to Fraction-Bot 9000.   I can multiply and divide fractions for you!\n")
-user_name = input("What is your name? [Type your name and press ENTER.]\n")
-print("Hello,", user_name,"how are you today?")
+print("Hi, welcome to Cheat On Fractions, I will multiply and divide the fractions for you!\n")
