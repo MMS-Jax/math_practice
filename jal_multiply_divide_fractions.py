d1fbb26ab1c00afff7bfe7570304ed0953da6896
Julia.L <s11042561@students.duvalschools.org>
2019-05-10 11:07:19 -0400

    Update jal_multiply_divide_fractions.py
    added initials

--- jal_multiply_divide_fractions.py
+++ jal_multiply_divide_fractions.py
@@ -1,6 +1,8 @@
 #Julia L ., Multiply and Dividing Fractions
 
 print('Hello my name is fractionbot 900 I multiply and divide frcations.\n')
+user_name = input("What is your name?\n")
+print("hello,", user_name, "How are you today?\n")
 print('To multiply and divide fractions I need to know the [Blank] and [Blank] for each fraction.\n')
 
 numerator0 = 0
