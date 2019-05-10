35120d0bf112005b51c3b594650f32e5383f6650
lukerenne4 <s11527645@students.duvalschools.org>
2019-05-10 16:04:43 -0400

    Defined Variables for the Fractions
    Added numerator and denominator values for the fractions.

---
+++ multiplying_dividing_fractions/lcr_multiplying_dividing_fractions_0.3py.py
@@ -0,0 +1,14 @@
+#Luke R, Multiplying and Dviding Fractions, 5/06/19, version 0.3
+
+print("Hello, my name is Fraction-Bot9000. I will multiply and divide fractions for you.\n")
+user_name = input("What is your name? [Type your name and press ENTER.]\n")
+print("Hello, ",user_name," how are you today?")
+
+print("For this program, I need to know the numerator and the denominator for both fractions. \n")
+
+# Variables for fraction 0.
+numerator0 =0
+denominator0 =0
+# Variables for fraction 1.
+numerator1 =0
+denominator1 =0
