816e1429c58356366b49213b6ccd3a1ee121eba0
lukerenne4 <s11527645@students.duvalschools.org>
2019-05-10 15:53:44 -0400

    Added first lines of print
    Added numerator and denominator values along with instructions for the user.

---
+++ multiplying_dividing_fractions/lcr_multiplying_dividing_fractions_0.1py.py
@@ -0,0 +1,14 @@
+#Luke R, Multiplying and Dviding Fractions, 5/06/19
+
+print("Hello, my name is Fraction-Bot9000. I will multiply and divide fractions for you.\n")
+print("In order to multiply or divide fractions, I need to know the numerator and the denominator. \n")
+
+numerator0 =0
+numerator1 =0
+
+print("The first numerator is", numerator0, "and the second number is", numerator1,".\n")
+
+denominator0 =0
+denominator1 =0
+
+print()
