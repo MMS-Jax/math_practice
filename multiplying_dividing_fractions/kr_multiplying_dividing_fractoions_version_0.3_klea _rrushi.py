9d0a1d3900c3d37939b0d2c8d20c4fc5716f717b
klea <s12366969@students.duvalschools.org>
2019-05-10 14:56:35 -0400

    Declared variables for the fractions.
    Added the numerator and denominator values for the two fractions.

---
+++ multiplying_dividing_fractions/kr_multiplying_dividing_fractoions_version_0.3_klea _rrushi.py
@@ -0,0 +1,19 @@
+# Klea R., Multiplying and Dividing Fractions, 05/06/19,version 0.3
+
+print("Hello, welcome to fraction-bot 9000.  I can  multiply and divide fractions for you !\n")
+user_name=input("What is your name?[Yype yuor name and press ENTER.]\n )
+print("Hello,",user_name ,"How are you today?\n")
+
+print ("For this program, i need to know the numerator and the denominator for bothe fractions.\n")
+
+# Variables for fraction 0.
+numerator0 =0
+denominator0 =0
+# Variables or fraction 1
+numerator1 =0
+denominator1 =0
+
+
+
+
+
