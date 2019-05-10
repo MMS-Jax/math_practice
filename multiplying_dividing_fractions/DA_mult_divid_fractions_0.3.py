dbf2f067e5578d5a710148575ded151fc060797f
OSOK6ix <49444784+OSOK6ix@users.noreply.github.com>
2019-05-10 10:02:49 -0400

    Version 0.4
    Added lines to print the two fractions on the screen

---
+++ multiplying_dividing_fractions/DA_mult_divid_fractions_0.3.py
@@ -0,0 +1,17 @@
+# Multiplying and Dividing Fractions By Daniel A. 05/06/19 Version 0.3
+
+print("Welcome to the Math bot 6IX9INE I can MULTIPLY AND DIVIDE FRACTIONS!\n ")
+user_name = input("What is your name? [type your name press ENTER.]\n ")
+print("Hello," ,user_name,"How are you today?")
+
+print("for this program,i need to know numerator and the denominator for both fractions.\n")
+
+# variables for fraction 0
+numerator0 = 0
+denominator0 = 0
+print("the fist fraction is", numerator0,"/", denominator0,".\n")
+
+# Variable for fraction 1
+numerator1 = 0
+denominator1 = 0
+print("the second fraction is", numerator1,"/", denominator1,".\n")
