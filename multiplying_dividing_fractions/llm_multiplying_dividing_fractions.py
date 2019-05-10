b0bdff1af5635ffecafdb6fd28edae43488609af
Laci <s11600111@students.duvalshools.org>
2019-05-10 11:27:00 -0400

    mutliplying and dividing fractions file
    Made a multiplying and dividing fractions file

---
+++ multiplying_dividing_fractions/llm_multiplying_dividing_fractions.py
@@ -0,0 +1,4 @@
+ # Laci M. , Multiplying and Dividing Frations, 5/06/2019, verison 0.0
+
+ print("My Name is BigBootyJudy.")
+ print("In order to multiply or divide fractions. I need to know the numerator and denominator.")
