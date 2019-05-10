3b4b2a37698073e0e6f0f13829beade587336ac4
Kennedy122 <49565896+Kennedy122@users.noreply.github.com>
2019-05-10 11:45:54 -0400

    Added user_name variable and input0 line.
    Added two lines of code to ask for the user's name save it to a variable using input0 and then printing the name back on the screen.

--- multiplying_dividing_fractions/Gray_K_multiply_division_fraction.py
+++ multiplying_dividing_fractions/Gray_K_multiply_division_fraction.py
@@ -1,4 +1,6 @@
 # Ryan K., Multiplying and Dividing Fractions, 05/05/2019
 
-print("hello! My Name Is MathBotand I Will multiply and divide fractions.\n")
-Print("In Order to multiply fractions, you need too  knwthe numerator and denomerator of the fractions.")
+print("hello! My Name Is MathBot and I Will multiply and divide fractions.\n")
+Print("In Order to multiply fractions, you need too  know the numerator and denomerator of the fractions.")
+user_name - input("what is your name?[Type your name and press ENTER.\n")
+print("Hello, "user_name," how are you today?")
---
+++ multiplying_dividing_fractions/krg_multiply_division_fraction.py
@@ -0,0 +1,6 @@
+# Ryan K., Multiplying and Dividing Fractions, 05/05/2019
+
+print("hello! My Name Is MathBot and I Will multiply and divide fractions.\n")
+print("In Order to multiply fractions, you need too  know the numerator and denomerator of the fractions.")
+user_name = input("what is your name?[Type your name and press ENTER.\n")
+print("Hello,",user_name," how are you today?")
