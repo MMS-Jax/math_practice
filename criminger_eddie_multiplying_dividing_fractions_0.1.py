37c2f91200ef4be80113ae3ffffd1a04d32e1f04
ABot56 <ABot56@github.com>
2019-05-10 13:26:28 -0400

    Update criminger_eddie_multiplying_dividing_fractions_0.1.py

--- criminger_eddie_multiplying_dividing_fractions_0.1.py
+++ criminger_eddie_multiplying_dividing_fractions_0.1.py
@@ -1,3 +1,5 @@
 # Eddie c. Multiplying and Dividing Fractions 5-9-19 version 0.1
 
-print("This program is designed to multiply and divide fractions.")
+print("This program is designed to multiply and divide fractions./n")
+user_name = input ("What is you username? [Type your username and press enter]/n") 
+print("Greetings", user_name," how are you today?)
