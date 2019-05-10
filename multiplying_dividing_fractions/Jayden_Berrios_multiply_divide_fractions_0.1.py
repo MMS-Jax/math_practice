5dd57b273c2d6b213305930275400d63021a19bb
HadesLXVI <s11279536@students.duvalschools.org>
2019-05-10 10:04:27 -0400

    added user_name variable output() lines
    Added two lines of code to ask for the user's name, save the variable using () and then printing the name back on the screen.

--- multiplying_dividing_fractions/Jayden_Berrios_multiply_divide_fractions_0.1.py
+++ multiplying_dividing_fractions/Jayden_Berrios_multiply_divide_fractions_0.1.py
@@ -1,7 +1,8 @@
 # Multiplying and Dividing Fractions by Jayden B. 
 
 print( "Welcome to the Auto Fraction Solver 4000000 I can multiply and divide fractions for you.\n")
-
+user_name = input("What is your name? [Type your name and press ENTER.]\N")
+PRINT("Hello, ", user_name,"how are you today?\n")
 print("In Order to Multiply or divide fractions we need to know the numerator and the denominator.\n")
 
 numerator0 = 0
