1852bea1b803062a8ae67671d66bfbd4ced95d02
Armani Hayes <s20004304@students.duvalschools.org>
2019-05-10 11:37:34 -0400

    Wrote code for solving the equations

---
+++ multiplying_dividing_fractions/azh_multiply_divide_fractions01.py
@@ -0,0 +1,29 @@
+# Armani H., Multiplying and Dividing Fractions, 05/06/2019
+
+print("Hi. My name is Jayce. I can multiply and divide fractions. \n")
+ user_name = input("What is your name? [Type your name and press ENTER.] \n")
+ 
+ +numerator0 = int(input("What is the first numerator? [Type a number and press enter.] ")) # Make sure it's an integer!
+
+denominator0 = int(input("What is the first denominator? [Type a number and press enter.] " ))
+
+print("Your first fraction is",numerator0,"/",denominator0,".")
+
+# Run the above code and see how it works! 
+
+print("What is the second fraction?")
+
+# Add code here to input the numerator and denominator for the second fraction.  Print the second fraction on the screen. 
+
+multiply_divide = int(input(print("Type 1 and press enter to multiply the fractions.  Type 2 and press enter to divide the fractions.\n")
+
+# This creates two variables for the NEW fraction.  Both variables are set equal to zero to start. 
+new_numerator = 0
+new_denominator = 0 
+
+if multiply_divide == : # What number for this variable means to multiply? 
+    # Write the code to multiply the two fractions here. 
+else: 
+    # Write the code to divide the two fractions here.  
+
+# Print the new fraction back onto the screen.  
--- multiplying_dividing_fractions/hayes_armani_multiply_divide_fractions.py
+++ multiplying_dividing_fractions/hayes_armani_multiply_divide_fractions.py
@@ -1,3 +1,4 @@
 # Armani H., Multiplying and Dividing Fractions, 05/06/2019
 
-print("Hi. My name is Jan. I can multiply and divide fractions.")
+print("Hi. My name is Jayce. I can multiply and divide fractions. \n")
+ user_name = input("What is your name? [Type your name and press ENTER.] \n")
