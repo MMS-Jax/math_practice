dc48f48170efc788973483e8111cca295418788e
S11413226 <49414078+S11413226@users.noreply.github.com>
2019-05-10 14:48:03 -0400

    Create ceg_Multiplying_dividing_fractions_0.2.py

---
+++ multiplying_dividing_fractions/ceg_Multiplying_dividing_fractions_0.2.py
@@ -0,0 +1,5 @@
+# Christian G., Multiplying and Dividing Fractions, 05/07/2019, version 0.2
+
+print("Hello, welcome to Fraction-Bot 9000.   I can multiply and divide fractions for you!\n")
+user_name = input("What is your name? [Type your name and press ENTER.] \n")
+print("Hello,", user_name,"how are you today?")
