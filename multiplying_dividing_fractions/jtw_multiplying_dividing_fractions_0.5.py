615e8d5acc3953ffbd0c9eca72f26fcb2f7be435
DGK-Jayden <49407930+DGK-Jayden@users.noreply.github.com>
2019-05-10 11:18:51 -0400

    Create jtw_multiplying_dividing_fractions_0.5.py

---
+++ multiplying_dividing_fractions/jtw_multiplying_dividing_fractions_0.5.py
@@ -0,0 +1,26 @@
+# Jayden W, Multiplying and Dividing Fractions, 05/06/2019, VERSION 0.5
+
+print("Mr. Thick is going to destroy the earth, we need to use math to help bepis protect the people of shell city!\n")
+print("In order to save shell city, bepis will need to know the numerator and denominator for the fractions.\n")
+user_name = input("What is your name? [Type your name amd press ENTER.]\n")
+print("Hello ", user_name," How are you today?\n")
+
+print("For this important mission i need the numerator and denominator for both fractions.\n")
+
+# Variables for fraction 0.
+numerator0 = 0
+denominator0 = 0
+print("The first fraction is", numerator0, "/", denominator0,".\n")
+
+# Variables for fraction 1.
+numerator1 = 0
+denominator1 = 0
+print("The second fraction is", numerator1, "/", denominator1,".\n")
+
+print("When multiplying fractions, you multiply the two numerators togeather.\n")
+print("Then you will multiply the two denominators togeather.\n")
+
+new_numerator = numerator0 * numerator1
+new_denominator = denominator0 * denominator1
+
+print("The new fraction is", new_numerator, "/", new_denominator,".\n")
