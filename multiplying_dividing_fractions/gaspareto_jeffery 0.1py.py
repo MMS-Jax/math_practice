020174a3402231cf4202d21111aebdde88cf28c2
kingjeffrobro <49403236+kingjeffrobro@users.noreply.github.com>
2019-05-10 09:57:17 -0400

    adding first print] string.
    printed some basic instructions for the user

---
+++ multiplying_dividing_fractions/gaspareto_jeffery 0.1py.py
@@ -0,0 +1,4 @@
+# jeffery G multiplying and dividing fractions, 05/06/2019, version 0.1
+
+print('welcome to fraction-bot 15! would you like me to multiply and divide fractions for you? \n')
+print
---
+++ multiplying_dividing_fractions/gaspareto_jeffery.py
@@ -0,0 +1,4 @@
+# jeffery G 5/6/2019
+
+print('welcome to fraction-bot 15! would you like me to multiply and divide fractions for you? \n')
+print
