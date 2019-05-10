12e3e948a52ff5e636a349d8af1fb415acd71b2a
deangelaxo <49207879+deangelaxo@users.noreply.github.com>
2019-05-10 13:02:00 -0400

    Create dkc_multiplying_dividing_fractions_0.5.py

---
+++ multiplying_dividing_fractions/dkc_multiplying_dividing_fractions_0.5.py
@@ -0,0 +1,32 @@
+# Deangela C., Multiplyig and Dividing Fractions, 5/07/19, version 0.5
+
+print("Hi, I am working on multiplying and dividing fractions.\n ")
+user_name = input("What is your name? [Type your name and press ENTER.]\n")
+print("Hello, My name is Deangela", user_name,"how was your day?")
+
+print("I need to know the numerator and denominator for both fractions.\n")
+
+# Variables for fractions 0
+numerator0 = 0
+denominator0 = 0
+print("The first fraction is", numerator0,"/", denominator0,".\n" )
+
+# Variables for fractions 1
+numerator1 = 0
+denominator1 = 0
+print("The second fraction is", numerator1,"/", denominator1,".\n" )
+
+print("When multiplying fractions you multiply the two numerators together.\n")
+print("Then you will multiply the two denominators together.\n")
+
+new_numerator = numerator0 * numerator1
+new_denominator = denominator0 * numerator1
+
+print("The new fraction is", new_numerator,"/", new_denominator,".\n" )
+
+
+
+
+
+
+
