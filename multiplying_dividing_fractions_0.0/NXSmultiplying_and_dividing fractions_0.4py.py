5251528bab9ec1054d4107b2a4d45f3478b5c145
s20014067 <49455500+s20014067@users.noreply.github.com>
2019-05-10 15:20:05 -0400

    declared variables for the fractions.
    Added the numerator and denominator values for the two fractions.

--- multiplying_dividing_fractions_0.0/NXSmultiplying_and_dividing fractions_0.4py.py
+++ multiplying_dividing_fractions_0.0/NXSmultiplying_and_dividing fractions_0.4py.py
@@ -1,8 +1,8 @@
-# Nehemiah S., Multiplying and Dividing Fractions, 05/08/2019 version 0.3
+# Nehemiah S., Multiplying and Dividing Fractions, 05/08/2019 version 0.4
 
 print("Hello, my name is FractionBot 9000.  I can multiply and divide fractions for you!\n") 
 user_name = input("what is your name? [type your name and press ENTER.]\n")
-print("Hello,",user_name",how are you today?")
+print("Hello,", user_name,",how are you today?")
 
 print("For this program, I need to know the numerator and the denominator for both fractions.\n")
 
@@ -14,5 +14,5 @@ print("The first fraction is", numerator0,"/",denominator0,".\n")
 #Variables for fraction 1.
 numerator1=0
 denominator1=0
-print("The first fraction is",numerator1"/",denominator1,".\n")
+print("The second fraction is", numerator1,"/",denominator1,".\n")
 
