940bb006abab51a1b55b2f157c892413b9b0a1d0
HadesLXVI <s11279536@students.duvalschools.org>
2019-05-10 10:06:22 -0400

    deleted old file name and changed file name
    changed file name

--- multiplying_dividing_fractions/Jayden_Berrios_multiply_divide_fractions.py
+++
@@ -1,8 +0,0 @@
-# Multiplying and Dividing Fractions by Jayden B. 
-
-print( "Welcome to the Auto Fraction Solver 4000000 I can multiply and divide fractions for you.\n")
-
-print("In Order to Multiply or divide fractions we need to know the numerator and the denominator.\n")
-
-numerator0 = 0
-numerator1 = 0
