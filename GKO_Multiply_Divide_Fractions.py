91eee52dda8014cd861e05662a2c3a29c3e0de23
Gabbyyyy1 <s11612132@students.duvalschools.org>
2019-05-10 11:07:18 -0400

    Added initials
    Initials

--- GKO_Multiply_Divide_Fractions.py
+++ GKO_Multiply_Divide_Fractions.py
@@ -1,8 +1,7 @@
 # Gabriella O. , Multiply and  Divide Fractions, 05/06/19
 
-print('Hello, My name is Mathbot 1000. I can multiply and divide fractions for you.\n')
-print('To multiply and divide fractions I need to know the Numerator and Denominator for each fraction\n')
+print("Hello, My name is Mathbot 1000. I can multiply and divide fractions for you.\n")
+user_name = input("What is your name?\n")
+print ("Hello,", user_name,"how are you today?\n")
 
-numerator0 =0
-numerator1 =0
 
--- Oliver_G_Multiply_Divide_Fractions.py
+++
@@ -1,8 +0,0 @@
-# Gabriella O. , Multiply and  Divide Fractions, 05/06/19
-
-print('Hello, My name is Mathbot 1000. I can multiply and divide fractions for you.\n')
-print('To multiply and divide fractions I need to know the Numerator and Denominator for each fraction\n')
-
-numerator0 =0
-numerator1 =0
-
