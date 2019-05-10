c10fc19002c99456cec11b43ca7cbbb27544fe2e
TotallyNotZayne <49252407+ThisUsernameHasAlreadyBeenTaken@users.noreply.github.com>
2019-05-10 15:55:26 -0400

    Added variables for fractions
    The fractions now have a variables, one named 'numerator0' and 'denominator0.' And the other is 'numerator1' and 'denominator1.'

---
+++ multiplying_dividing_fractions/ZED_Multiplying_Dividing_Fraction_0.3.py
@@ -0,0 +1,14 @@
+# Zayne D. Program will Multiply and Divide Fractions, 5/09/2019, Version 0.2
+
+print('Hi, Wlecome to Dividing/Multiplying Fractions. I will multiply any fraction you give me. \n')
+user_name = input('What is your name? (Type the name you prefer and then press ENTER.) \n')
+print('Hello,', user_name, ', how are you doing today?')
+
+print('For this program, I need to know the numerator and the denominator for both fractions. \n')
+
+# Variables for fraction 0.
+numerator0 = 0
+denominator0 = 0
+# Variables for fraction 1.
+numerator1 = 1
+denominator1 = 1
