e4abbd5318e7ca9eb548c74f7ab4851556c4f0cd
Khalil06 <49448376+FireAndIce904@users.noreply.github.com>
2019-05-10 11:53:11 -0400

    Finished teling computer how to multiply fraction
    I told the computer each step in order to multiply fraction

--- multiplying_dividing_fractions/ktg_multiply_divide_fractions0.7.py
+++ multiplying_dividing_fractions/ktg_multiply_divide_fractions0.7.py
@@ -34,6 +34,5 @@ new_denominator = denominator0 * denominator1
 print("The new fraction is", new_numerator,"/",new_denominator,".\n")
 
 # This is where the division of the fraction will start.
-pie =  22 / 7
-print(pie)
+
 print("To divide a fraction, you will multiply using the reciprocal or inverse of the second fraction.\n")
