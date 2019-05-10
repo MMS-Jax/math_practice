2ed92a69a042d1a7c68211edbdc45def974404fb
peachhirai1109 <49244343+peachhirai1109@users.noreply.github.com>
2019-05-10 15:11:30 -0400

    Asked User For Name
    User can now input their name into the code.

--- multiplying_dividing_fractions/sm_multiplying_dividing_fractions_0.2.py
+++ multiplying_dividing_fractions/sm_multiplying_dividing_fractions_0.2.py
@@ -1,5 +1,5 @@
-# Shruthi Manikandan, Multiply and Divide Fractions, 05/06/19, version 0.0
+# Shruthi Manikandan, Multiply and Divide Fractions, 05/06/19, version 0.2
 
 print("Welcome to my program! Would you like to see a magic trick involving fractions?\n")
 user_name = input("What shall I refer to you as? (Type it in and press enter)\n")
-print("Henlo, ", user_name, " how was your day?")
+print("Henlo,", user_name, "how was your day?")
