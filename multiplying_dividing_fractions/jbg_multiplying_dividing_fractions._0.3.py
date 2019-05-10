5051320069f530e67c29647ef8cca13acfb5627f
s11312444 <49413999+s11312444@users.noreply.github.com>
2019-05-10 15:00:51 -0400

    declared variable values for fractions
    set the values for the denominator and numerator in each of the two variables.

--- multiplying_dividing_fractions/jbg_multiplying_dividing_fractions._0.3.py
+++ multiplying_dividing_fractions/jbg_multiplying_dividing_fractions._0.3.py
@@ -1,4 +1,4 @@
-# Jeremy G., Multiplying and Divide Fractions, 05/6/19 version 0.0
+# Jeremy G., Multiplying and Divide Fractions, 05/6/19 version 0.3
 
 print("Hello, welcome to Fraction Bot 9000.  I can multiply and divide fractions for you\n")
 user_name = input("whats your name? [Type your name and press Enter.\n")
