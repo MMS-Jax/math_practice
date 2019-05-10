1bd657ab8e861b97c942970819985432e3ce2296
JacksonXD <s11558988@students.duvalschools.org>
2019-05-10 11:35:39 -0400

    version 0.4
    Added to print() lines to print the two fractions on the screen to verify they are correct.

---
+++ CBJ_multiplying_dividing_fractions_0.4.py
@@ -0,0 +1,17 @@
++#Jackson C., Multpily and dividing fractions, 05/06/19 version 0.2
+
+print("hello my name is Big Billy Chilly Boi and I multiply and divide fractions\n")
+print("To multiply and divide fractions for you master, I need to know the numerator and denominator.\n")
+user_name = input("Hello what is your name? [enter your name here and then press enter\n")
+print("Hello, " , user_name," how are you today?\n")
+
+print("For this program, I need to know the numerator and denominator for both fractions.\n")
+
+# Varibles for fraction 0.
+numerator0 = 0
+denominator0 = 0
+
+# Varibles for fractions 1.
+numerator1 = 0
+denominator1 = 0
+print("The second fraction is", numerator1,"/",denominator1,".\n"
--- multiplying_dividing_fractions/copeland_j_multply_divide_fractions.py
+++
@@ -1,6 +0,0 @@
-#Jackson C., Multpily and dividing fractions, 05/06/19
-
-print("hello my name is Big Billy Chilly Boi and I multiply and divide fractions\n")
-print("To multiply and divide fractions for you master, I need to know the numerator and denominator.\n")
-user_name = input("Hello what is your name? [enter your name here and then press enter\n")
-print("Hello, " , user_name," how are you today?\n")
