fc9c12e5fedbdea20a82797a6c3e64ad88d59743
deangelaxo <49207879+deangelaxo@users.noreply.github.com>
2019-05-10 13:37:11 -0400

    made the fractions
    I did the fractions with their numbers.

--- multiplying_dividing_fractions/dkc_multiplying_dividing_fractions_0.7py.py
+++ multiplying_dividing_fractions/dkc_multiplying_dividing_fractions_0.7py.py
@@ -1,4 +1,4 @@
-# Deangela C., Multiplyig and Dividing Fractions, 5/07/19, version 0.6
+# Deangela C., Multiplyig and Dividing Fractions, 5/07/19, version 0.7
 
 print("Hi, I am working on multiplying and dividing fractions.\n ")
 user_name = input("What is your name? [Type your name and press ENTER.]\n")
