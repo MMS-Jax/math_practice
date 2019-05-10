5f7e78bb870b4be8f654251d1206f76c8059b9ab
Mibsii <49242388+Shibazzz@users.noreply.github.com>
2019-05-10 11:00:42 -0400

    Version 0.4
    Added two print() liens to print the two fractions on the screen to verify they are correct.

---
+++ mlg_multiply_divide_fractions_0.3.py
@@ -0,0 +1,18 @@
+# Mya G., Multiply & Divide Fractions, 05/06/19
+
+print('Hello, I am Ducky Momo! I can mulitply and divide fractions for you.\n')
+print('To multiply and divide fractions, I need to know the numerator and denominator for each fraction.\n')
+user_name = input('What is your name? [Type your name and press ENTER.]\n')
+print('Hello', user_name, 'How are you today?')
+
+# Variables for fraction 0.
+numerator0 = 0
+numerator1 = 0
+print('The first fraction', numerator0, '/',denominator0,'.\n' )
+
+# Variables for fraction 1.
+denominator0 = 0
+denominator1 = 0
+print('The second fraction is', numerator1, '/',denominator1,'.\n')
+
+
