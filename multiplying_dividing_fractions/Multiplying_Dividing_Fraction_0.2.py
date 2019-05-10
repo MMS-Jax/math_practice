62f0ca237a9c4d4e721b7d8a54c2ea6771672c36
TotallyNotZayne <49252407+ThisUsernameHasAlreadyBeenTaken@users.noreply.github.com>
2019-05-10 15:50:47 -0400

    Added user_name and input
    Updated previous version with two lines in the code as the code asks for your name.

--- multiplying_dividing_fractions/Multiplying_Dividing_Fraction_0.2.py
+++ multiplying_dividing_fractions/Multiplying_Dividing_Fraction_0.2.py
@@ -1,4 +1,4 @@
-# Zayne D. Program will Multiply and Divide Fractions, 5/09/2019, Version 0.0
+# Zayne D. Program will Multiply and Divide Fractions, 5/09/2019, Version 0.2
 
 print('Hi, Wlecome to Dividing/Multiplying Fractions. I will multiply any fraction you give me. \n')
 user_name = input('What is your name? (Type the name you prefer and then press ENTER.) \n')
