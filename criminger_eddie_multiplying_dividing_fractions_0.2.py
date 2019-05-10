897a4f3567fb2185f6a54ae432a2116f5bd18a10
ABot56 <ABot56@github.com>
2019-05-10 13:31:32 -0400

    Fixed syntax error

---
+++ criminger_eddie_multiplying_dividing_fractions_0.2.py
@@ -0,0 +1,22 @@
+# Eddie c. Multiplying and Dividing Fractions 5-9-19 version 0.1
+
+print("This program is designed to multiply and divide fractions./n")
+user_name = input ("What is you username? [Type your username and press enter]/n") 
+print("Greetings,", user_name," how are you today?)
+
+print("For this program, I need to know the numerator and the denominator for both fractions.\n")
+
+# Variables for fraction 0. 
+numerator0 = 0
+denominator0 = 0
+print("The first fraction is", numerator0,"/",denominator0,".\n")
+
+# Variables for fraction 1. 
+numerator1 = 0
+denominator1 = 0
+print("The second fraction is", numerator1,"/",denominator1,".\n")
+
+print("When multiplying fractions you multiply the two numerators together.\n")
+print("Then you will multiply the two denominators together.\n")
+
+numerator0 = int(input("Type the first numerator and press enter.\n")) 
