851bb9086a84cc6cb6f308db37015524968b6ac4
DGK-Loo <s11370137@students.duvalschools.org>
2019-05-10 11:18:48 -0400

    Create laj_multiplying_dividing_fractions_0.1.py

---
+++ multiplying_dividing_fractions/laj_multiplying_dividing_fractions_0.1.py
@@ -0,0 +1,5 @@
+# Joseph., Multiplying and Dividing Fraction, 05/06/19, version 0.1
+
+print("Hi, my fish is drowning please help me solve a math question to revive him. \n")
+user_name = input("Whats your name? [Type your name and press ENTER.]\n") 
+print("Hello, ", user_name,"how are you today?")
