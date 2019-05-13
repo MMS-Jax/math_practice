570c089d521f2c0c174c1fc720629c6529f4c2e1
Einxhel <s12658720@students.duvalschools.org>
2019-05-13 10:40:32 -0400

    Added user_name variable and input line
    Added two lines of code to ask for the user's name than save it to a variable using input and than printin the name back on the screen.

---
+++ multiplying_dividing_fractions/multiply_divide_fractions 0.1.py
@@ -0,0 +1,5 @@
+# Multiplying and Diving Fractions by Einxhel D. 05/06/19     
+                  
+print("Hello, welcome to multipying and dividing fractios by Einxhel D.\n")
+unser_name=input("Einxhel./n")
+print("Hello,",user_name,"How are you today?\n")
