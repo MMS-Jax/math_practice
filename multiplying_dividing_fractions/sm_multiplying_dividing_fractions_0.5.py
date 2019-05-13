1d865906c8eac368a64226c4d01566553494e274
peachhirai1109 <49244343+peachhirai1109@users.noreply.github.com>
2019-05-13 15:44:29 -0400

    Version 0.5
    Able to multiply the numerator and denominator together to create a new fraction.

---
+++ multiplying_dividing_fractions/sm_multiplying_dividing_fractions_0.5.py
@@ -0,0 +1,25 @@
+# Shruthi Manikandan, Multiply and Divide Fractions, 05/06/19, version 0.5
+
+print("Welcome to my program! Would you like to see a magic trick involving fractions?\n")
+user_name = input("What shall I refer to you as? (Type it in and press enter)\n")
+print("Hello", user_name, "how was your day?\n")
+
+print("This program requires the numerators and denominators for both fractions.\n")
+
+# Variables for Fraction 0.
+numerator0 = 0
+denominator0 = 0
+print("The first fraction is", numerator0, "/", denominator0, ".\n")
+
+# Variables for Fraction 1.
+numerator1 = 0
+denominator1 = 0
+print("The second fraction is", numerator1, "/", denominator1, ".\n")
+
+print("When multiplying fractions, you multiply the numerators together.\n")
+print("Then you multiply the denominators together.\n")
+
+new_numerator = numerator0 * numerator1
+new_denominator = denominator0 * denominator1
+
+print("The new fraction is", new_numerator, "/", new_denominator, ".\n")
