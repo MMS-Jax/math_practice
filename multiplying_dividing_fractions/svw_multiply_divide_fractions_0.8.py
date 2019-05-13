4eff4c883507679843eaf91f18c218bcb2b8afa5
Shay <49175183+this-mailbox-is-mine@users.noreply.github.com>
2019-05-13 12:23:49 -0400

    Updated code.
    Added a couple more aspects to the code.

--- multiplying_dividing_fractions/svw_multiply_divide_fractions_0.8.py
+++ multiplying_dividing_fractions/svw_multiply_divide_fractions_0.8.py
@@ -1,4 +1,4 @@
-# Sheyna W., Multiplying and Dividing Fractions, 05/06/19, Version 0.5
+# Sheyna W., Multiplying and Dividing Fractions, 05/06/19, Version 0.8
 
 print("Hello, my name is Dr. Phil the Fraction God, and I was made to consult you on multiplying and dividing fractions. Nice to meet you.\n ")
 user_name = input("First, I need to know what your name is. Please type it into the space provided. [Type your username and press ENTER.] \n")
