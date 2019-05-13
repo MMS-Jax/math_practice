5bd0b470d8410a6d5312149fd5d0c0655190fddc
Jadin Rose <s20051643@students.duvalschools.org>
2019-05-13 14:20:24 -0400

    Version 0.4
    Added print() lines to print the two fractions on the screen to verify they are correct.

--- multiplying_dividing_fractions/jcr_multiplying_dividing_fractions_0.4.py
+++ multiplying_dividing_fractions/jcr_multiplying_dividing_fractions_0.4.py
@@ -1,4 +1,4 @@
-# Jadin R., Multiplying and Dividing Fractions, 05/09/2019, version 0.3
+# Jadin R., Multiplying and Dividing Fractions, 05/09/2019, version 0.4
 
 print ("Hello, welcome to fraction cowboy 69.   I can multiply and divide fractions for you!\n")
 user_name = input ("what is your name? [Type your name and press ENTER.]\n")
