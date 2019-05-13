eb27028dbf77f366c7c608fca076ece4bd2e5b06
peachhirai1109 <49244343+peachhirai1109@users.noreply.github.com>
2019-05-13 15:36:42 -0400

    Version 0.4
    Print lines added to display fractions.

--- multiplying_dividing_fractions/sm_multiplying_dividing_fractions_0.4.py
+++ multiplying_dividing_fractions/sm_multiplying_dividing_fractions_0.4.py
@@ -1,4 +1,4 @@
-# Shruthi Manikandan, Multiply and Divide Fractions, 05/06/19, version 0.3
+# Shruthi Manikandan, Multiply and Divide Fractions, 05/06/19, version 0.4
 
 print("Welcome to my program! Would you like to see a magic trick involving fractions?\n")
 user_name = input("What shall I refer to you as? (Type it in and press enter)\n")
@@ -14,3 +14,4 @@ print("The first fraction is", numerator0, "/", denominator0, ".\n")
 # Variables for Fraction 1.
 numerator1 = 0
 denominator1 = 0
+print("The second fraction is", numerator1, "/", denominator1, ".\n")
