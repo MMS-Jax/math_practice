97b4b8d81bed91cb00267327617687713ba26056
Jadin Rose <s20051643@students.duvalschools.org>
2019-05-13 14:08:28 -0400

    Declared variables for fractions.
    Added the numerator and denominatorvalues for the two fractions

--- multiplying_dividing_fractions/jcr_multiplying_dividing_fractions_0.3.py
+++ multiplying_dividing_fractions/jcr_multiplying_dividing_fractions_0.3.py
@@ -1,4 +1,4 @@
-# Jadin R., Multiplying and Dividing Fractions, 05/09/2019, version 0.2
+# Jadin R., Multiplying and Dividing Fractions, 05/09/2019, version 0.3
 
 print ("Hello, welcome to fraction cowboy 69.   I can multiply and divide fractions for you!\n")
 user_name = input ("what is your name? [Type your name and press ENTER.]\n")
@@ -6,8 +6,9 @@ print ("Hello! ,", user_name ,"How are you today?\n")
 
 print ("For this program I need the numerator and the denominator for both fractions.\n")
 
+# Variables for fraction 0.
 numerator0 = 0
 denominator0 = 0
-
+# Variables for fraction 1.
 numerator1 = 0
 denominator1 = 0
