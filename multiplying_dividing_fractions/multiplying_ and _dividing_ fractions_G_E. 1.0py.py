209acba3f78a8a6a419eb0f65f2b33b130ec88e6
E.G <s11607579@students.duvalschools.org>
2019-05-13 10:39:06 -0400

    Finished process of Multiplying and dividing fractions 1.0
    Finished the code for multiplying and dividing the two fractions and it works.

---
+++ multiplying_dividing_fractions/multiplying_ and _dividing_ fractions_G_E. 1.0py.py
@@ -0,0 +1,53 @@
+# Multiplying and Dividing fractions Elena G. 5/6/19 version 0.5
+
+print ("Welcome I am Multiplying-Bot 3000. I can Multiply and Divide fractions for you. \n")
+user_name = input("what is your name? [Type your name and press ENTER.  ]\n")
+print ("Hello ", user_name,"how are you today?")
+
+print("For this program, I need to know the numerator and denominator for both fractions. \n")
+
+# Variables for fraction 0.
+numerator0 = 0
+denominator0 = 0
+print(" The first fraction is",numerator0, "/",denominator0, ".\n" )
+
+# variables for fraction 1.
+numerator1 = 0
+denominator1 = 0
+print("The second fraction is",numerator1, "/" ,denominator1,  ".\n" )
+
+print("When multiplying fractions you multiply the two numerators together.\n")
+print("The you will also multiply the two denominators together.\n")
+
+numerator0 = int(input("Type the first numerator and press enter.\n"))
+numerator1 = int(input("Type the second numerator and press enter.\n"))
+
+denominator0 = int(input("Type the first denominator and press enter.\n"))
+denominator1= int(input("Type the second  denominator and press enter.\n"))
+
+print("The first fraction is", numerator0,"/", denominator0, ".\n")
+print("The second fraction is", numerator1,"/", denominator1, ".\n")
+
+new_numerator = numerator0 * numerator1
+new_denominator = denominator0 * denominator1
+
+print("the new fraction is", new_numerator,"/", new_denominator, ".\n" )
+
+#This is where the division of the fraction will start.You will need to change this code!  
+pie = 22 / 7
+print(pie)
+print("To divide a fraction, you will multiply ")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
