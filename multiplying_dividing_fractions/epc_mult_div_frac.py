e94f51e880f49c72f3d8ff489f55bf2cc706e563
realistik702 <s12441838@students.duvalschools.org>
2019-05-13 12:18:26 -0400

    I finished the division.
    I added the how to divide fractions section.

--- multiplying_dividing_fractions/epc_mult_div_frac.py
+++ multiplying_dividing_fractions/epc_mult_div_frac.py
@@ -36,3 +36,22 @@ print("The new fraction is", new_numerator,"/",new_denominator,".\n")
 # This is where the division of the fractions will start. You will need to change this code!
 
 print("To divide a fraction, you will multiply using the reciprocal or inverse of the second fraction.\n")
+
+print("When dividing fractions, you flip the second fraction.\n")
+
+print("Then, you will multiply the two numerators together.\n")
+print("Then, you will multiply the two denominators together.\n")
+
+numerator0 = int(input("Type the first numerator and press ENTER.\n"))
+numerator1 = int(input("Type the second numerator and press ENTER.\n"))
+
+denominator0 = int(input("Type the first denominator and press ENTER.\n"))
+denominator1 = int(input("Type the second denominator and press ENTER.\n"))
+
+print("The first fraction is", numerator0,"/",denominator0,".\n")
+print("The second fraction is", numerator1,"/0",denominator1,".\n")
+
+new_numerator = numerator0 * numerator1
+new_denominator = denominator0 * denominator1
+
+print("The new fraction is", new_numerator,"/",new_denominator,".\n")
