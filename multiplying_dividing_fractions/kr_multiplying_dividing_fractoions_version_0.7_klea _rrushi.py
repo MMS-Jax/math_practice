0a6f4f20087b55fa94d40fcbf94b92084cc39c2f
klea <s12366969@students.duvalschools.org>
2019-05-13 16:09:00 -0400

    starting to divide fractions.
    made the changes needed to divide.

--- multiplying_dividing_fractions/kr_multiplying_dividing_fractoions_version_0.7_klea _rrushi.py
+++ multiplying_dividing_fractions/kr_multiplying_dividing_fractoions_version_0.7_klea _rrushi.py
@@ -31,10 +31,10 @@ denominator1 = int(input("Type the second denominator and press enter.\n"))
 print("The first fraction is", numerator0,"/",denominator0,".\n")
 print("The second fraction is", numerator1,"/",denominator1,".\n")
 
-new_numerator = numerator0 * numerator1
-new_denominator = denominator0 * denominator1
+new_numerator = numerator0 * denominator1
+new_denominator = denominator0 * numerator1
 
 print("The new fraction is", new_numerator,"/",new_denominator,".\n")
 pie = 22 / 7
 print(pie)
-print("To divide a fraction, you will multiply using the reciprocal or inverse of the secon")d fraction.\n
+print("To divide a fraction, you will multiply using the reciprocal or inverse of the secon") fraction.\n
