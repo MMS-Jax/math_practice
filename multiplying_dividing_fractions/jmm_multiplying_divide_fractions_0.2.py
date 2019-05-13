875761d7563cbae05c9079dfb6fb25a45530344e
Jonathan Meister <49206594+JonathanHatesMathForeverUntillDeath@users.noreply.github.com>
2019-05-13 12:17:44 -0400

    added user_name variable
    addeds user name video

--- multiplying_dividing_fractions/jmm_multiplying_divide_fractions_0.2.py
+++ multiplying_dividing_fractions/jmm_multiplying_divide_fractions_0.2.py
@@ -1,4 +1,4 @@
-# Jonathan M. , Multiplying and dividing fractions, 05/06/2019, version
+# Jonathan M. , Multiplying and dividing fractions, 05/06/2019, version 0.2
 print("Hello! My name is Satan. I can multiply and divide fractions\n")
 print("To multiply and divide fractions I need to know the numerator and the denominator")
 user_name = input("What is you name? [Type your name here and press enter.]\n")
