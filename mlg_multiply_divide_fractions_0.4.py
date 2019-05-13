24ec58353befc2d8664cde3b88c37391198d2aa9
Mibsii <49242388+Shibazzz@users.noreply.github.com>
2019-05-13 11:27:03 -0400

    Added variables for the new fraction
    I declared new_numerator & new_denominator and I also wrote the code to multiply the two fractions together.

--- mlg_multiply_divide_fractions_0.4.py
+++ mlg_multiply_divide_fractions_0.4.py
@@ -10,7 +10,6 @@ numerator0 = 0
 denominator0 = 0
 print('The first fraction is', numerator0, '/',denominator0,'.\n' )
 
-
 # Variables for fraction 1.
 numerator1 = 0
 denominator1 = 0
@@ -19,4 +18,8 @@ print('The second fraction is', numerator1, '/',denominator1,'.\n')
 print('When multiplying fractions, you multiply the two numerators together.\n')
 print('Then, you will multiply the two denominators together.\n')
 
-new_numerator = numerator0
+new_numerator = numerator0 * numerator1
+new_denominator = denominator0 * denominator1
+
+print('The new fraction is', new_numerator,'/',new_denominator,'.\n')
+
