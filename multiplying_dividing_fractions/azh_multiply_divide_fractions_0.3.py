cf3c280dbb5e40bcbc27421f42c811a8e3d462db
Armani Hayes <s20004304@students.duvalschools.org>
2019-05-13 12:30:25 -0400

    Declared Variables for the Fractions.
    Added the nuerator and denominator values for the two fractions.

---
+++ multiplying_dividing_fractions/azh_multiply_divide_fractions_0.3.py
@@ -0,0 +1,14 @@
+# Armani H., Multiplying and Dividing Fractions, 05/06/2019
+
+print("Hi. My name is Jayce. I can multiply and divide fractions. \n")
+user_name = input("What is your name? [Type your name and press ENTER.] \n")
+print("Hello,", user_name, "how are you today?")
+
+print("In order for me to help you, I need to know the numerator and the denominator for both fractions. \n") 
+
+# Variables for fraction 0.
+numerator0 = 0
+denominator0 = 0
+#Variables for fraction 1.
+numerator1 = 0
+denominator1 = 0
