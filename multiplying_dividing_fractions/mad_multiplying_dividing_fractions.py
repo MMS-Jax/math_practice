31e3635ff44ebdbe53dfbb8261a4758297357207
HeWasNumber1 <49246121+HeWasNumber1@users.noreply.github.com>
2019-05-13 12:29:17 -0400

    Declared Variables for the fractions
    Added the numerator and denominator variables for the two fractions.

--- multiplying_dividing_fractions/mad_multiplying_dividing_fractions.py
+++ multiplying_dividing_fractions/mad_multiplying_dividing_fractions.py
@@ -2,4 +2,4 @@
 
 print("Hello! Welcome to lil Fraction- Over 9000. I Will Multiply and Divide your Fractions for You \n")
 user_name = input ("what is your name? [Type your name and press ENTER.]\n")
-print("Hello, ",user_name," how are you today?" )
+print("Hello, ",user_name," how are you today? \n")
