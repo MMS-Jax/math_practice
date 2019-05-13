b0e140426713430e81c050ad253401d40af85950
Einxhel <s12658720@students.duvalschools.org>
2019-05-13 10:52:11 -0400

    Declared variables for the fraction
    Added the numerator and denominator values.

---
+++ multiplying_dividing_fractions/multiply_divide_fractions 0.3.py
@@ -0,0 +1,14 @@
+# Multiplying and Diving Fractions by Einxhel D. 05/06/19     
+                  
+print("Hello, welcome to multipying and dividing fractios by Einxhel D.\n")
+unser_name=input("Einxhel./n")
+print("Hello,",user_name,"How are you today?\n")
+
+print("For this program, i need to know the numerator and dnominator for both frctions"/n")
+
+#Variables for fraction 0.
+numerator0 = 0
+denominator0 = 0
+#VAriables for frcation 1.
+numerator1= 0
+denominator1= 0
