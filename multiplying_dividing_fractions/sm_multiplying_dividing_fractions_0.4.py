c3587107d3b9b116129822e1f56a43b98be8c339
peachhirai1109 <49244343+peachhirai1109@users.noreply.github.com>
2019-05-13 15:56:43 -0400

    Version 0.5
    User can now input values for multiplication.

--- multiplying_dividing_fractions/sm_multiplying_dividing_fractions_0.4.py
+++ multiplying_dividing_fractions/sm_multiplying_dividing_fractions_0.4.py
@@ -15,3 +15,24 @@ print("The first fraction is", numerator0, "/", denominator0, ".\n")
 numerator1 = 0
 denominator1 = 0
 print("The second fraction is", numerator1, "/", denominator1, ".\n")
+
+print("When multiplying fractions, you multiply the numerators together.\n")
+print("Then you multiply the denominators together.\n")
+
+new_numerator = numerator0 * numerator1
+new_denominator = denominator0 * denominator1
+
+print("The new fraction is", new_numerator, "/", new_denominator, ".\n")
+
+print("Now you can try!\n")
+
+numerator0 = int(input("Type in the first numerator's value.\n"))
+numerator1 = int(input("Type in the second numerator's value.\n"))
+
+denominator0 = int(input("Type in the first denominator's value.\n"))
+denominator1 = int(input("Type in the second denominator's value.\n"))
+
+new_numerator = numerator0 * numerator1
+new_denominator = denominator0 * denominator1
+
+print("The new fraction is", new_numerator, "/", new_denominator, ".\n")
--- multiplying_dividing_fractions/sm_multiplying_dividing_fractions_0.5.py
+++ multiplying_dividing_fractions/sm_multiplying_dividing_fractions_0.5.py
@@ -1,8 +1,8 @@
-# Shruthi Manikandan, Multiply and Divide Fractions, 05/06/19, version 0.5
+# Shruthi Manikandan, Multiply and Divide Fractions, 05/06/19, version 0.4
 
 print("Welcome to my program! Would you like to see a magic trick involving fractions?\n")
 user_name = input("What shall I refer to you as? (Type it in and press enter)\n")
-print("Hello", user_name, "how was your day?\n")
+print("Henlo,", user_name, "how was your day?\n")
 
 print("This program requires the numerators and denominators for both fractions.\n")
 
@@ -23,3 +23,16 @@ new_numerator = numerator0 * numerator1
 new_denominator = denominator0 * denominator1
 
 print("The new fraction is", new_numerator, "/", new_denominator, ".\n")
+print("Now you can try!\n")
+
+numerator0 = int(input("Type in the first numerator's value.\n"))
+numerator1 = int(input("Type in the second numerator's value.\n"))
+
+denominator0 = int(input("Type in the first denominator's value.\n"))
+denominator1 = int(input("Type in the second denominator's value.\n"))
+
+new_numerator = numerator0 * numerator1
+new_denominator = denominator0 * denominator1
+
+print("The new fraction is", new_numerator, "/", new_denominator, ".\n")
+
