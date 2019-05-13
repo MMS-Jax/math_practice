683eb947f3243a045817ba7a0f3973747173de56
s11492378 <49407919+s11492378@users.noreply.github.com>
2019-05-13 12:33:33 -0400

    declared variables for fractions.
    added the numerator and denominator values for each fractions.

--- NO_multiply_divide_fractions_0.3.py
+++ NO_multiply_divide_fractions_0.3.py
@@ -1,4 +1,4 @@
-# Nolan.O, Multiplying and dividing Fractions, 05/06/2019, version 0.2
+# Nolan.O, Multiplying and dividing Fractions, 05/06/2019, version 0.3
 
 print("Hello! My name is yung fraction gawd. I can multiply and devide fractions for you!\n")
 user_name = input("What is your name? [Type your name and press ENTER.]\n")
@@ -6,9 +6,9 @@ print("Hello",user_name,"how are you today?")
 
 print("For this program, I need to know the numerator and denominator for both fractions.\n")
 
-
+# Variables for fractions 0.
 numerator0 = 0
 denominator0 = 0
-
+# Variables for fractions 1.
 numerator1 = 0
 denominator1 = 0
--- Ohnimus_Nolan_multiply_divide_fractions.py
+++ Ohnimus_Nolan_multiply_divide_fractions.py
@@ -1,1 +1,3 @@
 # Nolan.O, Multiplying and dividing Fractions 05/06/2019
+print("Hello! My name is yung fraction gawd. I will help you multiply and divide fractions.\n")
+print("I will need to know the numerator and the denomenator.")
