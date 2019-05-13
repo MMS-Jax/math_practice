63a8161ad7ef81034b9cdda0d93f05a199a229a8
peachhirai1109 <49244343+peachhirai1109@users.noreply.github.com>
2019-05-13 15:32:11 -0400

    Defined Variables
    Created 4 variables for numerator and denominator of both fractions.

---
+++ multiplying_dividing_fractions/sm_multiplying_dividing_fractions_0.3.py
@@ -0,0 +1,14 @@
+# Shruthi Manikandan, Multiply and Divide Fractions, 05/06/19, version 0.3
+
+print("Welcome to my program! Would you like to see a magic trick involving fractions?\n")
+user_name = input("What shall I refer to you as? (Type it in and press enter)\n")
+print("Henlo,", user_name, "how was your day?\n")
+
+print("This program requires the numerators and denominators for both fractions.\n")
+
+# Variables for Fraction 0.
+numerator0 = 0
+denominator0 = 0
+# Variables for Fraction 1.
+numerator1 = 0
+denominator1 = 0
