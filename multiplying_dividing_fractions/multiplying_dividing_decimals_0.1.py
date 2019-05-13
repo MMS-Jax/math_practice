0f936c6198525ba44f1d35bd1a0a2e14297d4a34
DGK-Quixotic <49407968+DGK-Quixotic@users.noreply.github.com>
2019-05-13 12:47:32 -0400

    added user_name variable and input() line.
    added two lines of code to ask for the user's name,save it to a variable using input() and then printing the name back on the screen.

--- multiplying_dividing_fractions/multiplying_dividing_decimals_0.1.py
+++ multiplying_dividing_fractions/multiplying_dividing_decimals_0.1.py
@@ -1,3 +1,4 @@
 # William B, Multiplying and Dividing Decimals, 5/13/2019, Version 0.1
 
 print("Hello, welcome to Decimal-Bot 9000.  I can multiply and divide decimals for you!\n")
+print("Hello, ", user_name," how are you today?")
---
+++ multiplying_dividing_fractions/multiplying_dividing_decimals_0.2.py
@@ -0,0 +1,5 @@
+# William B, Multiplying and Dividing Decimals, 5/13/2019, Version 0.2
+
+print("Hello, welcome to Decimal-Bot 9000.  I can multiply and divide decimals for you!\n")
+user_name = input("what is your name? [type your name and press ENTER.]\n")
+print("Hello, ", user_name," how are you today?\n")
