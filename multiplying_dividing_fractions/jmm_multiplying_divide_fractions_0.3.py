5e0030969a066623d6f64d1be6b68c1be888b1a4
Jonathan Meister <49206594+JonathanHatesMathForeverUntillDeath@users.noreply.github.com>
2019-05-13 12:24:40 -0400

    Create jmm_multiplying_divide_fractions_0.3.py
    added variables for fractions.

---
+++ multiplying_dividing_fractions/jmm_multiplying_divide_fractions_0.3.py
@@ -0,0 +1,13 @@
+# Jonathan M. , Multiplying and dividing fractions, 05/06/2019, version 0.3
+print("Hello! My name is Satan. I can multiply and divide fractions\n")
+print("To multiply and divide fractions I need to know the numerator and the denominator")
+user_name = input("What is you name? [Type your name here and press enter.]\n")
+print("Hello,", user_name, "how are you today?")
+
+print("for this program i need to know the numertor, and the denominator for both fractions. \n")
+#number variables for fraction A
+numeratorA = 0 
+denominatorA = 0
+#number variables for fraction B
+numeratorB = 0
+denominatorB = 0
