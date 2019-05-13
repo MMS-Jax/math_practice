a2bd3d0b3906645a092e08a98e56a04fae093010
Overpowered112 <s11524089@students.duvalschools.org>
2019-05-13 10:00:39 -0400

    added variable and input line
    used 2 lines of code for users name and saved it to a variable using input and then showing his name on the screen.

--- multiplying_dividing_fractions/sbm multiplying_dividing_fractions_0.2.py
+++ multiplying_dividing_fractions/sbm multiplying_dividing_fractions_0.2.py
@@ -1,4 +1,4 @@
-# Sasha M., Multiplying and Dividing Fractions, 05/10/2019, version 0.1
+# Sasha M., Multiplying and Dividing Fractions, 05/10/2019, version 0.2
 
 print("Hello, welcome to fraction bobster   I can multiply and divide fractions for you!\n")
 user_name = input("What is your name? [Type your name and press ENTER.]\n")
