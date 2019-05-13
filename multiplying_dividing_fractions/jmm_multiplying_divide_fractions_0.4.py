90002e493453477877f5fb5c6f0c3dc2c28bd0e1
Jonathan Meister <49206594+JonathanHatesMathForeverUntillDeath@users.noreply.github.com>
2019-05-13 12:37:36 -0400

    version 0.4
    added print commands.

--- multiplying_dividing_fractions/jmm_multiplying_divide_fractions_0.4.py
+++ multiplying_dividing_fractions/jmm_multiplying_divide_fractions_0.4.py
@@ -1,11 +1,12 @@
-# Jonathan M. , Multiplying and dividing fractions, 05/06/2019, version 0.3
+# Jonathan M. , Multiplying and dividing fractions, 05/06/2019, version 0.4
 print("Hello! My name is Satan. I can multiply and divide fractions\n")
 print("To multiply and divide fractions I need to know the numerator and the denominator")
 user_name = input("What is you name? [Type your name here and press enter.]\n")
 print("Hello,", user_name, "how are you today?")
 
 print("for this program i need to know the numertor, and the denominator for both fractions. \n")
-#number variables for fraction A
+
+# number variables for fraction A
 numeratorA = 0 
 denominatorA = 0
 print("the first fraction is" , numerator0, "/" ,denominator0, ".\n")
