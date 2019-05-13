46bfe5c4f481cc18fbcd34cd0b2c96afcc8065f4
DaBoyLaDerrick <49207692+DaBoyLaDerrick@users.noreply.github.com>
2019-05-13 14:25:19 -0400

    Update dj_Multiplying_Dividing_Fractions_0.1py.py

--- multiplying_dividing_fractions/dj_Multiplying_Dividing_Fractions_0.1py.py
+++ multiplying_dividing_fractions/dj_Multiplying_Dividing_Fractions_0.1py.py
@@ -1,3 +1,5 @@
 # Derrick J., Multiplying and Dividing fractions, 05/06/2019, verison 0.0
 
 print("Hello, welcome to Fraction-Bot 9000.  I can multiply and divide fractions for you!\n")
+user_name = input("What is your name? [Type your name and press ENTER.\n")
+print("Hello", user_name," how are you today?")
