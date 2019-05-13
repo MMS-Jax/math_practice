ba3be1a1f170675cf3f940e2a7c4b4cd0381e8e8
Aiden <s11573029@duvalschools.org>
2019-05-13 10:00:19 -0400

    Added First Print() String
    Printed welcome and what this program does.

---
+++ multiplying_dividing_fractions/AAEmultiplying_dividing_fractions_0.1.py
@@ -0,0 +1,3 @@
+# Aiden E., Multiplying and Dividing Fractions, 05/10/2019, version 0.1
+
+print("Hello, this is the most inconvenient calculator! I can only multiply and divide fractions which will not help you with your homework.\n")
