78611a455cd3445da57a2dd126666c80254c9a2e
Mljs1158 <49206695+Mljs1158@users.noreply.github.com>
2019-05-14 12:51:10 -0400

    finished program
    finished coding the fraction program.

---
+++ multiplying_dividing_fractions/mlj_multiply_divide_fractions_0.7.py
@@ -0,0 +1,45 @@
+# Marcellus J., Multiplying and Dividing Fractions, 05/06/2019, version 0.7
+
+print("Hello, My name is QuickmathsBot. I can Multiply and Divide fractions fro you!\n")
+user_name = input("What's your name? [Type your name and press ENTER.\n")
+print("Hello,", user_name,"how are you today?\n")
+
+print("For this program, I need to know the numerator and the denominator for both fractions.\n")
+
+# Variables for fraction 0. 
+numerator0 = 5
+denominator0 = 7
+print("The first fraction is", numerator0,"/",denominator0,".\n")print("The new fraction is", new_numerator,"/",new_denominator,".\n")
+
+# This is where the division of the fractions will start. You will need to change this code!
+pie = 22 / 7
+print(pie)
+print("To divide a fraction, you will multiply using the reciprocal or inverse of the second fraction.\n")
+
+# Variables for fraction 1. 
+numerator1 = 9
+denominator1 = 17
+print("The first fraction is", numerator1,"/",denominator1,".\n")
+
+print("When multiplying fractions you multiply the two numerators together.\n")
+print("Then you will multiply the two denominators together.\n")
+
+new_numerator = numerator0 * numerator1
+new_denominator = denominator0 * denominator1
+
+print("The new fraction is", new_numerator,"/",new_denominator,".\n")
+denominator1 = int(input("Type the second denominator and press enter.\n"))
+
+print("The first fraction is", numerator0,"/",denominator0,".\n")
+print("The second fraction is", numerator1,"/",denominator1,".\n")
+
+new_numerator = numerator0 * numerator1
+new_denominator = denominator0 * denominator1
+
+print("The new fraction is", new_numerator,"/",new_denominator,".\n")
+
+# This is where the division of the fractions will start. You will need to change this code!
+pie = 22 / 7
+print(pie)
+print("To divide a fraction, you will multiply using the reciprocal or inverse of the second fraction.\n")
+
---
+++ multiplying_dividing_fractions/mlj_multiply_divide_fractions_0.9.py
@@ -0,0 +1,65 @@
+# Marcellus J., Multiplying and Dividing Fractions, 05/06/2019, version 0.9
+
+print("Hello, My name is QuickmathsBot. I can Multiply and Divide fractions fro you!\n")
+user_name = input("What's your name? Type your name and press ENTER.\n")
+print("Hello," , user_name , "how are you today?\n")
+print("For this program, I need to know the numerator and the denominator for both fractions.\n")
+# Variables for fraction 1 
+numerator0 = int(input("Type the first numerator and press ENTER \n"))
+denominator0 = int(input("Type the first denominator and press ENTER \n"))
+
+print("The first fraction is", numerator0,"/",denominator0,".\n")
+# Variables for fraction 2
+numerator1 = int(input("Type the second numerator and press ENTER \n"))
+denominator1 = int(input("Type the second denominator and press ENTER \n"))
+print("The first fraction is", numerator1,"/",denominator1,".\n")
+new_numerator = numerator0 * numerator1
+new_denominator = denominator0 * denominator1
+print("The new fraction is", new_numerator,"/",new_denominator,".\n")
+decimal_version = new_numerator / new_denominator
+print("The decimal version of that is" , decimal_version , "\n")
+xD = input()
+# This is where the division of the fractions will start. You will need to change this code!
+pie = 22 / 7
+print(pie)
+print("To divide a fraction, you will multiply using the reciprocal or inverse of the second fraction.\n")
+
+# Variables for fraction 1. 
+numerator1 = 0
+denominator1 = 0
+print("The first fraction is", numerator1,"/",denominator1,".\n")
+
+print("When multiplying fractions you multiply the two numerators together.\n")
+print("Then you will multiply the two denominators together.\n")
+
+new_numerator = numerator0 * numerator1
+new_denominator = denominator0 * denominator1
+
+print("The new fraction is", new_numerator,"/",new_denominator,".\n")
+denominator1 = int(input("Type the second denominator and press enter.\n"))
+
+print("The first fraction is", numerator0,"/",denominator0,".\n")
+print("The second fraction is", numerator1,"/",denominator1,".\n")
+
+new_numerator = numerator0 * numerator1
+new_denominator = denominator0 * denominator1
+
+print("The new fraction is", new_numerator,"/",new_denominator,".\n")
+
+# This is where the division of the fractions will start. You will need to change this code!
+
+print("To divide a fraction, you will multiply using the reciprocal or inverse of the second fraction.\n")
+divide_numerator0 = int(input("Type the first numerator and press ENTER \n"))
+
+divide_denominator0 = int(input("Type the first denominator and press ENTER \n"))
+
+print("The first fraction is" , divide_numerator0 , "/" , divide_denominator0 , "\n")
+
+divide_numerator1 = int(input("Type the second numerator and press ENTER \n"))
+
+divide_denominator1 = int(input("Type the second denominator and press ENTER \n"))
+
+print("The second fraction is" , divide_numerator1 , "/" , divide_denominator1 , "\n")
+new_divide_numerator = divide_numerator0 * divide_denominator1
+new_divide_denominator = divide_denominator0 * divide_numerator1
+print("the new fraction is" , new_divide_numerator , "/" , new_divide_denominator , "\n")
