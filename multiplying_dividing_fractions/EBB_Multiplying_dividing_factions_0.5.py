9bedcefbbba8ef061b112675420f146dcb3527ee
EthanDaSavage <s11279171@student.duvalschools.org>
2019-05-14 15:59:18 -0400

    Verison .5 added new fractions
    I added the new_numerator and new_denominator variable. Then added a string to print the new fraction for the user.

--- multiplying_dividing_fractions/EBB_Multiplying_dividing_factions_0.5.py
+++ multiplying_dividing_fractions/EBB_Multiplying_dividing_factions_0.5.py
@@ -1,4 +1,4 @@
-# Ethan B., Multiplying and Dividing Fractions, 05/06/2019, version 0.3A
+# Ethan B., Multiplying and Dividing Fractions, 05/06/2019, version 0.5
 
 print("Hello, welcome to the Fraction-Bomber 3000.   I will multiply and divide those hard fractions for you!\n")
 user_name = input("What is your name? [Type your name and press ENTER.]\n")
