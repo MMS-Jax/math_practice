0ccd230af91c2a577782d06258e6648b29d92c9a
arianna015 <49210282+arianna015@users.noreply.github.com>
2019-05-14 10:02:26 -0400

    Version 0.4
    Added to print() lines to print the two fractions on the screen to verify they are correct.

--- multiplying_dividing_fractions/multiplying_dividing_fractions_0.4.py
+++ multiplying_dividing_fractions/multiplying_dividing_fractions_0.4.py
@@ -1,4 +1,4 @@
-# Ryan K., Multiplying and Dividing Fractions, 05/06/2019, version 0.4
+# Arianna V., Multiplying and Dividing Fractions, 05/10/2019, version 0.4
 
 print("Hello, welcome to Fraction-Bot 9000.   I can multiply and divide fractions for you!\n")
 user_name = input("What is your name? [Type your name and press ENTER.]\n")
@@ -15,11 +15,3 @@ print("The first fraction is", numerator0,"/",denominator0,".\n")
 numerator1 = 0
 denominator1 = 0
 print("The second fraction is", numerator1,"/",denominator1,".\n")
-
-print("When multiplying fractions you multiply the two numerators together.\n")
-print("Then you will multiply the two denominators together.\n")
-
-new_numerator = numerator0 * numerator1
-new_denominator = denominator0 * denominator1
-
-print("The new fraction is", new_numerator,"/",new_denominator,".\n")
