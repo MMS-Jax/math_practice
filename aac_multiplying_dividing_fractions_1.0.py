341031ae646a5f686e46fa7f91e510b32b05b87a
zguy1548 <s11369642@students.duvalschools.org>
2019-05-14 16:01:44 -0400

    added code to divide
    added the code to where you can divide fractions

--- aac_multiplying_dividing_fractions_1.0.py
+++ aac_multiplying_dividing_fractions_1.0.py
@@ -1,4 +1,4 @@
-# Alexzander C., Multiplying and Dividing Fractions, 05/06/2019, version 0.7
+# Alexzander C., Multiplying and Dividing Fractions, 05/06/2019, version 1.0
 
 print("Hello, welcome to Fraction-Bot 9000.   I can multiply and divide fractions for you!\n")
 user_name = input("what is your name. [Type your name herte and press ENTER.]\n")
