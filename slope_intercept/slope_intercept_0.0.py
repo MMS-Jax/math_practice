782aa8261561f26744433b2dc930aa375d91b54a
Kelley <kelleyr@duvalschools.org>
2019-05-14 17:12:05 -0400

    Update slope_intercept_0.0.py

--- slope_intercept/slope_intercept_0.0.py
+++ slope_intercept/slope_intercept_0.0.py
@@ -1,32 +1,45 @@
-# Ryan K., Slope-Intercept Ordered Pair Solver, 05/10/2019, v0.0
+# Ryan K., Slope-Intercept Ordered Pair Solver, 05/14/2019, v0.0
 import time # Imports a LIBRARY of methods related to time. 
 
 print("This program will solve slope-intercept equations in the form of y = mx + b.\n")
-print("m represents the slope of the line.  b represents the [REPLACE] of the line.\n")
+print("m represents the slope of the line.  b represents the y-intercept of the line.\n")
 print("After inputting a value for x, this program will solve for y.  Then five ordered pairs will be calculated and printed to the screen.\n")
-time.sleep(5) # This is a METHOD that pauses the program for the number of seconds indicated.  Change this to 10.
+time.sleep(10) # This is a METHOD that pauses the program for the number of seconds indicated.  Make this code pause for three seconds.  
+
+# The first thing your code must do is determine whether the SLOPE is given as a whole number or a fraction.  
+
+# If it is a WHOLE NUMBER, you can save the value as an int to a variable called m or slope. 
+# If it is a FRACTION, you will need to convert it to a float using DIVISION then save the value to a variable called m. 
+
+if : # You need to figure out the code needed here if the slope is a whole number. 
+    # Execute this code if the slope is a whole number. 
+    # Have the user input() an integer value for the slope and save it to the variable m.
+    # Make sure to print() the slope back to the screen so the user knows it is correct. 
+else:  
+    # Execute the code in this block if the slope is a fraction. 
+    # You will need to know the NUMERATOR and DENOMINATOR for the slope.  
+    # You must use divison, which is the / character, to convert from a fraction to a decimal.  
+    # Make sure to print() the slope back to the screen so the user knows it is correct. 
+
+# Pause the program for five seconds here so the user can read the information. 
 
 
-whole_or_fraction = int(input("Is the slope [m] a whole number or a fraction?  Enter 0 for a whole number.  Enter 1 for a fraction.\n"))
-
-if whole_or_fraction == 0: # == translates to "is this equal to" which is different than = which means "make this equal to".
-    m = 0 # Makes the slope an int data type. 
-    print("Ok, the slope will be stored as an integer.\n")
-    m = input("What is the slope?  [Type a number and press ENTER.]\n")
-    print("The slope entered is:", m)
-else: 
-    numerator = int(input("What is the numerator of the slope?\n"))
-    denominator = int(input("What is the denominator of the slope?\n"))
-    m = numerator / denominator # Calculates the slope with division. 
-    print("The slope entered is:", m) # Can you figure out how to print just two decimal places?
-
-# Add the code here for the user to input the y-intercept for the equation. Think about the code used for the slope and modify it. 
-# Make sure to print the y-intercept back on the screen to make sure it's correct. 
-
-x = int(input("The last piece of information needed is a value for x.  Please type an integer value for x and press ENTER.\n"))
-print("You entered:", x)
+# Next you need the user to input() the y-intercept of the equation which is represented by the variable b. 
+# Make sure to print() the y-intercept back to the screen. 
+# Pause the program for five seconds here so the user can read the information. 
 
 
+# After you have the y-intercept you need the user to enter a starting value for x. 
+# You will need to use input() and store the value as an int to the variable called x. 
+# Make sure to print the variable back to the screen so the user knows it is correct. 
+# Pause the program for five seconds here so the user can read the information. 
+
+
+while : # Make this loop run 5 times. 
+    y = ((m * x) + b) # This will solve for y using the slope, y-intercept, and x values entered earlier. 
+    print("The ordered pair is (",x,",",y,").\n")  # This will print the ordered pair to the screen using (x, y) form.
+    # Increment / decrement x by 1 each time through the loop. 
+    # Increase your loop control variable by 1.  
 
 
 
