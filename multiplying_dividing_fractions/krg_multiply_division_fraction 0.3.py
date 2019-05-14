39cada0d74d08b0be0d2ede154c9aeee51cc7c1d
Kennedy122 <49565896+Kennedy122@users.noreply.github.com>
2019-05-14 12:25:46 -0400

    Version 0.4
    Added print() lines to print the two fractions on the screen to verify they are correct.

--- multiplying_dividing_fractions/krg_multiply_division_fraction 0.3.py
+++ multiplying_dividing_fractions/krg_multiply_division_fraction 0.3.py
@@ -1,4 +1,4 @@
-# Ryan K., Multiplying and Dividing Fractions, 05/05/2019 version,0.3
+# Ryan K., Multiplying and Dividing Fractions, 05/05/2019,version 0.3
 
 print("hello! My Name Is MathBot and I Will multiply and divide fractions.\n")
 print("In Order to multiply fractions, you need too  know the numerator and denomerator of the fractions.")
@@ -8,8 +8,11 @@ print("Hello,",user_name," how are you today?")
 print("For this program, I need to know the numerator and the denominator for both fractions. \n")
 
 # Variables for fraction 0.
-numerator0= 0
-denominator0= 0
+numerator0= int(input("Type the first numerator and press enter. \n"))
+denominator0= int(input("Type the first denominator and press enter. \n"))
+print("The first fraction is", numerator0,"/",denominator0,".\n" )
+
 # variables for fraction 1.
-numerator1=0
-denominator1=0
+numerator1= int(input("Type the second numerator and press enter.\n"))
+denominator1= int(input(" Tpe the second denominator and press enter.\n"))
+print("The second fraction is", numerator1,"/",denominator1,".\n" )
