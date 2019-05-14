6d9735eae66d7765e7633b4ebb76d9d0df6703dc
OhOnJah <Jacob.osterland123@gmail.com>
2019-05-14 16:00:39 -0400

    added first print() string
    Printed some basic instructions for the user.

---
+++ multiplying_dividing_fractions/Multiplaying and Dividing fractions 0.1.py
@@ -0,0 +1,3 @@
+# Jacob O., Multiplying and Dividing Fractions, 05/14/2019, version 0.1
+
+print("What's up G, I can multiplay and divide fractions for you instead of you getting frustrated in math class.\n")
