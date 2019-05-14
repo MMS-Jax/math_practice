8ad8bac369b26ec5ed876a5ef8304f4df5380634
Aiden <s11573029@duvalschools.org>
2019-05-14 09:46:25 -0400

    Revert "Declared Fraction Variables."
    This reverts commit b9306c0177cbc402faa79d3946a70f48ff1f7287.

--- multiplying_dividing_fractions/AAEmultiplying_dividing_fractions_0.2.py
+++ multiplying_dividing_fractions/AAEmultiplying_dividing_fractions_0.2.py
@@ -2,12 +2,4 @@
 
 print("Hello, this is the most inconvenient calculator! I can only multiply and divide fractions which will not help you with your homework.\n")
 user_name = input("What is your name? [Type your name and press ENTER].\n")
-print("Hello,",user_name,"how are you doing today?\n")
-
-print("For this program, I need to know the numerator and denominator for both fractions.\n")
-
-numerator0 = 0
-denominator0 = 0
-
-numerator1 = 0
-denominator1 = 0
+print("Hello,",user_name,"how are you doing today?")
