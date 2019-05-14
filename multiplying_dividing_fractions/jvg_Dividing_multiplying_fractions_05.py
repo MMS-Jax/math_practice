f1738efea6451f9dcc3b185a35f63c6848eedf85
Jewel2143 <s11586674@student.duvalschools.org>
2019-05-14 12:50:52 -0400

    Jvg_math_05
    multiplying and dividing fractions

---
+++ multiplying_dividing_fractions/jvg_Dividing_multiplying_fractions_05.py
@@ -0,0 +1,31 @@
+# Julianna G., Muliplying and Dividing fractions, 05/06/2019
+
+print("Hi, this is for multiplying and dividing fractions\n")
+user_name = input("What is your name? [Type you name and press enter.\n ")
+print("Hello", user_name,"how are you today")
+
+print("For this program, I need to know the numerator and the denominator for both fractions.\n")
+
+# Variables for fraction 0.
+numertor0 = 0
+denomnator0 = 0
+print("The frist fraction is", numerator0,"/",denominator0,".\n")
+
+# Variables for fraction 1.
+numerator1 = 0
+denominator1 = 0
+print("The second fraction is", numerator1,"/",denominator1,".\n")
+
+print("What multiplying fractions you multiply the two numerators together.\n")
+print("Then you will multiply the two denominators together. \n")
+
+numerator0 = int(input("Type the first denominator and press enter"))                      
+numerator1 = int(input("Type the second denominator and press enter"))                      
+
+denominator0 = int(input("Type the first denominator and press enter"))                      
+denominator1 = int(input("Type the second denominator and press enter"))                      
+
+print("The first fraction is", numerator1,"/",denominator1,".\n")
+print("The second fraction is", numerator1,"/",denominator1,".\n")
+
+
