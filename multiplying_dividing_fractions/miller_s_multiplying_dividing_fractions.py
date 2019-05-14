66b6529a1ee80ec22f06cd9bd6443acaaf5cc327
mintgreen18 <49650916+mintgreen18@users.noreply.github.com>
2019-05-14 12:25:05 -0400

    Update miller_s_multiplying_dividing_fractions.py

--- multiplying_dividing_fractions/miller_s_multiplying_dividing_fractions.py
+++ multiplying_dividing_fractions/miller_s_multiplying_dividing_fractions.py
@@ -9,6 +9,9 @@ print("in order to multiply or divide fractions i need to know the numerator and
 # Variables for fraction 0.
 numerator0 = 0
 denominator0 = 0
+
+print("The first fraction is", numerator0,"/",denominator0,".\n")
 # Variables for fraction 1.
 numerator1 = 0
 denominator1 = 0
+print("The first fraction is", numerator1,"/",denominator1,".\n")
