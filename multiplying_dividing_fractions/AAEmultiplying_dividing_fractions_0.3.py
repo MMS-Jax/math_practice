b9306c0177cbc402faa79d3946a70f48ff1f7287
Aiden <s11573029@duvalschools.org>
2019-05-14 09:45:54 -0400

    Declared Fraction Variables.
    +Added numerator and denominators values for the fractions.

--- multiplying_dividing_fractions/AAEmultiplying_dividing_fractions_0.3.py
+++ multiplying_dividing_fractions/AAEmultiplying_dividing_fractions_0.3.py
@@ -1,13 +1,13 @@
-# Aiden E., Multiplying and Dividing Fractions, 05/10/2019, version 0.2
+# Aiden E., Multiplying and Dividing Fractions, 05/10/2019, version 0.3
 
 print("Hello, this is the most inconvenient calculator! I can only multiply and divide fractions which will not help you with your homework.\n")
 user_name = input("What is your name? [Type your name and press ENTER].\n")
 print("Hello,",user_name,"how are you doing today?\n")
 
 print("For this program, I need to know the numerator and denominator for both fractions.\n")
-
+# Variables for fraction 0.
 numerator0 = 0
 denominator0 = 0
-
+# Variables for fraction 1.
 numerator1 = 0
 denominator1 = 0
