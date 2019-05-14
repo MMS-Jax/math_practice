a630f9cd4b3fe3525bee804e9ab1eaa8805d92f8
EthanDaSavage <s11279171@student.duvalschools.org>
2019-05-14 15:46:18 -0400

    Create EBB_Multiplying_dividing_factions_0.4.py
    Saved the .4 version of code.

---
+++ multiplying_dividing_fractions/EBB_Multiplying_dividing_factions_0.4.py
@@ -0,0 +1,17 @@
+# Ethan B., Multiplying and Dividing Fractions, 05/06/2019, version 0.3A
+
+print("Hello, welcome to the Fraction-Bomber 3000.   I will multiply and divide those hard fractions for you!\n")
+user_name = input("What is your name? [Type your name and press ENTER.]\n")
+print("Hello,", user_name,"how are you today?\n")
+
+print("for this program, I will need to know the numerator and the denominator fro both fractions.\n")
+
+# Variables for fraction 0
+numerator0 = 0
+denominator0 = 0
+print("The first fraction is",numerator0, "/", denominator0, ". \n" )
+
+# Variables for fraction 1
+numerator1 = 0
+denominator1 = 0
+print("The second fraction is",numerator1, "/", denominator1, ". \n" )
