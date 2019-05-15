c053023e758209ba37508ddcfda0d27b67f05ca7
HeWasNumber1 <49246121+HeWasNumber1@users.noreply.github.com>
2019-05-15 12:31:49 -0400

    version 0.4
    added first two fractions

--- multiplying_dividing_fractions/mad_multiplying_dividing_fractions_0.4.py
+++ multiplying_dividing_fractions/mad_multiplying_dividing_fractions_0.4.py
@@ -1,4 +1,4 @@
-# Mason D., Multiplying and Dividing Fractions, 5/6/2019, version 0.1
+# Mason D., Multiplying and Dividing Fractions, 5/6/2019, version 0.4
 
 print("Hello! Welcome to lil Fraction- Over 9000. I Will Multiply and Divide your Fractions for You \n")
 user_name = input ("what is your name? [Type your name and press ENTER.]\n")
@@ -14,4 +14,4 @@ print("The first fraction is", numerator0,"/", denominator0, ".\n")
 # Variables for fraction 1.
 numerator1 = 0
 denominator1 = 0
-print("The first fraction is", numerator1,"/", denominator1, ".\n")
+print("The second fraction is", numerator1,"/", denominator1, ".\n")
