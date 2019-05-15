510dd83d63dbd471a3930b837504f9e59fc19695
fattocattos <49206622+fattocattos@users.noreply.github.com>
2019-05-15 12:27:14 -0400

    finished version

---
+++ multiplying_dividing_fractions/d_k_multiplying_dividing_fractions_1.0.py
@@ -0,0 +1,68 @@
+# Kailey D., Multiplying and Dividing Fractions, 05/06/2019, version 1.0
+
+print("Hello! My name is FattoCattoPugFraction and I will multiply and Divide Fractions for you.\n")
+user_name = input("What is your name? [ Type your name and press ENTER.]\n")
+print("Hello, ", user_name," how are you today?\n")
+
+print("In order to find the answer, you need to know the numerator and the denominator for both fractions.\n")
+
+# Variables for fraction 0.
+numerator0 = 0
+denominator0 = 0
+print("The first fraction is ", numerator0, "/",denominator0,".\n")
+
+# Variables for fraction 1.
+numerator1 = 0
+denominator1 = 0
+print("The second fraction is ", numerator1, "/",denominator1,".\n")
+
+print("Whenever you multiply fractions you multiply the two numerators together.\n")
+print("Next you are going to multiply the two denominators together.\n")
+
+numerator0 = int(input("Type the first numerator then press enter.\n"))
+numerator1 = int(input("Type the second numerator then press enter.\n"))
+
+denominator0 = int(input("Type the first denominator then press enter.\n"))
+denominator1 = int(input("Type the second denominator then press enter.\n"))
+
+print("The first fraction is", numerator0,"/",denominator0,".\n")
+print("The second fraction is", numerator1,"/",denominator1,".\n")
+
+new_numerator = numerator0 * numerator1
+new_denominator = denominator0 * denominator1
+
+print("Your new fraction is", new_numerator,"/", new_denominator,".\n")
+
+# this is where the division of the fractions will start.
+pie = 22 / 7
+print(pie)
+print("In order to divide a fraction, you will multiply using the inverse of the second fraction.\n")
+
+print("To find the answer we are going to need to know the numerator and the denominator for both fractions.\n")
+
+# Variables for fraction 0.
+numerator0 = 0
+denominator0 = 0
+print("The first fraction is ", numerator0, "/",denominator0,".\n")
+
+# Variables for fraction 1.
+numerator1 = 1
+denominator1 = 1
+print("The second fraction is ", numerator1, "/",denominator1,".\n")
+
+print("Whenever you divide fractions, you multiply the Numerator of the first fraction by the Denominator of the second fraction.\n")
+print("Next you multiply the Denominator of the first fraction by the Numerator of the second fraction.\n")
+
+numerator0 = int(input("Type the first numerator then press enter.\n"))
+numerator1 = int(input("Type the second numerator then press enter.\n"))
+
+denominator0 = int(input("Type the fist denominator then press enter.\n"))
+denominator1 = int(input("Type the second denominator then press enter.\n"))
+
+print("The first fraction is", numerator0,"/",denominator0,".\n")
+print("The second fraction is", numerator1,"/",denominator1,".\n")
+
+new_numerator = numerator0 * denominator1
+new_denominator = numerator1 * denominator0
+
+print("Your new fraction is", new_numerator,"/",new_denominator,".\n")
