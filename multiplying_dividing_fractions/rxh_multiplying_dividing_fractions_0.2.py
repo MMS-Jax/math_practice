6fb9a42ace0c3337953a6b172a234fd92db8193d
minecraftepic <rxhendricks05@live.com>
2019-05-15 11:31:05 -0400

    Added input for username and output
    Added username input leading into a output including the username.

---
+++ multiplying_dividing_fractions/rxh_multiplying_dividing_fractions_0.2.py
@@ -0,0 +1,7 @@
+#Roman H, Multiplying and Dividing Fractions, 05/15/2019, version 0.2
+
+print("Hey, i'm the fraction man, I do the stuff with multiply and divide the fraction.\n")
+
+user_name = input("You gotta name? [type it in then press ENTER.]\n")
+
+print("Yo, ", user_name,"whats good?")
