cb8fd5acbe01ed56cf86a53e32691c5e09dedb81
Elon Musk <49207612+youngdashboi@users.noreply.github.com>
2019-05-15 13:54:23 -0400

    first version of program
    this is a reupload because i messed up originally

---
+++ multiplying_dividing_fractions/crp_multiplying_and_dividing_fractions_0.0.py
@@ -0,0 +1,6 @@
+ # Powell C., Multiplying and Divide Fractions, 05/06/2019 Ver 0.6
+
+print("Hello, my name is Youngmathboi. I will multiply and divide fractions for you.\n")
+user_name = input("What is your name? [Type your name and press ENTER.\n")
+print("Hello,", user_name,"how are you today?\n")
+print("In order for me to do that i need to know the numerator and the denominator of the equation.\n")
