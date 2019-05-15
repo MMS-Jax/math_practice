d496c240443dd46fe72f09ac173825d9e495f6e3
Overpowered112 <s11524089@students.duvalschools.org>
2019-05-15 15:36:45 -0400

    added input values for numerator and denomenator
    told the user to type thefraction and press enter

--- multiplying_dividing_fractions/sbm multiplying_dividing_fractions_0.6.py
+++ multiplying_dividing_fractions/sbm multiplying_dividing_fractions_0.6.py
@@ -19,6 +19,15 @@ print("The second fraction is", numerator1,"/",denominator1,".\n")
 print("When multiplying fractions you multiply the two numerators together.\n")
 print("Then you will multiply the two denominators together.\n")
 
+numerator0 = int(input("Type the first numerator and press enter.\n")) 
+numerator1 = int(input("Type the second numerator and press enter.\n"))
+
+denominator0 = int(input("Type the first denominator and press enter.\n"))
+denominator1 = int(input("Type the second denominator and press enter.\n"))
+
+print("The first fraction is", numerator0,"/",denominator0,".\n")
+print("The second fraction is", numerator1,"/",denominator1,".\n")
+
 new_numerator = numerator0 * numerator1
 new_denominator = denominator0 * denominator1
 
