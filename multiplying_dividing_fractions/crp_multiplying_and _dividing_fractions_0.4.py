15f4cf9e0755cb9d25d12e4160a6690a688cbfbc
Elon Musk <49207612+youngdashboi@users.noreply.github.com>
2019-05-15 14:07:41 -0400

    Ver 0.4
    Added explanation to clarify process

---
+++ multiplying_dividing_fractions/crp_multiplying_and _dividing_fractions_0.4.py
@@ -0,0 +1,20 @@
+# Powell C., Multiplying and Divide Fractions, 05/06/2019 Ver 0.4
+
+print("Hello, my name is Youngmathboi. I will multiply and divide fractions for you.\n")
+user_name = input("What is your name? [Type your name and press ENTER.\n")
+print("Hello,", user_name,"how are you today?\n")
+print("In order for me to do that i need to know the numerator and the denominator of the equation.\n")
+
+
+# Variables for Fraction 0.
+numerator0 = 0
+denominator0 = 0
+print("The first fraction is", numerator0,"/", denominator0,".\n")
+
+# Variables for Fraction 1
+numerator1 = 0
+denominator1 = 0
+print("The second fraction is", numerator1,"/", denominator1,".\n")
+
+print("When multiplying fractions you multiply the two numerators together.\n")
+print("Then you will multiply the two denominators together.\n")
