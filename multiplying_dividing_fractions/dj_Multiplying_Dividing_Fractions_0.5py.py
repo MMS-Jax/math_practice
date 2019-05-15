1910f5f2a88238b42211b7470985bb6dde06f1e1
DaBoyLaDerrick <49207692+DaBoyLaDerrick@users.noreply.github.com>
2019-05-15 14:11:50 -0400

    Added varibles for new fraction.
    Declared new_numerator and new_denominator, also wrote the code to multiply the two fractions together

--- multiplying_dividing_fractions/dj_Multiplying_Dividing_Fractions_0.5py.py
+++ multiplying_dividing_fractions/dj_Multiplying_Dividing_Fractions_0.5py.py
@@ -1,4 +1,4 @@
-# Derrick J., Multiplying and Dividing fractions, 05/06/2019, verison 0.4
+# Derrick J., Multiplying and Dividing fractions, 05/06/2019, verison 0.5
 
 print("Hello, welcome to Fraction-Bot 9000.  I can multiply and divide fractions for you!\n")
 user_name = input("What is your name? [Type your name and press ENTER.]\n")
