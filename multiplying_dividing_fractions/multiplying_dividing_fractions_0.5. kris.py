c223fb15830e9aab04f715dc0c1b8dfed2db89da
imnotcool1 <49570771+imnotcool1@users.noreply.github.com>
2019-05-15 15:45:45 -0400

    added varibles for the new fraction

--- multiplying_dividing_fractions/multiplying_dividing_fractions_0.5. kris.py
+++ multiplying_dividing_fractions/multiplying_dividing_fractions_0.5. kris.py
@@ -1,4 +1,4 @@
-# kris m, mulitiplying and dividing fractions 05/09/2019 virsion 0.4
+# kris m, mulitiplying and dividing fractions 05/09/2019 virsion 0.5
 
 print("hello lets do some dividing   i can help you divid and mulitiply fractions so you can get good grades in math class!\n")
 user_name = input("what is your name going to be? [type what your name is then press ENTER.]\n")
