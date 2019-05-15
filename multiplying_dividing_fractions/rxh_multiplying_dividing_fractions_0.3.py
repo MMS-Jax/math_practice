74ba7b7e58702dafe78b983fdf19389eb1412fce
minecraftepic <rxhendricks05@live.com>
2019-05-15 11:36:35 -0400

    Declared Fraction Variables
    Added the numerator and denominator variables for the two fractions.

---
+++ multiplying_dividing_fractions/rxh_multiplying_dividing_fractions_0.3.py
@@ -0,0 +1,16 @@
+#Roman H, Multiplying and Dividing Fractions, 05/15/2019, version 0.3
+
+print("Hey, i'm the fraction man, I do the stuff with multiply and divide the fraction.\n")
+
+user_name = input("You gotta name? [type it in then press ENTER.]\n")
+
+print("Yo, ", user_name,"whats good?")
+
+print("Ok, so I'm gonna need the numerator and the denominator for both fractions.\n")
+
+# Variables for fraction 0
+numerator0 = 0
+denominator0 = 0
+# Variables for fraction 1
+numerator1 = 0
+denominator1 = 0
