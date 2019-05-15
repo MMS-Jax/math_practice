324a2a33d4fcf0f40ea4bb6b2d515f6ba30ff6c6
ABot56 <ABot56@github.com>
2019-05-15 14:30:08 -0400

    Update criminger_eddie_multiplying_dividing_fractions_1.0.py

--- criminger_eddie_multiplying_dividing_fractions_1.0.py
+++ criminger_eddie_multiplying_dividing_fractions_1.0.py
@@ -1,4 +1,4 @@
-# Eddie c. Multiplying and Dividing Fractions 5-9-19 version 0.7
+# Eddie c. Multiplying and Dividing Fractions 5-9-19 version 1.0
 
 print("Hello, welcome to Fraction-Bot 9000.   I can multiply and divide fractions for you!\n")
 user_name = input("What is your name? [Type your name and press ENTER.]\n")
