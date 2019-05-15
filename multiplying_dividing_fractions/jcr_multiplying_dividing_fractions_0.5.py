44cc71c694c92f521bf595a5f356ad4e76ac4b47
Jadin Rose <s20051643@students.duvalschools.org>
2019-05-15 14:24:54 -0400

    Divided fractions.
    So I basically repeated what I did when I was multiplying the fraction. But of course we divided instead of multiplying.

--- multiplying_dividing_fractions/jcr_multiplying_dividing_fractions_0.5.py
+++ multiplying_dividing_fractions/jcr_multiplying_dividing_fractions_0.5.py
@@ -19,7 +19,37 @@ print("The first fraction is", numerator1, "/",denominator1, ".\n")
 print("When multiplying fractions you multiply the two numerators together.\n")
 print("Then you multiply the denominators together.\n")
 
-new_numerator = numerator0 * numerator1
-new_denominator = denominator0 * denominator1
+# input the first fraction
+numerator0 = int(input("Type the first numerator and press enter.\n")) 
+denominator0 = int(input("Type the first denominator and press enter.\n"))
+
+# Input the second fraction. 
+numerator1 = int(input("Type the second numerator and press enter.\n"))
+denominator1 = int(input("Type the second denominator and press enter.\n"))
+
+print("The first fraction is", numerator0,"/",denominator0,".\n")
+print("The second fraction is", numerator1,"/",denominator1,".\n")
+
+new_numerator = numerator0 * numerator1 # Multiply the two numerators together. 
+new_denominator = denominator0 * denominator1 # Multiply the two denominators together.
+
+print("The new fraction is", new_numerator,"/",new_denominator,".\n")
+
+# This is where the division of the fractions will start. You will need to change this code!
+print("To divide a fraction, you will multiply using the reciprocal or inverse of the second fraction.\n")
+
+# input the first fraction
+numerator0 = int(input("Type the first numerator and press enter.\n")) 
+denominator0 = int(input("Type the first denominator and press enter.\n"))
+
+# Input the second fraction. 
+numerator1 = int(input("Type the second numerator and press enter.\n"))
+denominator1 = int(input("Type the second denominator and press enter.\n"))
+
+print("The first fraction is", numerator0,"/",denominator0,".\n")
+print("The second fraction is", numerator1,"/",denominator1,".\n")
+
+new_numerator = numerator0 * denominator1 
+new_denominator = denominator0 * numerator1 # Multip
 
 print("The new fraction is", new_numerator,"/",new_denominator,".\n")
