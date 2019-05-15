148e012ac2866ec391a040503be2ec3183be2afc
asg0 <s11355187@students.duvalschools.org>
2019-05-15 14:15:08 -0400

    Finished Version 1.0

--- multiplying_dividing_fractions/ansg_multiplying_dividing_fractions_1.0.py
+++ multiplying_dividing_fractions/ansg_multiplying_dividing_fractions_1.0.py
@@ -22,18 +22,18 @@ print("Then you will multiply the two denominators together.\n")
 numerator0 = int(input("Type the first numerator and press enter.\n"))
 numerator1 = int(input("Type the second numerator and press enter.\n"))
 
-denominator0 = int(input("Type the first denominator and press enter.\n")
-denominator1 = int(input("Type the second denominator and press enter.\n")
+denominator0 = int(input("Type the first denominator and press enter.\n"))
+denominator1 = int(input("Type the second denominator and press enter.\n"))
 
 print("The first fraction is",numerator0,"/",denominator0,".\n")
 print("The second fraction is", numerator1,"/",denominator1,".\n")
 
-new_numerator = numerator0 * numerator1
-new_denominator = denominator0 * denominator1
+#  This is where the division of the fraction will start. you will need to change this code!
+print("To divide a fraction, you will multiply using the reciprical or inverse of the second fraction.\n")
+
+new_numerator = numerator0 * denominator1
+new_denominator = denominator0 * numerator1
 
 print("The new fraction is", new_numerator,"/",new_denominator,".\n")
 
-#  This is where the division of the fraction will start. you will need to change this code!
-pie = 22 / 7
-print(pie)
-print("To divide a fraction, you will multiply using the reciprical or inverse of the second fraction.\n")
+print("In order to divide fractions you will need to, keep the first fraction the same and change the division sign to a multiplication sign and then flip the second fraction by putting the denominator where the numerator was and put the numerator where the denominater was.\n")
