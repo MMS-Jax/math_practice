d72016ef96e574ec50ae1ce191911e8b01e6c844
s11312444 <49413999+s11312444@users.noreply.github.com>
2019-05-16 11:49:49 -0400

    version 0.4
    added to print lines to print the two fractions.

--- multiplying_dividing_fractions/Gray_j_multiplying_dividing_fractions.py
+++ multiplying_dividing_fractions/Gray_j_multiplying_dividing_fractions.py
@@ -1,3 +1,5 @@
 # Jeremy G., Multiplying and Divide Fractions, 05/6/19 version 0.0
 
 print("Hello, welcome to Fraction Bot 9000.  I can multiply and divide fractions for you\n")
+user_name - input("whats your name? [Type your name and press Enter.\n")
+print("Hello, " , user_name, "How are you today?")
