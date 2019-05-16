26afff7042616b1dcbdd73222d0a65974ddcbfd2
imnotcool1 <49570771+imnotcool1@users.noreply.github.com>
2019-05-16 15:57:11 -0400

    finished what it does

--- multiplying_dividing_fractions/multiplying_dividing_fractions_0.9. kris.py
+++ multiplying_dividing_fractions/multiplying_dividing_fractions_0.9. kris.py
@@ -1,8 +1,10 @@
 # kris m, mulitiplying and dividing fractions 05/09/2019 virsion 0.9
 
 print("Hello,  I can multiply and divide fractions for you so you can get good grades in math class!\n")
+
 user_name = input("What is your name? [Type your name and press ENTER.]\n")
-print("Hello,", user_name,"how are you today?\n")
+
+print("Hello,", user_name,"how was your day?\n")
 
 print("For this program, I need to know the numerator and the denominator for both fractions.\n")
 
@@ -12,22 +14,28 @@ denominator0 = 0
 print("The first fraction is", numerator0,"/",denominator0,".\n")
 
 # Variables for fraction 1. 
+
 numerator1 = 0
 denominator1 = 0
+
 print("The second fraction is", numerator1,"/",denominator1,".\n")
 
 print("When multiplying fractions you multiply the two numerators together.\n")
+
 print("Then you will multiply the two denominators together.\n")
 
 # Input the first fraction. 
 numerator0 = int(input("Type the first numerator and press enter.\n")) 
+
 denominator0 = int(input("Type the first denominator and press enter.\n"))
 
 # Input the second fraction. 
 numerator1 = int(input("Type the second numerator and press enter.\n"))
+
 denominator1 = int(input("Type the second denominator and press enter.\n"))
 
 print("The first fraction is", numerator0,"/",denominator0,".\n")
+
 print("The second fraction is", numerator1,"/",denominator1,".\n")
 
 new_numerator = numerator0 * numerator1  
