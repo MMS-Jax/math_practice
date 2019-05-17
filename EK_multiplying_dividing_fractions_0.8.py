a10b1572e1bc63a166161cd53a69039b083b8475
Erdiisraw <49567991+Erdiisraw@users.noreply.github.com>
2019-05-17 14:07:09 -0400

    finished the code for multiplying and dividing fractions
    multiplied the numerators and denominators to multiply and divide fractions

--- EK_multiplying_dividing_fractions_0.8.py
+++ EK_multiplying_dividing_fractions_0.8.py
@@ -33,8 +33,9 @@ new_numerator = numerator0 * numerator1
 new_denominator = denominator0 * denominator1
 
 print("The new fraction is", new_numerator,"/",new_denominator,".\n")
-
-# This is where the division of the fractions will start. You will need to change this code!
-pie = 22 / 7
-print(pie)
 print("To divide a fraction, you will multiply using the reciprocal or inverse of the second fraction.\n")
+print("im gonna use the smae fractions as earlier")
+new_numerator=numerator0*denominator1
+new_denominator=numerator1*denominator0
+
+print("the new fraction is"new_numerator, "/"new_denominator, "\n")
