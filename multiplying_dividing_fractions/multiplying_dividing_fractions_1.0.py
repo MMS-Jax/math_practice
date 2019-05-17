50eab035d3794400e08cb7032372e3193dff750d
Derpy Puffer fish <49402886+Preston893@users.noreply.github.com>
2019-05-17 15:59:34 -0400

    finished code.
    finished multiplying and dividing fractions code.

---
+++ multiplying_dividing_fractions/multiplying_dividing_fractions_1.0.py
@@ -0,0 +1,30 @@
+# Kyle P. , Multiplying and Dividing Fractions, 05/09/2019, version 1.0
+
+print("Hello, welcome to Fraction-bot 9000,I can multiply and divide fractions for you!/n")
+user_name = input("What is your name? [Type your name and press ENTER.]/n")
+print("Hello", user_name,"How are you today?")
+
+print("For this program, I need to know the numerator and denominator for both fractions./n")
+
+#Variables for fraction 0.
+numerator0 = 0
+denominator0 = 0
+print("The first fraction is", numerator0,"/",denominator0,"./n")
+
+# Variables for fraction 1.
+numerator1 = 0
+denominator1 = 0
+print("The second fraction is", numerator1,"/",denominator1,"./n")
+
+print("When multiplying fractions you multiply the 2 numerators together./n")
+print("Then you will multiply the 2 denominators together./n")
+
+new_numerator = numerator0 * numerator1
+new_denominator = denominator0 * denominator1
+
+print("The new fraction is", new_numerator,"/",new_denominator,"./n")
+
+# This is where the division starts.
+pie = 22/7
+print(pie)
+print("To divide a fractions, you will multiply using the reciporocal or inverse of the 2nd fraction./n")
