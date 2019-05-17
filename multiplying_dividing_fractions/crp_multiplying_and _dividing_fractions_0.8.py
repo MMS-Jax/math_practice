ad50510392a125cb851fccd3b4001808cd4f8bd1
Elon Musk <49207612+youngdashboi@users.noreply.github.com>
2019-05-17 14:00:01 -0400

    Ver 0.8
    Changed the division processes

---
+++ multiplying_dividing_fractions/crp_multiplying_and _dividing_fractions_0.8.py
@@ -0,0 +1,41 @@
+# Powell C., Multiplying and Divide Fractions, 05/06/2019 Ver 0.8
+
+print("Hello, my name is Youngmathboi. I will multiply and divide fractions for you.\n")
+user_name = input("What is your name? [Type your name and press ENTER.\n")
+print("Hello,", user_name,"how are you today?\n")
+print("In order for me to do that i need to know the numerator and the denominator of the equation.\n")
+
+
+# Variables for Fraction 0.
+numerator0 = 0
+denominator0 = 0
+print("The first fraction is", numerator0,"/", denominator0,".\n")
+
+# Variables for Fraction 1
+numerator1 = 0
+denominator1 = 0
+print("The second fraction is", numerator1,"/", denominator1,".\n")
+
+print("When multpiplying fractions together you multiply the two numerators together.\n")
+print("Then you must multiply the two denominators together.\n")
+
+numerator0 = int(input("Type the first numerator and press enter.\n"))
+numerator1 = int(input("Type the second numerator and press enter.\n"))
+
+denominator0 = int(input("Type the first denominator and press enter.\n"))
+denominator1 = int(input("Type the second denominator and press enter.\n"))
+
+new_numerator = numerator0 * numerator1
+new_denominator = denominator0 * denominator1
+
+print("The new fraction is", new_numerator,"/", new_denominator,".\n")
+
+new_numerator = numerator0 * denominator1
+new_denominator = denominator0 * numerator1
+
+# This is where the division of the Fractions will start.
+
+new_numerator = numerator0 * denominator1
+new_denominator = denominator0 * numerator1
+
+print("To divide fraction you need to multiply the reciprocal or the opposites together.\n")
