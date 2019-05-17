8b38da4d1e6a176cd2c3669239d5d73a0d60132a
s11692498 <49410493+s11692498@users.noreply.github.com>
2019-05-17 14:09:25 -0400

    Update ARK_multiplying_dividing_fractions_0.1.py

--- multiplying_dividing_fractions/ARK_multiplying_dividing_fractions_0.1.py
+++ multiplying_dividing_fractions/ARK_multiplying_dividing_fractions_0.1.py
@@ -1,3 +1,5 @@
 # Anthony k., Multiplying and dividing fractions, 05/07/2019, version 0.1
 
 print("hello, Let me multiply & divide your fractions.\n")
+user_name = input("what is your username [type your Name and press Enter]\n")
+print("hello ,", user_name,"how are you today?\n                                                                ````````")
