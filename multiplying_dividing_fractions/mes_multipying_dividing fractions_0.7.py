8b011995970f047ef85dd927cf580dc6a0cb49a4
mattlyn2 <49207683+mattlyn2@users.noreply.github.com>
2019-05-17 13:54:05 -0400

    Update mes_multipying_dividing fractions_0.7.py

--- multiplying_dividing_fractions/mes_multipying_dividing fractions_0.7.py
+++ multiplying_dividing_fractions/mes_multipying_dividing fractions_0.7.py
@@ -43,6 +43,6 @@ print(pie)
 print("To divide a fraction, you will multiply using the reciprocal or inverse of the second fraction.\n")
 
 new_numerator = numerator0 * denominator1
-new_denominator = denominator1 * numerator0
+new_denominator = denominator0 * numerator1
 
 print("The new fraction is", new_numerator,"/",new_denominator,".\n")
