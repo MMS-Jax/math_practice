c2d7e214ee10db6f9a15415610c10842cc6efac6
avrielle <s11650678@students.duvalschools.org>
2019-05-17 15:53:03 -0400

    finished program
    added more things to it

--- multiplying_dividing_fractions/adding_and_subtracting_numbers 0.2.py
+++
@@ -1,5 +0,0 @@
-# Adding And Subtracting Whole Numbers 5/06/19 Avrielle G.
-
-print("welcome to Math-Ro-Bot I can Add and Subtract Whole Numbers for you!\n")
-user_name = input("What's your name? [Type your name and press ENTER.]\n")
-print("Hello", user_name, "How is your day going?\n")
---
+++ multiplying_dividing_fractions/multiply_divide_fractions0.7ag.py
@@ -0,0 +1,40 @@
+# Adding And Subtracting Fractions 5/06/19 Avrielle G. 
+
+print("welcome to Math-Ro-Bot I can multiply and divide fractions for you!\n")
+user_name = input("What's your name? [Type your name and press ENTER.]\n")
+print("Hello", user_name, "How is your day going?\n")
+
+print("For this area, I need to know the numerator and the denoinator so i can multiply or divide them.\n")
+
+# variables for multiplying and dividing 0
+numerator0 = 0
+denominator0 = 0
+
+print("The first fractions is", numerator0,"/",denominator0,".\n")
+
+# variables for multiplying and dividing 1
+numerator1 = 0
+denominator1 = 0
+print("The second fraction is", numerator1,"/",denominator1,".\n")
+
+print("When adding fractions you add the two numbers together.\n")                                                                                                                                 
+
+numerator0 = int(input("Type the first numerator and press enter .\n"))
+denominator0 = int(input("Type the first denominator and press enter .\n"))
+
+
+numerator0 = int(input("Type the second numerator and press enter .\n"))
+numerator1 = int(input("Type the second denominaor and press enter .\n"))
+
+
+#This is where the dividing of the fraction beggings
+new_numerator = numerator0 * denominator1
+new_denominator = numerator1 * denominator0
+
+print("The new fraction is", new_numerator,"/",new_denominator,".\n")
+
+print("We got this number by fliping the second set of numerator and denominator .\n")
+
+print("FOR EXAMPLE we have 5/6 divided by 7/8 we would flip the 7/8 to get 8/7 .\n")
+
+print("")
