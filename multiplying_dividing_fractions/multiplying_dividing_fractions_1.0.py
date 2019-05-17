3b02411577d8c46f9920a666ab1ac8c908f86436
Potato5789 <49240664+Potato5789@users.noreply.github.com>
2019-05-17 15:49:10 -0400

    Corrected file name
    corrected file name as the final version for code.

--- multiplying_dividing_fractions/multiplying_dividing_fractions_1.0.py
+++ multiplying_dividing_fractions/multiplying_dividing_fractions_1.0.py
@@ -1,4 +1,4 @@
-# Multiplying and Dividing Fractions by Skylar C. 5/06/19 version 0.7
+# Multiplying and Dividing Fractions by Skylar C. 5/06/19 version 1.0
 
 print("Welcome to the Fraction Calculator! I can multiply and divide fractions for you.\n" )
 user_name = input("What is your name? [Type your name and press ENTER.] \n")
