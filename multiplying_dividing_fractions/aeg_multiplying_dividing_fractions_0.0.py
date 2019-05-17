19eb1e6f8bf1b893b1a472341ed8c17960d706eb
Quadakid <49415099+Quadakid@users.noreply.github.com>
2019-05-17 11:35:46 -0400

    Added first print string
    Printed some basic instruction for the user

--- multiplying_dividing_fractions/aeg_multiplying_dividing_fractions_0.0.py
+++ multiplying_dividing_fractions/aeg_multiplying_dividing_fractions_0.0.py
@@ -1,1 +1,2 @@
-# Aquaryus G., Multipying and Dividing Fractions, 05/10/19, versions 0.0
+# Aquaryus G., Multipying and Dividing Fractions, 05/10/19,versions 0.0
+
---
+++ multiplying_dividing_fractions/aeg_multiplying_dividing_fractions_0.1.py
@@ -0,0 +1,3 @@
+# Aquaryus G., Multipying and Dividing Fractions, 05/10/19,versions 0.1
+
+print("Hello, welcome to fraction-Bot9000.   I can multiply and divide fractions for you!\n")
