0ca61c074c9df5f77fe92435e23006662d5a8e00
arianna015 <49210282+arianna015@users.noreply.github.com>
2019-05-17 11:32:12 -0400

    Writing Code to Divide Fractions
    Code to divide fractions was written.

--- multiplying_dividing_fractions/av_multiplying_dividing_fractions_1.0.py
+++ multiplying_dividing_fractions/av_multiplying_dividing_fractions_1.0.py
@@ -1,4 +1,4 @@
-# Arianna V., Multiplying and Dividing Fractions, 05/10/2019, version 0.8
+# Arianna V., Multiplying and Dividing Fractions, 05/10/2019, version 1.0
 
 print("Hello, welcome to Fraction-Bot 9000.   I can multiply and divide fractions for you!\n")
 user_name = input("What is your name? [Type your name and press ENTER.]\n")
