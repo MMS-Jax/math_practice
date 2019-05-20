3baf4fdb115d046bdb728bfe6608387381056f58
s11338480 <chelseavs1008@icloud.com>
2019-05-20 10:57:46 -0400

    Declared Variables for the fractions.
    Added the numberator and denomiator values for the two fractions.

---
+++ multiplying_dividing_fractions/cvs_multiplying_dividing_fractions_0.3.py
@@ -0,0 +1,14 @@
+# Chelsea. S. Multiplying and Dividing Fractions, 05/10/2019, version 0.3
+
+print("Hello, welcome human. This is Fraction-Bot 3000\n")
+user_name = input("What is your name? [Type your name and press ENTER.]\n")
+print("Hello, ", user_name,"how are you today?")
+
+print("For this program, I need to know the numerator and denominator for both fractions.\n")
+
+#Variables for fraction 0.
+numerator0 = 0
+denominator0 = 0
+#Variables for fraction 1.
+numerator1 = 0
+denominator10 = 0
