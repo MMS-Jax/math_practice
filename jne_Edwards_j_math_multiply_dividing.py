270555f90b6fb5535966c00232745e3d3078f7b9
joshua <s11688777@students.duvalschools.org>
2019-05-20 10:59:07 -0400

    added variable for fraction
    addd the numerator and denomanator variabel tab

--- jne_Edwards_j_math_multiply_dividing.py
+++ jne_Edwards_j_math_multiply_dividing.py
@@ -3,3 +3,4 @@
 print("hello,my name is fraction bot i can do any fraction math problem for you i am the smarted bot in the world!!!!!!!!!!!!\n")
 print("to multiply and divide frations i need to know the [blank] and [blank]" )
 
+numerator0 = 0
