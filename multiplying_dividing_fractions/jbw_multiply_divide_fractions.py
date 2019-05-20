9dd85fdee9c967c677dccaa06c7448c7bb3e78b0
Rainbow SteveGamingYT <49448622+RainbowSteveGamingYT@users.noreply.github.com>
2019-05-20 11:22:59 -0400

    Added Division Code to program.
    Added the division code the fractions program.

--- multiplying_dividing_fractions/jbw_multiply_divide_fractions.py
+++ multiplying_dividing_fractions/jbw_multiply_divide_fractions.py
@@ -33,7 +33,28 @@ new_denominator = denominator0 * denominator1
 
 print("The new fraction is", new_numerator,"/",new_denominator,".\n")
 
-# This is where the division of the fractions will start. You will need to change this code!
-pie = 22 / 7
-print(pie)
 print("To divide a fraction, you will multiply using the reciprocal or inverse of the second fraction.\n")
+
+# Variables for fraction 0.
+numerator0 = 0
+denominator0 = 0
+print("The first fraction is", numerator0,"/",denominator0,".\n")
+
+# Variables for fraction 1.
+numerator1 = 0
+denominator1 = 0
+print("The second fraction is", numerator1,"/",denominator1,".\n")
+
+numerator0 = int(input("Type the first numerator and press enter.\n")) 
+numerator1 = int(input("Type the second numerator and press enter.\n"))
+
+denominator0 = int(input("Type the first denominator and press enter.\n"))
+denominator1 = int(input("Type the second denominator and press enter.\n"))
+
+print("The first fraction is", numerator0,"/",denominator0,".\n")
+print("The second fraction is", numerator1,"/",denominator1,".\n")
+
+new_numerator = numerator0 * denominator1
+new_denominator = denominator0 * numerator1
+
+print("The new fraction is", new_numerator,"/",new_denominator,".\n")
