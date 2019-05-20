7cc88fcb49ef70222f2088b17a16933ce91c68ff
Gabbyyyy1 <s11612132@students.duvalschools.org>
2019-05-20 11:06:07 -0400

    Version 0.4
    Added to print0 lines to print the two fractions on the screen to verify they are correct.

--- GKO_Multiply_Divide_Fractionss.py
+++ GKO_Multiply_Divide_Fractionss.py
@@ -1,4 +1,4 @@
-# Gabriella O. , Multiply and  Divide Fractions, 05/06/19
+# Gabriella O. , Multiply and  Divide Fractions, 05/20/19
 
 print("Hello, My name is Mathbot 1000. I can multiply and divide fractions for you.\n")
 user_name = input("What is your name?\n")
@@ -7,8 +7,11 @@ print ("Hello,", user_name,"how are you today?\n")
 print("For this program, I need to know the numerator and the denominator for both fractions.\n")
 
 # Varibles for fraction 0.
-numerator = 0
-denominator = 0
+numerator0 = 0
+denominator0 = 0
+print("The first fraction is", numerator0,"/",denominator0,".\n")
+
 # Varibles for fraction 1.
-numerator = 1
-denominator = 1
+numerator1 = 0
+denominator1 = 0
+print("The second fraction is", numerator1,"/",denominator1,".\n")
