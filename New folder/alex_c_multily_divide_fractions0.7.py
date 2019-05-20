58d5ee853a5de6273656ace6ab0f9ccc224da779
alexalexrc <49314337+alexalexrc@users.noreply.github.com>
2019-05-20 10:54:27 -0400

    Starting to Divide Fractions
    Wrote the code on how to divide fractions

---
+++ New folder/alex_c_multily_divide_fractions0.7.py
@@ -0,0 +1,25 @@
+# Alex C., Multiply and Divide Fractions, 05/06/2019, version 0.7
+
+print("Hello, my name is fraction bot 9000. I can multiply and divide fractions for you.\n")
+print("To multiply and divide fractions I need to know the numerator and denominator for each fraction./n")
+
+numerator0=0
+numerator1=0
+print("The first fraction is", numerator0,"/", denominator0,".\n")
+
+denominator0=0
+denominator1=0
+print("The second fraction is", numerator1,"/",denominator1,".\n")
+
+print("When multiplying fractions you multiply the two numerators together.\n")
+print("Then you will multiply the two denominators together.\n")
+
+new_numerator = numerator0 * numerator1
+new_denominator = denominator0 * denominator1
+
+print("The new fraction is", new_numerator,"/",new_denominator,".\n")
+
+# This is where the division of the fractions will start
+pie = 22/7
+print(pie)
+print("To divide a fraction, you will multiply using the reciprocal or inverse of the second fraction.\n")
