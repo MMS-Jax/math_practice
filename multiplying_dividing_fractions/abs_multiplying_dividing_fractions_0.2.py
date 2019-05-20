9d70b452828767739fe829e1de0f75ae75ca790c
TherealBUZZ13 <49244460+TherealBUZZ13@users.noreply.github.com>
2019-05-20 15:18:42 -0400

    Declared Variables for the fractions.
    Added the numerator and denominator values for the two fractions

--- multiplying_dividing_fractions/abs_multiplying_dividing_fractions_0.2.py
+++ multiplying_dividing_fractions/abs_multiplying_dividing_fractions_0.2.py
@@ -2,4 +2,4 @@
 
 print("Hello, welcome to Fraction Bot 9000.  I can multiply and divide fractions for you!\n")
 user_name = input("What is your name? [Type your name and press ENTER.]\n")
-print("Hello, ", user_name,"how are you today?")
+print("Hello, ", user_name,"how are you today?\n")
