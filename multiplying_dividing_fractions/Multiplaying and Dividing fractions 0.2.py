1eb8e431d441d412eef82fc82a66f408dbf2b21e
OhOnJah <Jacob.osterland123@gmail.com>
2019-05-20 15:09:46 -0400

    Added username variable and input line
    Added 2 lines of code to ask for the username and then they type it them get it back

--- multiplying_dividing_fractions/Multiplaying and Dividing fractions 0.2.py
+++ multiplying_dividing_fractions/Multiplaying and Dividing fractions 0.2.py
@@ -1,4 +1,4 @@
-# Jacob O., Multiplying and Dividing Fractions, 05/14/2019, version 0.1
+# Jacob O., Multiplying and Dividing Fractions, 05/14/2019, version 0.2
 
 print("What's up G, I can multiplay and divide fractions for you instead of you getting frustrated in math class.\n")
 user_name = input("What is your name? [Type your name and press ENTER.]\n")
