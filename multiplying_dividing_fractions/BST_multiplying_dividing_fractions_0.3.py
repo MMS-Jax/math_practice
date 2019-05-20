2ef14033b82acf04adefedc5a51e90d4352aeeb2
bradythebest <49413981+bradythebest@users.noreply.github.com>
2019-05-20 15:11:42 -0400

    Declared Variables for the fractions.
    Added the numerator an denominator values for the two fractions.

---
+++ multiplying_dividing_fractions/BST_multiplying_dividing_fractions_0.3.py
@@ -0,0 +1,14 @@
+# Brady T., multiplying and dividing fractions version 0.3
+
+print("Hello, welcome to fration-bot 9000.   I can multiplyand divide fractions for you!\n")
+user_name = input("what is your name? [Type your name an press ENTER.] \n")
+print("Hello, ", user_name," how are you today? \n " )
+
+print("For this program, I need to know the numerator and denominator for both fractions.\n ")
+
+# Variables for fraction 0.
+numerator0 = 0
+denominator0 = 0
+# Variables for fraction 1.
+numerator1 = 0
+denominator1 = 0
