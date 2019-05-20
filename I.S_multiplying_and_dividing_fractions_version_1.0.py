4bfd02cd610f712d806314e146ea8dc313290a28
isabella1231 <49206750+isabella1231@users.noreply.github.com>
2019-05-20 11:51:18 -0400

    finished version

--- I.S_multiplying_and_dividing_fractions_version_1.0.py
+++ I.S_multiplying_and_dividing_fractions_version_1.0.py
@@ -35,4 +35,29 @@ print("the new fraction is", new_numerator,"/",new_denominator,".\n")
 
 #this is where the division of the fractions will start.
 
-print("to divide a fraction, you will multiply using the reciprocal or inverse of the secooned fraction.\n")
+print("to divide a fraction, you will multiply using the reciprocal or inverse of the seconed fraction.\n")
+
+#variables for fraction 0.
+numerator0 = 0
+denominator0 = 0
+print("the first fraction is", numerator0,"/",denominator0,".\n")
+
+print("now you will multipltiply the fraction by it's reciprical\n")
+
+#variables for fraction 0.
+numerator1 = 0
+denominator1 = 0
+print("the seconed fraction is", numerator0,"/",denominator0,".\n")
+
+numerator0 =  int(input("Type the first numerator and press enter.\n")) 
+numerator1 =  int(input("Type the second numerator and press enter.\n"))
+
+denominator0 = int(input("Type the first denominator and press enter.\n"))
+denominator1 = int(input("Type the second denominator and press enter.\n"))
+
+new_numerator = numerator0 * denominator1
+new_denominator = denominator0 * numerator1
+
+print("the new fraction is", new_numerator,"/",new_denominator,".\n")
+
+
