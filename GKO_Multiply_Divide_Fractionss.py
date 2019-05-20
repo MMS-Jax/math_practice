1809836d5d701e0490dda540c4978c7467892a18
Gabbyyyy1 <s11612132@students.duvalschools.org>
2019-05-20 10:52:05 -0400

    Declared varibles for the fractions.
    Added numerator and demoninator values for the two fractions.

---
+++ GKO_Multiply_Divide_Fractionss.py
@@ -0,0 +1,14 @@
+# Gabriella O. , Multiply and  Divide Fractions, 05/06/19
+
+print("Hello, My name is Mathbot 1000. I can multiply and divide fractions for you.\n")
+user_name = input("What is your name?\n")
+print ("Hello,", user_name,"how are you today?\n")
+
+print("For this program, I need to know the numerator and the denominator for both fractions.\n")
+
+# Varibles for fraction 0.
+numerator = 0
+denominator = 0
+# Varibles for fraction 1.
+numerator = 1
+denominator = 1
