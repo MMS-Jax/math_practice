97e3fcc4528581e2e6c42f298d3b3d8ecff2379c
s11338480 <chelseavs1008@icloud.com>
2019-05-20 10:51:21 -0400

    Added user_name variable andinput[) line
    Added two lines of code to ask for the user's name save it to a variable using input[) and then printing the name back on the screen

--- multiplying_dividing_fractions/cvs_multiplying_dividing_fractions_0.2.py
+++ multiplying_dividing_fractions/cvs_multiplying_dividing_fractions_0.2.py
@@ -1,4 +1,4 @@
-# Chelsea. S. Multiplying and Dividing Fractions, 05/10/2019, version 0.0
+# Chelsea. S. Multiplying and Dividing Fractions, 05/10/2019, version 0.2
 
 print("Hello, welcome human. This is Fraction-Bot 3000\n")
 user_name = input("What is your name? [Type your name and press ENTER.]\n")
