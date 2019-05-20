9cc48dcdfb0bd5ceaf14dd3346789aeb4374ebe9
Quadakid <49415099+Quadakid@users.noreply.github.com>
2019-05-20 15:57:52 -0400

    Declaring Varibles for the Fractions
    Added the numorator and denominator values for the two fractions

---
+++ multiplying_dividing_fractions/aeg_multiplying_dividing_fractions_0.3.py
@@ -0,0 +1,14 @@
+# Aquaryus G., Multipying and Dividing Fractions, 05/10/19,versions 0.1
+
+print("Hello, welcome to fraction-Bot9000.   I can multiply and divide fractions for you!\n")
+user_name = input("What is your name?[Type your name and press ENTER.]\n")
+print("Hello,", user_name,"how are you today?\n")
+
+print("For this program, I need to know the numorator and denominator for both fractions.\n")
+
+# Varibles for fractions
+numorator0 = 0
+deniminator0 = 0
+
+numorator1 = 0
+denominator1 = 0
