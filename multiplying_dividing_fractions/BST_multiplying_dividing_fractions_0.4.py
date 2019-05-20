40f3db07c2c551dc230b0bf1153131a49cb0766c
bradythebest <49413981+bradythebest@users.noreply.github.com>
2019-05-20 15:19:34 -0400

    Version 0.4
    added to print() lines to print the two fractions on the screen to verify they are correct

---
+++ multiplying_dividing_fractions/BST_multiplying_dividing_fractions_0.4.py
@@ -0,0 +1,17 @@
+# Brady T., multiplying and dividing fractions version 0.4
+
+print("Hello, welcome to fration-bot 9000.   I can multiplyand divide fractions for you!\n")
+user_name = input("what is your name? [Type your name an press ENTER.] \n")
+print("Hello, ", user_name," how are you today? \n " )
+
+print("For this program, I need to know the numerator and denominator for both fractions.\n ")
+
+# Variables for fraction 0.
+numerator0 = 0
+denominator0 = 0
+print("The first fraction is", numerator0,"/", denominator0,".\n")
+
+# Variables for fraction 1.
+numerator1 = 0
+denominator1 = 0
+print("The second fraction is", numerator1,"/", denominator1,".\n")
