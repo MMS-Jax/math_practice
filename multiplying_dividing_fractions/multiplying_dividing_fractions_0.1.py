3b985fc6076205b2914ec5951de703f3647758d1
Emma Miller <s20084609@students.duvalschools.org>
2019-05-20 15:51:46 -0400

    Added user_name variable and input] line
    Added two lines of code to ask for the user's name, save it to a variable using input0 and then printing the name back on the screen.

--- multiplying_dividing_fractions/multiplying_dividing_fractions_0.1.py
+++ multiplying_dividing_fractions/multiplying_dividing_fractions_0.1.py
@@ -1,3 +1,5 @@
-# Emma M., Multiplying and Dividing Fractions, 05/16/2019, version 0.1
+# Emma M., Multiplying and Dividing Fractions, 05/16/2019, version 0.2
 
 print("Hello, Welcome to Fraction-Bot 9000. I can multiply and divided fractions for you!/n")
+user_name = input("What is your name? [Type your name and press ENTER.]/n")
+print("Hello,", user_name,"how are you today?/n")
