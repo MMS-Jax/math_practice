9866ea473569d91a1e6093f138edef9311655928
Parker <s11562949@students.duvalschools.org>
2019-05-20 10:58:36 -0400

    Added Numerator and denominator codes
    Put in the codes for the first fractions

--- pck_multiplying_dividing_fractions_0.3.py
+++ pck_multiplying_dividing_fractions_0.3.py
@@ -8,9 +8,10 @@ print("For this program, I need to know the numerator and denominator for both t
 
 # Variables for fraction 0.
 numerator0 = 0
-denominator1 = 0
-print("The first fraction is", numerator0,"/", denominator1,".\n")
+denominator0 = 0
+print("The first fraction is", numerator0,"/",denominator0,".\n")
 
 # Variables for fraction 1.
-numerator0 = 0
+numerator1 = 0
 denominator1 = 0
+print("The first fraction is", numerator1,"/",denominator1,".\n")
