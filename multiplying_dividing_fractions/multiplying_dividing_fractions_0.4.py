f1eda1f26a6e767a5dd25dfa1f83184bf3c43d08
AdventurousFox <49415322+AdventurousFox@users.noreply.github.com>
2019-05-21 10:35:11 -0400

    Added Variables for new fraction
    Declared new numerator and new denominator. Also wrote the code to multiply the two fractions together.

--- multiplying_dividing_fractions/multiplying_dividing_fractions_0.4.py
+++ multiplying_dividing_fractions/multiplying_dividing_fractions_0.4.py
@@ -15,3 +15,11 @@ print("The second fraction is", numerator0, "/", denominator0,".\n")
 numerator1 = 0
 denominator1 = 0
 print("The first fraction is", numerator1, "/", denominator1,".\n")
+
+print("When multiplying fractions you multiply the two numerators together. \n")
+print("Then you will multiply the two denominators together\n")
+
+new_numerator = numerator0 * numerator1
+new_denominator = denominator0 * denominator1       
+
+print("The new fraction is", new_numerator, "/", new_denominator,".\n")
