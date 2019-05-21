d20ce0630bbf866d98eec86ef0e046a6f4776f27
Parker <s11562949@students.duvalschools.org>
2019-05-21 16:10:20 -0400

    added code to skip,flip,multiply
    added the code for the skip, flip, multiply

--- pck_multiplying_dividing_fractions_0.3.py
+++ pck_multiplying_dividing_fractions_0.3.py
@@ -15,3 +15,25 @@ print("The first fraction is", numerator0,"/",denominator0,".\n")
 numerator1 = 0
 denominator1 = 0
 print("The first fraction is", numerator1,"/",denominator1,".\n")
+
+print("When multiplying fractions you multiply the two numerators together.\n")
+print("Then you will multiply the two denominators together.\n")
+
+# Input the first fraction. 
+numerator0 = int(input("Type the first numerator and press enter.\n")) 
+denominator0 = int(input("Type the first denominator and press enter.\n"))
+
+# Input the second fraction. 
+numerator1 = int(input("Type the second numerator and press enter.\n"))
+denominator1 = int(input("Type the second denominator and press enter.\n"))
+
+print("The first fraction is", numerator0,"/",denominator0,".\n")
+print("The second fraction is", numerator1,"/",denominator1,".\n")
+
+new_numerator = numerator0 * numerator1 # Multiply the two numerators together. 
+new_denominator = denominator0 * denominator1 # Multiply the two denominators together.
+
+print("The new fraction is", new_numerator,"/",new_denominator,".\n") # Print the new fraction on the screen.
+
+new_numerator = numerator0 * denominator1
+new_denominator = denominator0 * numerator1
