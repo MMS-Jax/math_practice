380a73e1ea8bcb0ff76da75ca21d0fee8a0cb13f
OhOnJah <Jacob.osterland123@gmail.com>
2019-05-21 09:51:43 -0400

    version 0.4
    was able to prin the 2 fractions correctly using the print action

--- multiplying_dividing_fractions/Multiplaying and Dividing fractions 0.4.py
+++ multiplying_dividing_fractions/Multiplaying and Dividing fractions 0.4.py
@@ -14,3 +14,4 @@ print("The first fraction is",numerator0,"/",denominator0,".\n")
 #variables for fraction 1.
 numerator1 = 0
 denominator1 = 0
+print("The second fraction is",numerator1,"/",denominator1,".\n")
