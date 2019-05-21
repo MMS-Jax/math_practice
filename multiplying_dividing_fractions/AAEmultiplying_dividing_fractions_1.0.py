8edd2646228d7226d38aa59c267016257bb8f10c
Aiden <s11573029@duvalschools.org>
2019-05-21 10:43:12 -0400

    Created a Working Fraction Calculator
    Finished project and the code now multiplies and divides fractions.

--- multiplying_dividing_fractions/AAEmultiplying_dividing_fractions_1.0.py
+++ multiplying_dividing_fractions/AAEmultiplying_dividing_fractions_1.0.py
@@ -1,4 +1,4 @@
-# Aiden E., Multiplying and Dividing Fractions, 05/10/2019, version 0.7
+# Aiden E., Multiplying and Dividing Fractions, 05/10/2019, version 1.0
 
 print("Hello, this is the most inconvenient calculator! I can only multiply and divide fractions which will not help you with your homework.\n")
 user_name = input("What is your name? [Type your name and press ENTER].\n")
