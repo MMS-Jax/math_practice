246e3b0df6e6747b56c823040be8d5e2f7f5ca0c
Hobensack <s10676963@students.duvalschools.org>
2019-05-21 10:10:32 -0400

    Dividing Fractions
    I finished my code and finished dividing fractions.

--- multiplying_dividing_fractions/LWH_Multiply_Dividing_Fractions_0.7.py
+++ multiplying_dividing_fractions/LWH_Multiply_Dividing_Fractions_0.7.py
@@ -25,13 +25,11 @@ new_denominator= denominator0 * denominator1
 
 print("The new fractions is",new_numerator,"/",new_denominator,".\n")
 
+
 #This is where the division of the fractions will start. 
 
 
-print("To divide a fraction, you will multiply using the reciprocal or inverse of the second fraction.\n")
+print("To divide a fraction, you will multiply using the reciprocal or inverse of the second fraction fraction.\n")
 
-new_numerator = numerator0 * denominator1
-new_denominator= denominator0 * numerator1
 
-print("The new fraction is", new_numerator,"/",new_denominator,".\n")
 
