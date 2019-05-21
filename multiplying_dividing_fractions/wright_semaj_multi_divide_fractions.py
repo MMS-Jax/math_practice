6efd9bccdaac2e346dfc7b8e699f8bb735198edb
AquaWolf16 <49403222+AquaWolf16@users.noreply.github.com>
2019-05-21 11:44:09 -0400

    Create wright_semaj_multi_divide_fractions.py

---
+++ multiplying_dividing_fractions/wright_semaj_multi_divide_fractions.py
@@ -0,0 +1,9 @@
+#Multiplying and Dividing Fractions by Semaj W. 5/6/2019 version 1.0
+
+print("Welcome to Fraction-Bot 16! Would you like me to multiply and divide fractions for you? \n")
+print("In order to multiply and divide fractions we need to know the [....] and [....]. \n")
+
+numerator0= 0
+numerator1= 0
+
+print("The first numerator is:", numerator0, ".")
