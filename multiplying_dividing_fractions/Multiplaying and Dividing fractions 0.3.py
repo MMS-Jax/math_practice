3fdc8e4739239d946b0556a311fcab7b8d63e7ca
OhOnJah <Jacob.osterland123@gmail.com>
2019-05-21 09:47:04 -0400

    declared variables for fractions
    added the numerator and denominator values

---
+++ multiplying_dividing_fractions/Multiplaying and Dividing fractions 0.3.py
@@ -0,0 +1,14 @@
+# Jacob O., Multiplying and Dividing Fractions, 05/14/2019, version 0.2
+
+print("What's up G, I can multiplay and divide fractions for you instead of you getting frustrated in math class.\n")
+user_name = input("What is your name? [Type your name and press ENTER.]\n")
+print("What's up", user_name, "How are you today?\n" )
+
+print("for this program, I need to know the numerator and the denominator for both fractions.\n")
+
+#variables for fraction 0.
+numerator0 = 0
+denominator0 = 0
+#variables for fraction 1.
+numerator1 = 0
+denominator1 = 0
