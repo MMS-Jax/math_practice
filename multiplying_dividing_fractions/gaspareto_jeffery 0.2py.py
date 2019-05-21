d42d2dbb25d4511c0a077cd917a75bb3da898e50
kingjeffrobro <49403236+kingjeffrobro@users.noreply.github.com>
2019-05-21 11:48:20 -0400

    Update gaspareto_jeffery 0.2py.py

--- multiplying_dividing_fractions/gaspareto_jeffery 0.2py.py
+++ multiplying_dividing_fractions/gaspareto_jeffery 0.2py.py
@@ -2,4 +2,6 @@
 
 print('welcome to fraction-bot 15! would you like me to multiply and divide fractions for you? \n')
 user_name = input("what is your name? [type your name and press ENTER.]\n")
-print("hello,", user_name,"how are you today?")jefery
+print("hello,", user_name,"how are you today?")
+
+print("for this ")
