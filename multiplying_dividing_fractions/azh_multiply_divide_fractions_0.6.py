a18c3676506ef0e2bd5703695f4d62d2c2214c43
Armani Hayes <s20004304@students.duvalschools.org>
2019-05-21 12:17:15 -0400

    Added code for Dividing Fractions.

--- multiplying_dividing_fractions/azh_multiply_divide_fractions_0.6.py
+++ multiplying_dividing_fractions/azh_multiply_divide_fractions_0.6.py
@@ -11,7 +11,7 @@ numerator0 = 0
 denominator0 = 0
 print("The first fraction is", numerator0, "/", denominator0,". \n")
 
-#Variables for fraction 1.
+# Variables for fraction 1.
 numerator1 = 0
 denominator1 = 0
 print("The second fraction is", numerator1, "/", denominator1,". \n")
@@ -19,12 +19,29 @@ print("The second fraction is", numerator1, "/", denominator1,". \n")
 print("When multiplying fractions, you multiply the numerators together. \n")
 print("When multiplying fractions, you multiply the denominators together. \n")
 
-new_numerator = numerator1 * numerator1
+new_numerator = numerator0 * numerator1
 new_denominator = denominator0 * denominator1
 
 print("The new fraction is", new_numerator, "/", new_denominator,". \n")
 
-# This is where the divisions of the fractions starts
+# Dividing Fractions
 pie = 22/7
 print(pie)
 print("To divide a fracton, you will multiply using the reciprocal or the inverse of the second fraction. \n")
+
+# Variables for fraction 0
+numerator0 = 0
+denominator0 = 0
+print("The first fraction is", numerator0, "/", denominator0,". \n")
+
+# Variables for fraction 1
+numerator1 = 0
+denominator1 = 0
+print("The second fraction is", numerator1,"/", denominator1, ". \n")
+
+print("When dividing fractions you Keep, Change, and Flip.\n")
+
+new_numerator = numerator0 * denominator1
+new_denominator = denominator0 * numerator1
+
+print("The new fraction is", new_numerator, "/", new_denominator, ". \n")
