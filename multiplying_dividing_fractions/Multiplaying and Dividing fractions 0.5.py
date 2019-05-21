cf7a732f4431f1e21fe9cf94b931fbbc19feaa35
OhOnJah <Jacob.osterland123@gmail.com>
2019-05-21 09:57:50 -0400

    added variables for the new fraction
    declared new_numerator and new_denominator

---
+++ multiplying_dividing_fractions/Multiplaying and Dividing fractions 0.5.py
@@ -0,0 +1,25 @@
+# Jacob O., Multiplying and Dividing Fractions, 05/14/2019, version 0.2
+
+print("What's up G, I can multiplay and divide fractions for you instead of you getting frustrated in math class.\n")
+user_name = input("What is your name? [Type your name and press ENTER.]\n")
+print("What's up", user_name, "How are you today?\n" )
+
+print("for this program, I need to know the numerator and the denominator for both fractions.\n")
+
+#variables for fraction 0.
+numerator0 = 0
+denominator0 = 0
+print("The first fraction is",numerator0,"/",denominator0,".\n")
+
+#variables for fraction 1.
+numerator1 = 0
+denominator1 = 0
+print("The second fraction is",numerator1,"/",denominator1,".\n")
+
+print("When multiplying fractions you multiply the two numertors together.\n")
+print("Then you will multiply the two denominators together.\n")
+
+new_numerator = numerator0 * numerator1
+new_denominator = denominator0 * denominator1
+
+print("The new fraction is",new_numerator,"/",new_denominator,".\n")
