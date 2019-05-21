a5eb77bbe49ea7bc45d7d625eff2243af434f47b
bradythebest <49413981+bradythebest@users.noreply.github.com>
2019-05-21 09:59:16 -0400

    Added variable for the new fraction
    declared new_numerator and new_denominator also wrote code to multiply the two fractions together.

--- multiplying_dividing_fractions/BST_multiplying_dividing_fractions_0.5.py
+++ multiplying_dividing_fractions/BST_multiplying_dividing_fractions_0.5.py
@@ -1,4 +1,4 @@
-# Brady T., multiplying and dividing fractions version 0.4
+# Brady T., multiplying and dividing fractions version 0.5
 
 print("Hello, welcome to fration-bot 9000.   I can multiplyand divide fractions for you!\n")
 user_name = input("what is your name? [Type your name an press ENTER.] \n")
