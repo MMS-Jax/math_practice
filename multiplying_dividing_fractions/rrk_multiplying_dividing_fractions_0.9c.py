df468edd6f3531f0890c50b530a951336ee28cb1
HeWasNumber1 <49246121+HeWasNumber1@users.noreply.github.com>
2019-05-21 12:41:54 -0400

    version 1.0
    Finished code

--- multiplying_dividing_fractions/rrk_multiplying_dividing_fractions_0.9c.py
+++ multiplying_dividing_fractions/rrk_multiplying_dividing_fractions_0.9c.py
@@ -1,4 +1,4 @@
-# Ryan K., Multiplying and Dividing Fractions, 05/10/2019, version 0.9c
+         # Ryan K., Multiplying and Dividing Fractions, 05/10/2019, version 0.9c
 
 print("Hello, welcome to Fraction-Bot 9000.   I can multiply and divide fractions for you!\n")
 user_name = input("What is your name? [Type your name and press ENTER.]\n")
