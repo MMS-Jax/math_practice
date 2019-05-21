31a5c07ce5a11b6ae4bff9430ea2b964bf26d593
TherealBUZZ13 <49244460+TherealBUZZ13@users.noreply.github.com>
2019-05-21 10:14:15 -0400

    Added variables for the new fraction.
    Declared ne_numerator and new_denominator, also wrote the code to multiply the two fractions together.

--- multiplying_dividing_fractions/abs_multiplying_dividing_fractions_0.5.py
+++ multiplying_dividing_fractions/abs_multiplying_dividing_fractions_0.5.py
@@ -1,4 +1,4 @@
-# Antoine B., Multiplying and Dividing Fractions, 05/06/2019, version 0.4
+# Antoine B., Multiplying and Dividing Fractions, 05/06/2019, version 0.5
 
 print("Hello, welcome to Fraction Bot 9000.  I can multiply and divide fractions for you!\n")
 user_name = input("What is your name? [Type your name and press ENTER.]\n")
