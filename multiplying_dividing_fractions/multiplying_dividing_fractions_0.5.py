f274ecc10ce26bad5e20a08c7ff252cdb09dca63
AdventurousFox <49415322+AdventurousFox@users.noreply.github.com>
2019-05-21 11:05:17 -0400

    Starting to divide fractons
    Starting the code to divide the fractions enrered, explained how to divide using the reciprocal.

--- multiplying_dividing_fractions/multiplying_dividing_fractions_0.5.py
+++ multiplying_dividing_fractions/multiplying_dividing_fractions_0.5.py
@@ -1,4 +1,4 @@
-# Richardson A., Multiplying Dividing Fractions 05/15/19 version 0.5
+# Richardson A., Multiplying Dividing Fractions 05/15/19 version 0.6
 
 print("Hello. I am Fraction Bot 9000.   I can multiply and divide fractions for you!\n")
 user_name = input("Please state your name [Type Your Name And Press Enter.]")
@@ -17,11 +17,23 @@ denominator1 = 0
 print("The second fraction is", numerator1, "/", denominator1,".\n")
 
 print("When multiplying fractions you multiply the two numerators together. \n")
+# Input the first fraction. 
+numerator0 = int(input("Type the first numerator and press enter.\n")) 
+denominator0 = int(input("Type the first denominator and press enter.\n"))
 
-print("Then you will multiply the two denominators together\n")
+# Input the second fraction. 
+numerator1 = int(input("Type the second numerator and press enter.\n"))
+denominator1 = int(input("Type the second denominator and press enter.\n"))
 
-new_numerator = numerator0 * numerator1
-new_denominator = denominator0 * denominator1       
-print("The new fraction is", new_numerator, "/", new_denominator,".\n")
+print("The first fraction is", numerator0,"/",denominator0,".\n")
+print("The second fraction is", numerator1,"/",denominator1,".\n")
 
+new_numerator = numerator0 * numerator1 # Multiply the two numerators together. 
+new_denominator = denominator0 * denominator1 # Multiply the two denominators together.
 
+print("The new fraction is", new_numerator,"/",new_denominator,".\n") # Print the new fraction on the screen. 
+
+# This is where the division of the fractions will start. 
+pie = 22/7
+print(pie)
+print("To divide a fraction,you will multiply the reciprocal or inverse of the second fraction.\n")
