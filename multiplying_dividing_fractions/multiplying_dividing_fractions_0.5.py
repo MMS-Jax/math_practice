38c24da7b9ea1a868e6c7fadde670e850ed44a7b
Isignedupongithub <49456804+Isignedupongithub@users.noreply.github.com>
2019-05-22 14:48:55 -0400

    added variables for the new fractions.
    Declared new_numerator and new_denominator, also write the code to multiply the two new fractions together.

--- multiplying_dividing_fractions/multiplying_dividing_fractions_0.5.py
+++ multiplying_dividing_fractions/multiplying_dividing_fractions_0.5.py
@@ -1,32 +1,23 @@
-# Ryan K., Multiplying and Dividing Fractions, 05/06/2019, version 0.5
+# Tyler A., Multiplying and Dividing Fractions, 05/10/19, version 0.5
 
-print("Hello, welcome to Fraction-Bot 9000.   I can multiply and divide fractions for you!\n")
-user_name = input("What is your name? [Type your name and press ENTER.]\n")
+print("Hello, welcome to Fraction-Bot 9000.     I can multiply and divide fractions for you!\n")
+user_name = input ("What is your name? [Type your name and press ENTER]\n")
 print("Hello,", user_name,"how are you today?\n")
 
 print("For this program, I need to know the numerator and the denominator for both fractions.\n")
 
-# Variables for fraction 0. 
+# variables for fraction 0.
 numerator0 = 0
 denominator0 = 0
 print("The first fraction is", numerator0,"/",denominator0,".\n")
 
-# Variables for fraction 1. 
+# Variables for fraction 1.
 numerator1 = 0
 denominator1 = 0
 print("The second fraction is", numerator1,"/",denominator1,".\n")
 
-print("When multiplying fractions you multiply the two numerators together.\n")
+print("When multiplying fractions you multiply the two numerators togethers.\n")
 print("Then you will multiply the two denominators together.\n")
-
-numerator0 = int(input("Type the first numerator and press enter.\n")) 
-numerator1 = int(input("Type the second numerator and press enter.\n"))
-
-denominator0 = int(input("Type the first denominator and press enter.\n"))
-denominator1 = int(input("Type the second denominator and press enter.\n"))
-
-print("The first fraction is", numerator0,"/",denominator0,".\n")
-print("The second fraction is", numerator1,"/",denominator1,".\n")
 
 new_numerator = numerator0 * numerator1
 new_denominator = denominator0 * denominator1
