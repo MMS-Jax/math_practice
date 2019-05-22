2e2174795e9f23618cb884d620923def1fd51448
s11315744 <49210019+s11315744@users.noreply.github.com>
2019-05-22 14:36:23 -0400

    Added user_name and input() line.
    Added two lines of code to ask fo rthe user's name and saved it to a variable with input() and then printed the name bck on the screen.

---
+++ multiplying_dividing_fractions/cch_multiplying_dividing_fractions_0.2.py
@@ -0,0 +1,5 @@
+# Charlene H., Multiplying and Dividing Fractions, 05/10/2019, version 0.2
+
+print("Hello, this program can multiply and divide fractions.\n")
+user_name = input("What is your name? [Type your name and press ENTER.]\n")
+print("Hello,", user_name,"how are you today?\n")
