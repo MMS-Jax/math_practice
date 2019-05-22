76213a4449b403715ec80c58966cdb1d2da0af66
Mars_Is_Aids <49207579+Benjamin-Hadziabdic@users.noreply.github.com>
2019-05-22 12:28:46 -0400

    Added the first print string

---
+++ multiplying_dividing_fractions/bh_Multiplying_and_dividing_fractions_version_0.1.py
@@ -0,0 +1,3 @@
+# Benjamin Hadziabdic, Multiplying and Dividing Fractions, 05/20/2019, Version 0.1
+
+print('This is a program about multiplying and dividing fractions for you because you are too lazy to.')
