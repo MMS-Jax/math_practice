09ef8bf785a98096f75b3ec7ec51fc713b6b19f8
kingjeffrobro <49403236+kingjeffrobro@users.noreply.github.com>
2019-05-22 09:40:12 -0400

    Create gaspareto_jeffery 0.333py.py

---
+++ multiplying_dividing_fractions/gaspareto_jeffery 0.333py.py
@@ -0,0 +1,7 @@
+# jeffery G multiplying and dividing fractions, 05/06/2019, version 0.2
+
+print('welcome to fraction-bot 15! would you like me to multiply and divide fractions for you? \n')
+user_name = input("what is your name? [type your name and press ENTER.]\n")
+print("hello,", user_name,"how are you today?")
+
+print("for this program, i need to know the numerator and the denominator for both fractions. \n")jeffery
