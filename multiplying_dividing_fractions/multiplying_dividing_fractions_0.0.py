ffc928b91c7af8d84c419650547391cd57917523
Laurore <s11686755@students.duvalschools.org>
2019-05-22 11:25:40 -0400

    added first print string
    printed some basic info for user

--- multiplying_dividing_fractions/multiplying_dividing_fractions_0.0.py
+++ multiplying_dividing_fractions/multiplying_dividing_fractions_0.0.py
@@ -1,1 +1,3 @@
 # Abby L., multiplying and subtracting fractions, 05/13/19, version 0.0
+
+print('hello, welcome to mutiplying and dividing fractions!!')/n
--- multiplying_dividing_fractions/multiplying_dividing_fractions_0.1.py
+++ multiplying_dividing_fractions/multiplying_dividing_fractions_0.1.py
@@ -1,5 +1,3 @@
-# Ryan K., Multiplying and Dividing Fractions, 05/06/2019, version 0.1
+# Abby L., multiplying and subtracting fractions, 05/13/19, version 0.1
 
-print("Hello, welcome to Fraction-Bot 9000.   I can multiply and divide fractions for you!\n")
-user_name = input("What is your name? [Type your name and press ENTER.]\n")
-print("Hello,", user_name,"how are you today?")
+print('hello, welcome to mutiplying and dividing fractions!!')/n
