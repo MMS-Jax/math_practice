b3634cf2f2e5a718dab4d589a953b28ba08e3ad5
Mars_Is_Aids <49207579+Benjamin-Hadziabdic@users.noreply.github.com>
2019-05-22 12:45:07 -0400

    Made the variables for numerator and denominator 0 and numerator and denominator 1

--- multiplying_dividing_fractions/bh_Multiplying_and_dividing_fractions_version_0.3.py
+++ multiplying_dividing_fractions/bh_Multiplying_and_dividing_fractions_version_0.3.py
@@ -4,4 +4,10 @@ print('This is a program about multiplying and dividing fractions for you becaus
 user_name = input('What do you want me to call you? [Type your potentially fake name and press ENTER].\n')
 print('Wassup broski.',user_name,'...kinda generic but whatever')
 
-print('I assume you know what a fraction is. Tell me what the two numerators and denominators. If you dont know what a fraction is, Hit')
+print('I assume you know what a fraction is. Tell me what the two numerators and denominators. If you dont know what a fraction is, hit ALT+F4.\n')
+# Variables for fraction 0
+numerator0 = 0
+denominator0 = 0
+# Variables for fraction 0
+numerator1 = 0
+denominator1 = 0
