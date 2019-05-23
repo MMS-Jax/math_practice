6425a67386af830ecc00a87dadeeac8351559934
Jordan188 <49407861+Jordan188@users.noreply.github.com>
2019-05-23 17:15:26 -0400

    Added user_name variable and imput
    Added two lines of code to ask for the user name save it to a variable

--- multiplying_dividing_fractions/Multiplying And Dividing Fractions 0.2.py
+++ multiplying_dividing_fractions/Multiplying And Dividing Fractions 0.2.py
@@ -1,4 +1,4 @@
-#Jordan M., Multiplying and Dividing Fractions, 05/10/2019, version 0.1
+#Jordan M., Multiplying and Dividing Fractions, 05/10/2019, version 0.2
 
 print("Hello, welcome to Fraction-Bot 9000.   I can multiply and divide fractions for you!\n")
 user_name = input("What is your name? [Type your name and press ENTER.]\n")
