6d31679f9f6a6a062f5029091ff6e7ee86cb08a8
kingjeffrobro <49403236+kingjeffrobro@users.noreply.github.com>
2019-05-23 09:47:50 -0400

    version 0.4
    added to print() lines to print the two fractions on the screen to verify that they are correct

--- multiplying_dividing_fractions/gaspareto_jeffery 0.4py.py
+++ multiplying_dividing_fractions/gaspareto_jeffery 0.4py.py
@@ -1,4 +1,4 @@
-# jeffery G multiplying and dividing fractions, 05/06/2019, version 0.3
+# jeffery G multiplying and dividing fractions, 05/06/2019, version 0.4
 
 print('welcome to fraction-bot 15! would you like me to multiply and divide fractions for you? \n')
 user_name = input("what is your name? [type your name and press ENTER.]\n")
