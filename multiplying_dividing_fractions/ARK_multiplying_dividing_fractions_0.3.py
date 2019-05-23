20067b2622de7c8ab5bc51d31210fed3af18e4f4
s11692498 <49410493+s11692498@users.noreply.github.com>
2019-05-23 13:26:58 -0400

    declared variables for the fractions
    added the numerator and denominator values for the two fractions

---
+++ multiplying_dividing_fractions/ARK_multiplying_dividing_fractions_0.3.py
@@ -0,0 +1,14 @@
+# Anthony k., Multiplying and dividing fractions, 05/07/2019, version 0.3
+
+print("hello, Let me multiply & divide your fractions.\n")
+user_name = input("what is your username [type your Name and press Enter]\n")
+print("hello ,", user_name,"how are you today?\n")
+
+print("for this program, I need to know the numerator & the denominator for both fractions.\n")\
+
+#variable for fraction 0.
+numerator0 = 0
+denominator0 = 0
+#variable for fraction 1.
+numerator1 = 0
+denominator1 = 0
