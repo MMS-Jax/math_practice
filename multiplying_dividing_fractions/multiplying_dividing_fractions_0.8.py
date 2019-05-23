f22a38b92db9ba174482418ba422684858acc233
s11615358 <49244219+s11615358@users.noreply.github.com>
2019-05-23 15:20:07 -0400

    Update multiplying_dividing_fractions_0.8.py

--- multiplying_dividing_fractions/multiplying_dividing_fractions_0.8.py
+++ multiplying_dividing_fractions/multiplying_dividing_fractions_0.8.py
@@ -1,26 +1,26 @@
 # Ryan K., Multiplying and Dividing Fractions, 05/10/2019, version 0.8
 
 print("Hello, welcome to Fraction-Bot 9000.   I can multiply and divide fractions for you!\n")
-user_name = input("What is your name? [Type your name and press ENTER.]\n")
+user_name = input("What is your name? [Type your name and press Enter.]\n")
 print("Hello,", user_name,"how are you today?\n")
 
 print("For this program, I need to know the numerator and the denominator for both fractions.\n")
 
 # Variables for fraction 0. 
-numerator0 = 0
-denominator0 = 0
+numerator0 = 4
+denominator0 = 5
 print("The first fraction is", numerator0,"/",denominator0,".\n")
 
 # Variables for fraction 1. 
-numerator1 = 0
-denominator1 = 0
+numerator1 = 6
+denominator1 = 7
 print("The second fraction is", numerator1,"/",denominator1,".\n")
 
 print("When multiplying fractions you multiply the two numerators together.\n")
 print("Then you will multiply the two denominators together.\n")
 
-numerator0 = int(input("Type the first numerator and press enter.\n")) 
-numerator1 = int(input("Type the second numerator and press enter.\n"))
+numerator0 = int(input(" 4.\n")) 
+numerator1 = int(input(" 6.\n"))
 
 denominator0 = int(input("Type the first denominator and press enter.\n"))
 denominator1 = int(input("Type the second denominator and press enter.\n"))
@@ -37,3 +37,13 @@ print("The new fraction is", new_numerator,"/",new_denominator,".\n")
 pie = 22 / 7
 print(pie)
 print("To divide a fraction, you will multiply using the reciprocal or inverse of the second fraction.\n")
+print("Then you have to do the same with the other numerator and the 1st fraction's denominator.\n")
+
+numerator0 = int(input(" 4.\n")) 
+numerator1 = int(input(" 6.\n"))
+
+denominator0 = int(input("Type the first denominator and press enter.\n"))
+denominator1 = int(input("Type the second denominator and press enter.\n"))
+
+new_numerator = numerator0 * denominator1
+new_denominator = denominator0 *numerator1 
