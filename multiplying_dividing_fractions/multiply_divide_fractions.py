509ea6e04a5617e8ede864137ab70dfd1d9df0d6
jacob <s11171527@students.duvalschools.org>
2019-05-23 09:56:55 -0400

    Update multiply_divide_fractions.py

--- multiplying_dividing_fractions/multiply_divide_fractions.py
+++ multiplying_dividing_fractions/multiply_divide_fractions.py
@@ -1,3 +1,12 @@
-# This program will multiply and divide proper and improper fractions. 
+# multiplying and dividing fractions by jacob.b 05/06/2019
 
+print("welcome to math bot 3 billion for all your multiplying and dividing needs.\n")
+print("in order to multiply or divide fractions we need to know the [....]and[....]\n " )
 
+numoratorA=0
+numoratorB=0
+
+denominatorA=0
+denominatorB=0
+
+print('the first numerator')
