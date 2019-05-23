16fa0351aa3eec08ec5ece5d28599137c75cb05a
Derpy Puffer fish <49402886+Preston893@users.noreply.github.com>
2019-05-23 10:16:39 -0400

    finished.

--- multiplying_dividing_fractions/multiplying_dividing_fractions_0.5.py
+++ multiplying_dividing_fractions/multiplying_dividing_fractions_0.5.py
@@ -7,23 +7,60 @@ print("Hello", user_name,"How are you today?")
 print("For this program, I need to know the numerator and denominator for both fractions.\n")
 
 #Variables for fraction 0.
-numerator0 = 1
-denominator0 = 2
+numerator0 = 0
+denominator0 = 0
 print("The first fraction is", numerator0,"/",denominator0,".\n")
 
 # Variables for fraction 1.
-numerator1 = 3
-denominator1 = 4
+numerator1 = 0
+denominator1 = 0
 print("The second fraction is", numerator1,"/",denominator1,".\n")
 
 print("When multiplying fractions you multiply the 2 numerators together.\n")
 print("Then you will multiply the 2 denominators together.\n")
 
-new_numerator = numerator0 * numerator1
-new_denominator = denominator0 * denominator1
+# Input the first fraction. 
+numerator0 = int(input("Type the first numerator and press enter.\n")) 
+denominator0 = int(input("Type the first denominator and press enter.\n"))
+
+# Input the second fraction. 
+numerator1 = int(input("Type the second numerator and press enter.\n"))
+denominator1 = int(input("Type the second denominator and press enter.\n"))
+
+print("The first fraction is", numerator0,"/",denominator0,".\n")
+print("The second fraction is", numerator1,"/",denominator1,".\n")
+
+new_numerator = numerator0 * numerator1 
+new_denominator = denominator0 * denominator1 
+
+print("The new fraction is", new_numerator,"/",new_denominator,".\n")  
+
+# This is where the division of the fractions will start.
+pie = 22/7
+print(pie)
+print("To divide a fraction, you will multiply using the reciprocal or inverse of the second fraction.\n")
+
+#Variables for fraction 0.
+numerator0 = 0
+denominator0 = 0
+print("The first fraction is", numerator0,"/",denominator0,".\n")
+
+# Variables for fraction 1.
+numerator1 = 0
+denominator1 = 0
+print("The second fraction is", numerator1,"/",denominator1,".\n")
+
+# Input the first fraction.
+numerator0 = int(input("Type the first numerator and press enter.\n"))
+denominator0 = int(input("Type the first denominator and press enter.\n"))
+
+# Input the second fraction.
+numerator1 = int(input("Type the first numerator and press enter.\n"))
+denominator1 = int(input("Type the first denominator and press enter.\n"))
+
+new_numerator = numerator0 * denominator1 
+new_denominator = denominator0 * numerator1 
 
 print("The new fraction is", new_numerator,"/",new_denominator,".\n")
 
-# This is where the division starts.
-pie = 22/7
-print(pie)
+
--- multiplying_dividing_fractions/multiplying_dividing_fractions_1.0.py
+++ multiplying_dividing_fractions/multiplying_dividing_fractions_1.0.py
@@ -1,35 +1,66 @@
 # Kyle P. , Multiplying and Dividing Fractions, 05/09/2019, version 1.0
 
-print("Hello, welcome to Fraction-bot 9000,I can multiply and divide fractions for you!/n")
-user_name = input("What is your name? [Type your name and press ENTER.]/n")
+print("Hello, welcome to fraction-bot 9000, I can multiply and divide fractions for you!\n")
+user_name = input("What is your name? [Type your name and press ENTER.]\n")
 print("Hello", user_name,"How are you today?")
 
-print("For this program, I need to know the numerator and denominator for both fractions./n")
+print("For this program, I need to know the numerator and denominator for both fractions.\n")
 
 #Variables for fraction 0.
 numerator0 = 0
 denominator0 = 0
-print("The first fraction is", numerator0,"/",denominator0,"./n")
+print("The first fraction is", numerator0,"/",denominator0,".\n")
 
 # Variables for fraction 1.
 numerator1 = 0
 denominator1 = 0
-print("The second fraction is", numerator1,"/",denominator1,"./n")
+print("The second fraction is", numerator1,"/",denominator1,".\n")
 
-print("When multiplying fractions you multiply the 2 numerators together./n")
-print("Then you will multiply the 2 denominators together./n")
+print("When multiplying fractions you multiply the 2 numerators together.\n")
+print("Then you will multiply the 2 denominators together.\n")
 
-new_numerator = numerator0 * denominator1
-new_denominator = denominator0 * numerator1
+# Input the first fraction. 
+numerator0 = int(input("Type the first numerator and press enter.\n")) 
+denominator0 = int(input("Type the first denominator and press enter.\n"))
 
-print("The new fraction is", new_numerator,"/",new_denominator,"./n")
+# Input the second fraction. 
+numerator1 = int(input("Type the second numerator and press enter.\n"))
+denominator1 = int(input("Type the second denominator and press enter.\n"))
 
-# This is where the division starts.
+print("The first fraction is", numerator0,"/",denominator0,".\n")
+print("The second fraction is", numerator1,"/",denominator1,".\n")
+
+new_numerator = numerator0 * numerator1 
+new_denominator = denominator0 * denominator1 
+
+print("The new fraction is", new_numerator,"/",new_denominator,".\n")  
+
+# This is where the division of the fractions will start.
 pie = 22/7
 print(pie)
-print("To divide a fractions, you will multiply using the reciporocal or inverse of the 2nd fraction./n")
+print("To divide a fraction, you will multiply using the reciprocal or inverse of the second fraction.\n")
 
+#Variables for fraction 0.
+numerator0 = 0
+denominator0 = 0
+print("The first fraction is", numerator0,"/",denominator0,".\n")
 
+# Variables for fraction 1.
+numerator1 = 0
+denominator1 = 0
+print("The second fraction is", numerator1,"/",denominator1,".\n")
 
+# Input the first fraction.
+numerator0 = int(input("Type the first numerator and press enter.\n"))
+denominator0 = int(input("Type the first denominator and press enter.\n"))
+
+# Input the second fraction.
+numerator1 = int(input("Type the first numerator and press enter.\n"))
+denominator1 = int(input("Type the first denominator and press enter.\n"))
+
+new_numerator = numerator0 * denominator1 
+new_denominator = denominator0 * numerator1 
+
+print("The new fraction is", new_numerator,"/",new_denominator,".\n")
 
 
