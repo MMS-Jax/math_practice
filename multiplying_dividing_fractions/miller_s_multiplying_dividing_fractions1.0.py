637ce2d72d15cf6bd73081009c90a533bf574d96
mintgreen18 <49650916+mintgreen18@users.noreply.github.com>
2019-05-23 11:53:03 -0400

    created a fraction
    did keep, change, and flip

---
+++ multiplying_dividing_fractions/miller_s_multiplying_dividing_fractions1.0.py
@@ -0,0 +1,30 @@
+#Sophie M., Multiplying and dividing fractions, 05/06/2019
+
+print("my name is duck")
+user_name = input("what is your name? Sophie.\n")
+print("Hello",",mintgreen18,how are you today?\n")
+
+print("in order to multiply or divide fractions i need to know the numerator and denominator for both fractions.\n")
+
+# Variables for fraction 0.
+numerator0 = 0
+denominator0 = 0
+
+print("The first fraction is", numerator0,"/",denominator0,".\n")
+# Variables for fraction 1.
+numerator1 = 0
+denominator1 = 0
+print("The second fraction is", numerator1,"/",denominator1,".\n")
+
+print("WHen multiplying fractions you multiply the two numerators together.\n")
+print("Then you will multiply the two denominators together.\n")
+
+new_numerator = numerator0 * denominator1
+new_denominator = denominator0 * denominator1
+
+print("The new fraction is", new_numerator,"/",new_denominator,".\n")
+
+new_numerator = numerator1 * denominator0
+new_denominator = denominator1 * numerator0
+
+print("The new fraction is", new_numerator,"/", new_denominator,".\n")
