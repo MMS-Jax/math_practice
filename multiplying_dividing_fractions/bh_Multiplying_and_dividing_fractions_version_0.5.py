f67d8a498755a258c7d29f63f44a7a2027f56827
Mars_Is_Aids <49207579+Benjamin-Hadziabdic@users.noreply.github.com>
2019-05-27 16:27:06 -0400

    added the ability for users to see the result of multiplying their fractions.

--- multiplying_dividing_fractions/bh_Multiplying_and_dividing_fractions_version_0.5.py
+++ multiplying_dividing_fractions/bh_Multiplying_and_dividing_fractions_version_0.5.py
@@ -1,4 +1,4 @@
-# Benjamin Hadziabdic, Multiplying and Dividing Fractions, 05/20/2019, Version 0.4
+# Benjamin Hadziabdic, Multiplying and Dividing Fractions, 05/27/2019, Version 0.5
 
 print('This is a program about multiplying and dividing fractions for you because you are too lazy to.\n')
 user_name = input('What do you want me to call you? [Type your potentially fake name and press ENTER].\n')
