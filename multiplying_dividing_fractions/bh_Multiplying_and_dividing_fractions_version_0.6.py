a245ebfb9ef03b10d172a535e16cfa826e169699
Mars_Is_Aids <49207579+Benjamin-Hadziabdic@users.noreply.github.com>
2019-05-28 13:18:03 -0400

    allowed user fraction input

--- multiplying_dividing_fractions/bh_Multiplying_and_dividing_fractions_version_0.6.py
+++ multiplying_dividing_fractions/bh_Multiplying_and_dividing_fractions_version_0.6.py
@@ -1,4 +1,4 @@
-# Benjamin Hadziabdic, Multiplying and Dividing Fractions, 05/20/2019, Version 0.4
+# Benjamin Hadziabdic, Multiplying and Dividing Fractions, 05/20/2019, Version 0.6
 
 print('This is a program about multiplying and dividing fractions for you because you are too lazy to.\n')
 user_name = input('What do you want me to call you? [Type your potentially fake name and press ENTER].\n')
@@ -19,8 +19,16 @@ print('Okay I see you.', numerator1,'/', denominator1,'.\n')
 print('When multiplying fractions, you multiply the numerator by the other numerator\n')
 print('Same thing with the denominator.\n')
 
-numerator0 = int(input('type the first numerator and hit ENTER\n'))
-numerator1 = int(input('type the second numerator and hit ENTER\n'))
+numerator0 = int(input('and the first numerator is...? type your number and hit ENTER\n'))
+numerator1 = int(input('and the second numerator is...? type your number and hit ENTER\n'))
+
+denominator0 = int(input('and the first denominator is...? type your number and hit ENTER\n'))
+denominator0 = int(input('and the second denominator is...? type your number and hit ENTER\n'))
+
+print('Aight, BET.', numerator0,'/', denominator0,'.\n')
+print('Okay I see you.', numerator1,'/', denominator1,'.\n')
+
+new_numerator = numerator0 * numerator1
+new_denominator = denominator0 * denominator1
 
 print('Okay I see you MORE.', new_numerator,'/', new_denominator,'.\n')
-
