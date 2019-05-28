ff029901f3959af864dee880ad5e2f4343848592
Mars_Is_Aids <49207579+Benjamin-Hadziabdic@users.noreply.github.com>
2019-05-28 19:01:47 -0400

    indicated and exampled fraction division

---
+++ multiplying_dividing_fractions/bh_Multiplying_and_dividing_fractions_version_0.7.py
@@ -0,0 +1,39 @@
+# Benjamin Hadziabdic, Multiplying and Dividing Fractions, 05/20/2019, Version 0.7
+
+print('This is a program about multiplying and dividing fractions for you because you are too lazy to.\n')
+user_name = input('What do you want me to call you? [Type your potentially fake name and press ENTER].\n')
+print('Wassup broski.',user_name,'...kinda generic but whatever')
+
+print('I assume you know what a fraction is. Tell me what the two numerators and denominators. If you dont know what a fraction is, hit ALT+F4.\n')
+
+# Variables for fraction 0
+numerator0 = 0
+denominator0 = 0
+print('Aight, BET.', numerator0,'/', denominator0,'.\n')
+
+# Variables for fraction 0
+numerator1 = 0
+denominator1 = 0
+print('Okay I see you.', numerator1,'/', denominator1,'.\n')
+
+print('When multiplying fractions, you multiply the numerator by the other numerator\n')
+print('Same thing with the denominator.\n')
+
+numerator0 = int(input('and the first numerator is...? type your number and hit ENTER\n'))
+numerator1 = int(input('and the second numerator is...? type your number and hit ENTER\n'))
+
+denominator0 = int(input('and the first denominator is...? type your number and hit ENTER\n'))
+denominator0 = int(input('and the second denominator is...? type your number and hit ENTER\n'))
+
+print('Aight, BET.', numerator0,'/', denominator0,'.\n')
+print('Okay I see you.', numerator1,'/', denominator1,'.\n')
+
+new_numerator = numerator0 * numerator1
+new_denominator = denominator0 * denominator1
+
+print('Okay I see you MORE.', new_numerator,'/', new_denominator,'.\n')
+
+# This is where division starts.
+pie = 22 / 7
+print(pie)
+print('To divide a fraction, you will multiply using the reciprocal or inverse of the second fraction.')
