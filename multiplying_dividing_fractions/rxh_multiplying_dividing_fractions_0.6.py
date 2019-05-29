8af75c8b5cfa245f317005e81ab3172e84f0a412
minecraftepic <rxhendricks05@live.com>
2019-05-29 14:55:27 -0400

    Added variables for new fractions
    declared new_denominator and new_numarator also wrote code to multply two fractions together.

---
+++ multiplying_dividing_fractions/rxh_multiplying_dividing_fractions_0.6.py
@@ -0,0 +1,27 @@
+#Roman H, Multiplying and Dividing Fractions, 05/15/2019, version 0.6
+
+print("Hey, i'm the fraction man, I do the stuff with multiply and divide the fraction.\n")
+
+user_name = input("You gotta name? [type it in then press ENTER.]\n")
+
+print("Yo, ", user_name,"whats good?")
+
+print("Ok, so I'm gonna need the numerator and the denominator for both fractions.\n")
+
+# Variables for fraction 0
+numerator0 = 0
+denominator0 = 0
+print("The first fraction is" , numerator0, "/",denominator0, ".\n")
+
+# Variables for fraction 1
+numerator1 = 0
+denominator1 = 0
+print("The second fraction is" , numerator1, "/",denominator1, ".\n")
+
+print("When you multiply the fractions, you gotta multiply the two numerators together.\n")
+print("Then you multiply the two denominators together.\n")
+
+new_numerator = numerator0 * numerator1
+new_denominator = denominator0 * denominator1
+
+print("Your new fraction is", new_numerator, "/" ,new_denominator, ".\n")
